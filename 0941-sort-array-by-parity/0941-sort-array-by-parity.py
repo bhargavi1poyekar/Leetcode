eca4a4ef9f759c3779556b53ac4c8c3e0f56b6c6
bhargavi1poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2023-06-23 15:51:31 -0400

    Time: 93 ms (56.05%), Space: 17.2 MB (39.09%) - LeetHub

--- 0941-sort-array-by-parity/0941-sort-array-by-parity.py
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.py
@@ -12,15 +12,11 @@ class Solution:
                 temp=nums[left]
                 nums[left]=nums[right]
                 nums[right]=temp
-                left+=1
-                right-=1
                 
-            elif nums[left]%2==1 and nums[right]%2==1:
+            if nums[right]%2==1:
                 right-=1
-            elif nums[left]%2==0 and nums[right]%2==1:
-                left+=1
-                right-=1
-            elif nums[left]%2==0 and nums[right]%2==0:
+            
+            if nums[left]%2==0:
                 left+=1
             
         return nums
