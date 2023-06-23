6d50d975dd3430aaefd26aff552ed4b8fac2399d
bhargavi1poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2023-06-23 15:46:15 -0400

    Time: 176 ms (67.45%), Space: 18 MB (49.76%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -4,10 +4,7 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         ptr1=0
-        ptr2=0
-        
         n=len(nums)
-        count=0
         
         for ptr2 in range(n):
             if nums[ptr2]!=0:
@@ -15,9 +12,5 @@ class Solution:
                 nums[ptr2]=nums[ptr1]
                 nums[ptr1]=temp
                 ptr1+=1
-                
-            
-        
-        
 
         return nums
