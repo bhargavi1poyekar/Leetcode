ded8262e4715be5d09d41eb202da9e7b9533f82a
bhargavi1poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2023-06-24 10:10:37 -0400

    Time: 876 ms (94.77%), Space: 31.2 MB (36.14%) - LeetHub

--- 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.py
+++ 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.py
@@ -1,15 +1,30 @@
 class Solution:
     def waysToSplitArray(self, nums: List[int]) -> int:
 
-        prefix_sum=[nums[0]]
+        # prefix_sum=[nums[0]]
+
+        # n=len(nums)
+        # for i in range(1,n):
+        #     prefix_sum.append(nums[i]+prefix_sum[i-1])
+
+        # count=0
+        # for i in range(n-1):
+        #     if prefix_sum[i]>=prefix_sum[n-1]-prefix_sum[i]:
+        #         count+=1
+        
+        # return count
+
+        left=right=count=0
 
         n=len(nums)
-        for i in range(1,n):
-            prefix_sum.append(nums[i]+prefix_sum[i-1])
+        total=sum(nums)
 
-        count=0
-        for i in range(n-1):
-            if prefix_sum[i]>=prefix_sum[n-1]-prefix_sum[i]:
+        for i in range(len(nums)-1):
+            left+=nums[i]
+            right=total-left
+
+            if left>=right:
                 count+=1
         
         return count
+
