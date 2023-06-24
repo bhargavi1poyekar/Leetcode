a7741ea22a484c3042d847c75a28c2a11bf6fa6c
bhargavi1poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2023-06-24 15:21:08 -0400

    Time: 1811 ms (9.08%), Space: 34.3 MB (92.72%) - LeetHub

--- 2211-k-radius-subarray-averages/2211-k-radius-subarray-averages.py
+++ 2211-k-radius-subarray-averages/2211-k-radius-subarray-averages.py
@@ -3,16 +3,17 @@ class Solution:
 
         prefix_sum=[0]
         num_in_win=2*k+1
-        ans=[0 for i in range(len(nums))]
+        # ans=[0 for i in range(len(nums))]
+        ans=[]
 
         for i in range(len(nums)):
             prefix_sum.append(nums[i]+prefix_sum[i])
         
         for i in range(len(nums)):
             if i-k<0 or i+k>=len(nums):
-                ans[i]=-1
+                ans.append(-1)
             else:
-                ans[i]=(prefix_sum[i+k+1]-prefix_sum[i-k])//num_in_win
+                ans.append((prefix_sum[i+k+1]-prefix_sum[i-k])//num_in_win)
                 
         
         return ans
