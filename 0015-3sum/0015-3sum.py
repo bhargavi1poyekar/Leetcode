2ba605989c822e12dc30c08d9ba6c20191f5ef03
bhargavi1poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2023-06-25 10:34:52 -0400

    Time: 1312 ms (82.16%), Space: 21.4 MB (11.83%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,15 +1,17 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
 
-        ans=[]
+        ans=[] # Final Answer
 
-        nums=sorted(nums)
-
+        nums=sorted(nums) # Sort for two sum
+ 
         for i in range(len(nums)):
-            if nums[i]>0:
+            if nums[i]>0: 
+                # No later numbers will give negative sum to become 0
                 break
             elif i!=0 and nums[i]!=nums[i-1]:
                 ans.extend(self.twoSum(nums[i+1:],-nums[i]))
+                # if i not repeated, send for two sum, with remaining nums and target as negative of current num
             elif i==0:
                 ans.extend(self.twoSum(nums[i+1:],-nums[i]))
         
@@ -23,17 +25,20 @@ class Solution:
         right=len(nums)-1
 
         ans=[]
-        while(left<right):
+        while(left<right): # Till left and right overlap
 
-            if nums[left]+nums[right]==target:
-                ans.append([-target,nums[left],nums[right]])
-                left+=1
+            if nums[left]+nums[right]==target: # If correct triplet,
+                ans.append([-target,nums[left],nums[right]]) 
+                # Append to the triplets
+                left+=1 
                 right-=1
 
-                while(left<right and nums[left]==nums[left-1]):
+                while(left<right and nums[left]==nums[left-1]): 
+                    # to avoid repeatation
                     left+=1
 
-            elif nums[left]+nums[right]>target:
+            elif nums[left]+nums[right]>target: 
+                # If total greater than target
                 right-=1
 
             else:
