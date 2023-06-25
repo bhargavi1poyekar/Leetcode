4cf7e40beae624ec439064effc65a23d2f2aba14
bhargavi1poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2023-06-25 10:55:20 -0400

    Time: 147 ms (62.63%), Space: 17.2 MB (24.72%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,37 +1,41 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
+        
+        if len(t)>len(s):return ""
 
         t_char=collections.Counter(t)
-        unique_t=len(t_char)
+        unique=len(t_char)
 
         win_char=collections.Counter()
-        
-        left=0
+        match=0
+
         min_length=float('inf')
         min_right=0
         min_left=0
 
-        match=0
+        left=0
 
         for right in range(len(s)):
             
             win_char[s[right]]+=1
 
-            if win_char[s[right]]==t_char[s[right]]:
+            if t_char[s[right]]==win_char[s[right]]:
                 match+=1
             
-            while(match==unique_t):
-                if min_length>right-left+1:
+            
+
+            while(unique==match):
+                if right-left+1<min_length:
                     min_length=right-left+1
-                    min_left=left
                     min_right=right
+                    min_left=left
 
-                if win_char[s[left]]==t_char[s[left]]:
+                if t_char[s[left]]==win_char[s[left]]:
                     match-=1
-
+                
                 win_char[s[left]]-=1
                 left+=1
-
+        
         return s[min_left:min_right+1] if min_length!=float('inf') else ""
-            
-            
+
+
