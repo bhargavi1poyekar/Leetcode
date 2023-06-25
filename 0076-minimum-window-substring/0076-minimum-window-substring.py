cda197bbcb6fdb67515ef4a5968f6fb04edf8dab
bhargavi1poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2023-06-25 10:57:26 -0400

    Time: 155 ms (56.26%), Space: 17.2 MB (62.24%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,15 +1,15 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         
-        if len(t)>len(s):return ""
+        if len(t)>len(s):return "" # If t>s no substring
 
-        t_char=collections.Counter(t)
-        unique=len(t_char)
+        t_char=collections.Counter(t) # Char freq for t
+        unique=len(t_char) # unique characters in t
 
-        win_char=collections.Counter()
-        match=0
+        win_char=collections.Counter() # Char freq for window
+        match=0  # Match count of window and t
 
-        min_length=float('inf')
+        min_length=float('inf') 
         min_right=0
         min_left=0
 
@@ -17,25 +17,23 @@ class Solution:
 
         for right in range(len(s)):
             
-            win_char[s[right]]+=1
+            win_char[s[right]]+=1 # inc window count
 
-            if t_char[s[right]]==win_char[s[right]]:
+            if t_char[s[right]]==win_char[s[right]]: # If window count of char matches t count of char
                 match+=1
-            
-            
 
-            while(unique==match):
-                if right-left+1<min_length:
-                    min_length=right-left+1
+            while(unique==match): # If all unique char match
+                if right-left+1<min_length: # Update min
+                    min_length=right-left+1 
                     min_right=right
                     min_left=left
 
-                if t_char[s[left]]==win_char[s[left]]:
+                if t_char[s[left]]==win_char[s[left]]: # If they match before removing, dec match 
                     match-=1
                 
-                win_char[s[left]]-=1
-                left+=1
+                win_char[s[left]]-=1 # dec wi freq
+                left+=1 # Inc window length
         
-        return s[min_left:min_right+1] if min_length!=float('inf') else ""
+        return s[min_left:min_right+1] if min_length!=float('inf') else "" 
 
 
