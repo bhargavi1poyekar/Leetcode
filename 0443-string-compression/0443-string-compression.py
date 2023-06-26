32520299c6ec179eca33f43a1ccd5e77ff2aa8d2
bhargavi1poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2023-06-25 21:13:07 -0400

    Time: 76 ms (64.88%), Space: 16.4 MB (52.36%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.py
@@ -0,0 +1,42 @@
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+
+       
+        n=len(chars)
+
+        if n==1:
+            return 1
+        
+        ptr1=0
+        ptr2=1
+        index=0
+        count=1
+        while(ptr2<n):
+            if chars[ptr1]!=chars[ptr2]:
+                chars[index]=chars[ptr1]
+                index+=1
+                if count>1 and count<10:
+                    chars[index]=str(count)
+                    index+=1
+                elif count>=10:
+                    chars[index:index+len(str(count))]=str(count)
+                    index+=len(str(count))
+                count=0
+                ptr1=ptr2
+            count+=1
+            ptr2+=1
+        
+        chars[index]=chars[ptr1]
+        index+=1
+        if count>1 and count<10:
+            chars[index]=str(count)
+            index+=1
+        elif count>=10:
+            
+            chars[index:index+len(str(count))]=str(count)
+            index+=len(str(count))
+        
+        
+        return index
+        
+
