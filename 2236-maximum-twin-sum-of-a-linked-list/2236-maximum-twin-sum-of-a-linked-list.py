a55a5411fc6e249c4dc0430bbea522f81f197a15
bhargavi1poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2023-06-26 15:13:04 -0400

    Time: 915 ms (55.36%), Space: 47.5 MB (77.12%) - LeetHub

--- 2236-maximum-twin-sum-of-a-linked-list/2236-maximum-twin-sum-of-a-linked-list.py
+++ 2236-maximum-twin-sum-of-a-linked-list/2236-maximum-twin-sum-of-a-linked-list.py
@@ -6,46 +6,78 @@
 class Solution:
     def pairSum(self, head: Optional[ListNode]) -> int:
 
-        prev=None
-        slow=head
-        fast=head
-        count=0
+        slow,fast=head,head
+        
 
         # Find middle of linked list
         while(fast and fast.next):
             prev=slow
             slow=slow.next
             fast=fast.next.next
-            count+=2
+        
+        prev,curr=None,slow
 
-        curr=slow
-        con, tail=prev,curr
-
-        # Reversing the remaining part of linked list.
         while(curr):
             next_node=curr.next
             curr.next=prev
             prev=curr
             curr=next_node
-        
-        if con:
-            con.next=prev
-        else:
-            head=prev
-        
-        tail.next=curr
 
-        diff_n=count//2
-        slow=head
-        fast=head
+        start=head
         max_sum=0
-        while(fast):
-            if diff_n<1:
-                max_sum=max(max_sum,slow.val+fast.val)
-                slow=slow.next
-            fast=fast.next
-            diff_n-=1
+        while(prev):
+            max_sum=max(max_sum, start.val+prev.val)
+            prev=prev.next
+            start=start.next
         
         return max_sum
+        '''
+        Below solution is also correct, but I am reversed list and again traversing from start which is not needed.
+        '''
+
+        # prev=None
+        # slow,fast=head,head
+        # count=0
+
+        # # Find middle of linked list
+        # while(fast and fast.next):
+        #     prev=slow
+        #     slow=slow.next
+        #     fast=fast.next.next
+        #     count+=2
+
+        # curr=slow
+        # con, tail=prev,curr
+
+        # # Reversing the remaining part of linked list.
+        # while(curr):
+        #     next_node=curr.next
+        #     curr.next=prev
+        #     prev=curr
+        #     curr=next_node
+        # if con:
+        #     con.next=prev
+        # else:
+        #     head=prev
+        
+        # tail.next=curr
+
+        # # Now comparing nodes
+        # diff_n=count//2
+        # slow=head
+        # fast=head
+        # max_sum=0
+        # while(fast):
+        #     if diff_n<1:
+        #         max_sum=max(max_sum,slow.val+fast.val)
+        #         slow=slow.next
+        #     fast=fast.next
+        #     diff_n-=1
+        
+        # return max_sum
+
+
+
+
         
 
