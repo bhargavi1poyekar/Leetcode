2dc50c12a635628126a5415d87c81ae746d53b3d
bhargavi1poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2023-06-28 15:18:20 -0400

    Time: 2865 ms (5.07%), Space: 73.1 MB (6.57%) - LeetHub

--- 1354-find-players-with-zero-or-one-losses/1354-find-players-with-zero-or-one-losses.py
+++ 1354-find-players-with-zero-or-one-losses/1354-find-players-with-zero-or-one-losses.py
@@ -2,22 +2,42 @@ class Solution:
     def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
 
        
-        loses=collections.Counter()
+        # loses=collections.Counter()
+        
+        # for i in range(len(matches)):
+        #     loses[matches[i][0]]+=0
+        #     loses[matches[i][1]]+=1
         
         
-        for i in range(len(matches)):
-            loses[matches[i][0]]+=0
-            loses[matches[i][1]]+=1
-        
-        
+        # notlost=[]
+        # lostone=[]
+        # for i in loses:
+        #     if loses[i]==0:
+        #         notlost.append(i)
+        #     elif loses[i]==1:
+        #         lostone.append(i)
+    
+        # return [sorted(notlost),sorted(lostone)]
+
+        # Since players are in the range of 0,len(matches):
+
+        loses=[-1 for i in range(100001)]
+
+        for i in matches:
+            if loses[i[0]-1]==-1:
+                loses[i[0]-1]=0
+            if loses[i[1]-1]==-1:
+                loses[i[1]-1]=1
+            else:loses[i[1]-1]+=1
+
+        # print(loses)
         notlost=[]
         lostone=[]
-        for i in loses:
+       
+        for i in range(len(loses)):
             if loses[i]==0:
-                notlost.append(i)
+                notlost.append(i+1)
             elif loses[i]==1:
-                lostone.append(i)
-    
-        return [sorted(notlost),sorted(lostone)]
-        
+                lostone.append(i+1)
+        return [notlost,lostone]
         
