93d854fd8db85bcda49ee19a64bf3dcf561dac39
bhargavi1poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2023-06-28 12:37:02 -0400

    Time: 60 ms (15.8%), Space: 16.4 MB (16.54%) - LeetHub

--- 2053-check-if-all-characters-have-equal-number-of-occurrences/2053-check-if-all-characters-have-equal-number-of-occurrences.py
+++ 2053-check-if-all-characters-have-equal-number-of-occurrences/2053-check-if-all-characters-have-equal-number-of-occurrences.py
@@ -1,13 +1,15 @@
 class Solution:
     def areOccurrencesEqual(self, s: str) -> bool:
 
-        hash=collections.Counter()
-        for i in s:
-            hash[i]+=1
+        # hash=collections.Counter()
+        # for i in s:
+        #     hash[i]+=1
         
-        exp=len(s)//len(hash)
+        # exp=len(s)//len(hash)
         
-        for i in hash:
-            if hash[i]!=exp:
-                return False
-        return True
+        # for i in hash:
+        #     if hash[i]!=exp:
+        #         return False
+        # return True
+        
+        return len(set(Counter(s).values())) == 1
