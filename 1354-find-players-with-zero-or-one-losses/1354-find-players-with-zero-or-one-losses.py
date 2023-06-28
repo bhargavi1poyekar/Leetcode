5590cceec7a7ab0b72e30cf2458a1b56faf496a3
bhargavi1poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2023-06-28 15:00:54 -0400

    Time: 1930 ms (30.59%), Space: 71.2 MB (47.38%) - LeetHub

--- 1354-find-players-with-zero-or-one-losses/1354-find-players-with-zero-or-one-losses.py
+++ 1354-find-players-with-zero-or-one-losses/1354-find-players-with-zero-or-one-losses.py
@@ -1,19 +1,19 @@
 class Solution:
     def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
 
-        players=set()
+       
         loses=collections.Counter()
         
+        
         for i in range(len(matches)):
-            players.add(matches[i][0])
-            players.add(matches[i][1])
+            loses[matches[i][0]]+=0
             loses[matches[i][1]]+=1
         
         
         notlost=[]
         lostone=[]
-        for i in players:
-            if i not in loses:
+        for i in loses:
+            if loses[i]==0:
                 notlost.append(i)
             elif loses[i]==1:
                 lostone.append(i)
