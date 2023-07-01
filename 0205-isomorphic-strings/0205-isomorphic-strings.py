60b641442d920bd09e24cc31c4ecf5d45f16d75f
bhargavi1poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2023-07-01 14:28:09 -0400

    Time: 54 ms (74.32%), Space: 16.4 MB (98.6%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,23 +1,11 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        # s_ind=[]
-        # t_ind=[]
-
-        # for i in s:
-        #     s_ind.append(s.index(i))
         
-        # for i in t:
-        #     t_ind.append(t.index(i))
-        
-
-        # if s_ind==t_ind:
-        #     return True
-        # return False
-
-        s_map={}
-        t_map={}
+        s_map=collections.Counter()
+        t_map=collections.Counter()
 
         for i in range(len(s)):
+
             if s[i] not in s_map and t[i] not in t_map:
                 s_map[s[i]]=t[i]
                 t_map[t[i]]=s[i]
