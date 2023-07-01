4e48db72ca3bd99137c7de8315cc5b37fbc89cb7
bhargavi1poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2023-07-01 10:57:00 -0400

    Time: 81 ms (67.53%), Space: 16.3 MB (95.77%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.py
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.py
@@ -8,13 +8,11 @@ class Solution:
 
         l1=self.reverseList(l1)
         l2=self.reverseList(l2)
-        
+
         dummy=ListNode()
         curr=dummy
         carry=0
-
-        while l1 or l2 or carry:
-
+        while(l1 or l2 or carry):
             val1=l1.val if l1 else 0
             val2=l2.val if l2 else 0
 
@@ -22,19 +20,25 @@ class Solution:
             carry=val//10
             val=val%10
 
-            newNode=ListNode(val)
-            curr.next=newNode
+            newnode=ListNode(val)
+            curr.next=newnode
             curr=curr.next
 
             l1=l1.next if l1 else 0
             l2=l2.next if l2 else 0
+
+        return self.reverseList(dummy.next) 
+
         
-        return self.reverseList(dummy.next)
-    
+        
     def reverseList(self,head):
 
+        if not head or not head.next:
+            return head
+        
         prev=None
         curr=head
+
         while(curr):
             nn=curr.next
             curr.next=prev
@@ -42,3 +46,4 @@ class Solution:
             curr=nn
         
         return prev
+
