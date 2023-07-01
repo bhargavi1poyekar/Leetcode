b434c869c02265afcdd3b191298a514d2b18e11e
bhargavi1poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2023-07-01 12:31:53 -0400

    Time: 2128 ms (52.9%), Space: 56.1 MB (25.48%) - LeetHub

--- 2196-reverse-nodes-in-even-length-groups/2196-reverse-nodes-in-even-length-groups.py
+++ 2196-reverse-nodes-in-even-length-groups/2196-reverse-nodes-in-even-length-groups.py
@@ -8,42 +8,42 @@ class Solution:
 
         if not head or not head.next:
             return head
-
-        prev=head
-        curr=head.next
-
-        grp_count=2
-        count=0
-        count_ptr=head.next
+        
+        grp=2
         node_count=0
-        while curr:
+        count_ptr=head.next
+        curr=head.next
+        prev=head
 
-            while(count_ptr and node_count<grp_count):
+        while(curr):
+            while(count_ptr and node_count<grp):
                 count_ptr=count_ptr.next
                 node_count+=1
-                
+            print(node_count)
+            
             if node_count%2==0:
                 con,tail=prev,curr
-                while(count<grp_count and curr):
+                while(node_count and curr):
                     nn=curr.next
                     curr.next=prev
                     prev=curr
                     curr=nn
-                    count+=1
-                    
+                    node_count-=1
+                
                 con.next=prev
                 tail.next=curr
                 prev=tail
-                count=0
             
             else:
                 curr=count_ptr
                 while(prev.next!=curr):
                     prev=prev.next
+                    
             node_count=0
-            grp_count+=1
-            
+            grp+=1
         
-        return(head)
-            
+        return head
+
+
+
 
