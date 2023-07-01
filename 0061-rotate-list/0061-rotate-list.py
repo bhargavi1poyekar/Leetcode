1e6ae060d5e9d2c980e35715b82643e90572a76f
bhargavi1poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2023-07-01 10:06:40 -0400

    Time: 48 ms (83.3%), Space: 16.1 MB (99.98%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.py
+++ 0061-rotate-list/0061-rotate-list.py
@@ -6,78 +6,34 @@
 class Solution:
     def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
 
-        
-        if not head:
+        if not head or not head.next or k==0:
             return head
-            
+        
         count=1
-        ptr=head
-        while(ptr.next):
-            ptr=ptr.next
+        curr=head
+        while(curr.next):
             count+=1
+            curr=curr.next
 
         k=k%count
-        # print(count,k)
-        ptr.next=head
-        new_end=count-k
-        ptr=head
-        # print(new_end)
+        if k==0:
+            return head
 
-        while(new_end>1):
-            ptr=ptr.next
-            new_end-=1
+        curr.next=head
 
-        head=ptr.next
-        ptr.next=None
-        
-        return head
+        prev=None
+        curr=head
 
+        n=count
+        pos=0
+        while(pos<(n-k)):
+            prev=curr
+            curr=curr.next
+            pos+=1
 
-        # if not head or k==0:
-        #     return head
-        # count=0
-        # ptr=head
-        # while(ptr):
-        #     ptr=ptr.next
-        #     count+=1
-        # print(count)
-        # # print(count)
-        # k=k%count
-        # if count==1 or k==0:
-        #     return head
-        # head=self.reverseList(head,1,count)
-        # head=self.reverseList(head,1,k)
-        # head=self.reverseList(head,k+1,count)
-        # return head
-    
-    # def reverseList(self,head,left,right):
-
-    #     if not head or left==right :
-    #         return head
-        
-    #     prev=None
-    #     curr=head
-
-    #     while(left>1):
-    #         prev=curr
-    #         curr=curr.next
-    #         left,right=left-1,right-1
-
-    #     con,tail=prev,curr
-    #     while(right and curr):
-    #         nn=curr.next
-    #         curr.next=prev
-    #         prev=curr
-    #         curr=nn
-    #         right-=1
-        
-    #     if con:
-    #         con.next=prev
-    #     else:
-    #         head=prev
-        
-    #     tail.next=curr
-    #     return head
+        head=prev.next
+        prev.next=None
+        return(head)
 
 
         
