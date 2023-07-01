0ba468d5c1e4d2b2c0a0c0a1526352f012416755
bhargavi1poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2023-07-01 09:47:43 -0400

    Time: 1612 ms (99.37%), Space: 62.9 MB (64.78%) - LeetHub

--- 2216-delete-the-middle-node-of-a-linked-list/2216-delete-the-middle-node-of-a-linked-list.py
+++ 2216-delete-the-middle-node-of-a-linked-list/2216-delete-the-middle-node-of-a-linked-list.py
@@ -9,19 +9,16 @@ class Solution:
         if not head or not head.next:
             return None
 
-        # prev=None
+        prev=None
         slow=head
-        fast=head.next.next
+        fast=head
 
         while(fast and fast.next):
-
-            # prev=slow
+            prev=slow
             slow=slow.next
             fast=fast.next.next
         
-        # if slow:prev.next=slow.next 
-        # else: prev.next=None
-        if slow.next:
-            slow.next=slow.next.next
+        if prev and prev.next:
+            prev.next=prev.next.next
 
-        return(head)
+        return head
