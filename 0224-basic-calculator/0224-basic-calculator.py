3e1a77df748cd3554e9404e8e9918565fa325866
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2023-07-06 12:51:21 -0400

    Time: 85 ms (80.49%), Space: 17.6 MB (99.5%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.py
@@ -0,0 +1,37 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+
+        stack=[]
+        res=0
+        op=0
+        sign=1
+
+        for i in s:
+            if i=='(':
+                stack.append(res)
+                stack.append(sign)
+
+                res=0
+                sign=1
+            
+            elif i.isdigit():
+                op=op*10+int(i)
+            
+            elif i=='+':
+                res+=sign*op
+                sign=1
+                op=0
+            
+            elif i=='-':
+                res+=sign*op
+                sign=-1
+                op=0
+            
+            elif i==')':
+                res+=sign*op
+                res*=stack.pop()
+                res+=stack.pop()
+                op=0
+        
+        return res+sign*op
+            
