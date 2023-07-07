3bd7237e92d864303f1f94532b10eb84690f4193
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2023-07-07 15:21:45 -0400

    Time: 1618 ms (91.3%), Space: 33.2 MB (24.68%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,23 +1,23 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:   
 
-        queue=deque()
-        max_window=[]
+        queue=deque() # monotonically decreasing queue
+        max_window=[] # list to store max of windows
 
         for curr in range(len(nums)):
             
-            while(queue and nums[queue[-1]]<nums[curr]):
+            while(queue and nums[queue[-1]]<nums[curr]): # To maintain decreasing queue
                 queue.pop()
 
-            queue.append(curr)
+            queue.append(curr)  
 
-            if queue[0]+k==curr:
+            # If window has reached its limits and max has expired, remove the front (max)
+            if queue[0]+k==curr: 
                 queue.popleft()
             
+            # if window is created. (i.e. there are k elements in the window)
             if curr>=k-1:
                 max_window.append(nums[queue[0]])
-            
-            # print(curr, queue, max_window)
         
         return(max_window)
 
