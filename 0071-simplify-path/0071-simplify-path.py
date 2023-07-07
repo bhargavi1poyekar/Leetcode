01dff13f33d88acf0b05b45ba476b9bbd17956da
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2023-07-07 15:50:08 -0400

    Time: 53 ms (39.8%), Space: 16.2 MB (93.74%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.py
+++ 0071-simplify-path/0071-simplify-path.py
@@ -1,20 +1,22 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
 
-        stack_path=[]
+        stack_path=[] # stack to store directories
 
-        for direc in path.split('/'):
-            # print(direc)
+        for direc in path.split('/'): # split the path on /
 
+            # go to parent directory 
             if direc=='..':
                 if stack_path:
                     stack_path.pop()
             
+            # stay in curr directory 
             elif direc=='.' or direc=='':
                 continue
             
+            # else add the directory name in stack
             else:
                 stack_path.append(direc)
-            
-
+        
+        # starts with /
         return '/' + '/'.join(stack_path)
