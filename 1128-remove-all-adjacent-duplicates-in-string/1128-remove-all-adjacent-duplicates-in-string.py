394d36512aa4782afae73b29d911ea59cd7f4848
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2023-07-07 12:09:52 -0400

    Time: 94 ms (54.58%), Space: 17.2 MB (47.36%) - LeetHub

--- 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.py
+++ 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.py
@@ -4,12 +4,9 @@ class Solution:
         stack=[]
 
         for i in s:
-            if not stack:
-                stack.append(i)
-            else:
-                if stack[-1]==i:
-                    stack.pop()
-                else:
-                    stack.append(i)
+            if stack and i==stack[-1]:
+                stack.pop()
+            
+            else:stack.append(i)
         
-        return(''.join(stack))
+        return ''.join(stack)
