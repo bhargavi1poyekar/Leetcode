9fc95ec65a9293444938c57147e48d4ba7bc5341
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2023-07-07 15:59:35 -0400

    Time: 72 ms (42.91%), Space: 16.8 MB (7.12%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,22 +1,25 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        
+        # Hash map to store next greater element of nums2
         next_greater={}
-        stack=[]
+        stack=[] 
 
         for num in nums2:
-            while stack and stack[-1]<num:
-                next_greater[stack.pop()]=num
+            while stack and stack[-1]<num: # loop till new element is greater than top
+                next_greater[stack.pop()]=num # for tops, curr element will be next greater
             
-            stack.append(num)
+            stack.append(num) # add curr in stack
         
-        while stack:
+        while stack: # removing remaining elements who don't have next greater element
             next_greater[stack.pop()]=-1
         
         answer=[]
-        for num in nums1:
+        for num in nums1: # now just check nums1 and get the answer array
             answer.append(next_greater[num])
         
         return answer
+
          
 
 
