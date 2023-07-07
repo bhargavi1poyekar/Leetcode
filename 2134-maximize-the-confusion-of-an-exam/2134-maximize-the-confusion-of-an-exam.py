200cf7acceb0568a13e8f70326aa61e0d1425711
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2023-07-07 09:45:06 -0400

    Time: 499 ms (32.31%), Space: 16.7 MB (55.49%) - LeetHub

--- 2134-maximize-the-confusion-of-an-exam/2134-maximize-the-confusion-of-an-exam.py
+++ 2134-maximize-the-confusion-of-an-exam/2134-maximize-the-confusion-of-an-exam.py
@@ -1,22 +1,22 @@
 class Solution:
     def maxConsecutiveAnswers(self, answerKey: str, k: int) -> int:
         s=answerKey
-        maxChar=s[0]
-        charFreq=collections.Counter()
+        maxAns=answerKey[0]
+        ansFreq=collections.Counter()
         left=0
 
         max_length=0
 
-        for right in range(len(s)):
-            charFreq[s[right]]+=1
+        for right in range(len(answerKey)):
+            ansFreq[answerKey[right]]+=1
 
-            if charFreq[s[right]]>=charFreq[maxChar]:
-                maxChar=s[right]
+            if ansFreq[answerKey[right]]>=ansFreq[maxAns]:
+                maxAns=answerKey[right]
             
             current_len=right-left+1
 
-            if(current_len-charFreq[maxChar]>k):
-                charFreq[s[left]]-=1
+            if(current_len-ansFreq[maxAns]>k):
+                ansFreq[answerKey[left]]-=1
                 left+=1
             
             max_length=max(max_length,right-left+1)
