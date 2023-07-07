a5a50b818b4ba362d279bcb467b5fc5b01e65c97
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2023-07-07 16:01:43 -0400

    Time: 273 ms (50.11%), Space: 17.9 MB (84.74%) - LeetHub

--- 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.py
+++ 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.py
@@ -4,9 +4,9 @@ class Solution:
         stack=[]
 
         for i in s:
-            if stack and i=='*':
+            if stack and i=='*': # remove closest non star char 
                 stack.pop()
-            else:
+            else: # add all other elements other than star
                 stack.append(i)
 
         return(''.join(stack))
