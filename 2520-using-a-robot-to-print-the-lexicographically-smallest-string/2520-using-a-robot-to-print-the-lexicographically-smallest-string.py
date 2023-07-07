73613da2138a47074a14d120c29a47a3a92a8ae5
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2023-07-07 19:47:08 -0400

    Time: 1799 ms (13.97%), Space: 19.5 MB (63.24%) - LeetHub

--- 2520-using-a-robot-to-print-the-lexicographically-smallest-string/2520-using-a-robot-to-print-the-lexicographically-smallest-string.py
+++ 2520-using-a-robot-to-print-the-lexicographically-smallest-string/2520-using-a-robot-to-print-the-lexicographically-smallest-string.py
@@ -1,23 +1,32 @@
 class Solution:
     def robotWithString(self, s: str) -> str:
 
-        count=Counter(s)
-        t_stack,ans=[],[]
+        char_freq=Counter(s)
+        t_stack=[]
+        paper=[]
 
         for ch in s:
+           
             t_stack.append(ch)
 
-            if count[ch]==1:
-                del count[ch]
+            if char_freq[ch]==1:
+                del char_freq[ch]
             else:
-                count[ch]-=1
+                char_freq[ch]-=1
             
-            while count and t_stack and min(count)>=t_stack[-1]:
-                ans.append(t_stack.pop())
+            while t_stack and char_freq and t_stack[-1]<=min(char_freq):
+                paper.append(t_stack.pop())
+
+
+        while t_stack:
+            paper.append(t_stack.pop())
         
-        while(t_stack):
-            ans.append(t_stack.pop())
-        
-        return ''.join(ans)
+        return ''.join(paper)
+
+                
+
+
+
+
 
                 
