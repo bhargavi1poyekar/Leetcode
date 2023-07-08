6f4fe9df29ab75a0f0ef00443c2d925782c2ff10
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2023-07-07 20:06:06 -0400

    Time: 116 ms (38.33%), Space: 17.6 MB (61.57%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -1,23 +1,23 @@
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
 
-        stack=[]
+        stack=[] 
 
         for mag in asteroids:
-            flag=True
-            while stack and (stack[-1]>0 and mag<0):
-
-                if abs(stack[-1])<abs(mag):
-                    stack.pop()
-                    continue
-                elif abs(stack[-1])==abs(mag):
-                    stack.pop()
-                    flag=False
+            doAppend=True # Flag to check if curr element survives collision
+            while stack and (stack[-1]>0 and mag<0): 
+                # while curr element and stack top, moving towards each other
+                if abs(stack[-1])<abs(mag): # if curr element greater
+                    stack.pop() # top gets destroyed
+                    continue # continue to check with next stack top
+                elif abs(stack[-1])==abs(mag): # if both are equal, both get destroyed
+                    stack.pop() # top is destroyed
+                    doAppend=False # and curr is destroyed so not appended
                 
-                flag=False
+                doAppend=False # if top > curr, then curr gets destroyed
                 break
                 
-            if flag==True:
+            if doAppend: # if survived collision, append it.
                 stack.append(mag)
             
         return stack
