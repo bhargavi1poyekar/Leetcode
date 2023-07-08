7de08154dac96250eebdec781d0e7e57d9535e71
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2023-07-08 15:42:31 -0400

    Time: 112 ms (64.14%), Space: 16.6 MB (48.01%) - LeetHub

--- 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.py
+++ 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.py
@@ -15,7 +15,7 @@ class Solution:
         stack=[-1]
 
         for right in range(len(nums)+1):
-            while stack[-1]!=-1 and (right==len(nums) or nums[stack[-1]]<=nums[right]):
+            while stack[-1]!=-1 and (right==len(nums) or nums[stack[-1]]<nums[right]):
                 target_min=stack.pop()
                 num_sub=(target_min-stack[-1])*(right-target_min)
                 answer+=num_sub*nums[target_min]
