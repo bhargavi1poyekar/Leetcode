62b4f620ebbb96b604244e1386227fd2e8ba2ddc
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2023-07-07 20:23:23 -0400

    Time: 1234 ms (84.87%), Space: 29.5 MB (65.68%) - LeetHub

--- 1792-find-the-most-competitive-subsequence/1792-find-the-most-competitive-subsequence.py
+++ 1792-find-the-most-competitive-subsequence/1792-find-the-most-competitive-subsequence.py
@@ -1,14 +1,14 @@
 class Solution:
     def mostCompetitive(self, nums: List[int], k: int) -> List[int]:
 
-        inc_stack=[]
-        allowed_skips=len(nums)-k
+        inc_stack=[] # keep an monotonic increasing stack
+        allowed_skips=len(nums)-k # number of val, we are allowed to skip
 
-        for i in range(len(nums)):
-            while inc_stack and allowed_skips and inc_stack[-1]>nums[i]:
-                inc_stack.pop()
+        for i in range(len(nums)): 
+            while inc_stack and allowed_skips and inc_stack[-1]>nums[i]: # while curr num is smaller
+                inc_stack.pop() # skip it and dec allowed skips
                 allowed_skips-=1
             
-            inc_stack.append(nums[i])
+            inc_stack.append(nums[i]) # append new number
         
-        return inc_stack[:k]
+        return inc_stack[:k] # return only the required array
