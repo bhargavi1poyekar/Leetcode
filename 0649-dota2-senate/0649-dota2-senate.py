8bf1a65423e38a9833196797c873e027b404ffd3
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2023-07-08 14:37:38 -0400

    Time: 59 ms (96.18%), Space: 16.8 MB (19.99%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.py
+++ 0649-dota2-senate/0649-dota2-senate.py
@@ -1,20 +1,23 @@
 class Solution:
     def predictPartyVictory(self, senate: str) -> str:
 
-        rq,dq=deque(),deque()
+        rq,dq=deque(),deque() # queues for both parties
 
+        # Adding the indexes in respective queues.
         for i in range(len(senate)):
             if senate[i]=='R':
                 rq.append(i)
             else:
                 dq.append(i)
-            
+        
+        # until one of the party is empty (all senators are banned from a party)
         while rq and dq:
-            if rq[0]<dq[0]:
-                dq.popleft()
-                rq.append(rq.popleft()+len(senate))
-            else:
-                rq.popleft()
+            if rq[0]<dq[0]: # Radiant senator turn is before dire
+                dq.popleft() # Ban dire senator
+                rq.append(rq.popleft()+len(senate)) # next turn after n 
+            else: # dire's turn
+                rq.popleft() # ban radiant
                 dq.append(dq.popleft()+len(senate))
+
 
         return 'Radiant' if rq else 'Dire'
