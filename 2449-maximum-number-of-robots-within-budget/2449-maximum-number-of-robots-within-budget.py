ff0e79a3764254e35f7f326e4f3e9e3d5537671d
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2023-07-08 15:07:32 -0400

    Time: 1340 ms (85.81%), Space: 24.3 MB (82.58%) - LeetHub

--- 2449-maximum-number-of-robots-within-budget/2449-maximum-number-of-robots-within-budget.py
+++ 2449-maximum-number-of-robots-within-budget/2449-maximum-number-of-robots-within-budget.py
@@ -1,22 +1,23 @@
 class Solution:
     def maximumRobots(self, chargeTimes: List[int], runningCosts: List[int], budget: int) -> int:
 
-        left=0
         max_charge=deque()
-        sum_run=0
+        run_sum=0
+        left=0
         max_length=0
 
         for right in range(len(chargeTimes)):
-            
+
             while max_charge and max_charge[-1]<chargeTimes[right]:
                 max_charge.pop()
-                
+            
             max_charge.append(chargeTimes[right])
 
-            sum_run+=runningCosts[right]
-            
-            while(max_charge and (max_charge[0]+(right-left+1)*sum_run)>budget):
-                sum_run-=runningCosts[left]
+            run_sum+=runningCosts[right]
+
+            while max_charge and max_charge[0]+ ((right-left+1) * run_sum)>budget:
+
+                run_sum-=runningCosts[left]
                 if max_charge[0]==chargeTimes[left]:
                     max_charge.popleft()
                 left+=1
@@ -24,9 +25,3 @@ class Solution:
             max_length=max(max_length,right-left+1)
         
         return max_length
-
-
-
-
-
-
