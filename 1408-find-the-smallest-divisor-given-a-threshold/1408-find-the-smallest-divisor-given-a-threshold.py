040b7ac7b44b33025a90ec1bf92f95589ed32b6d
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2023-07-31 17:41:44 -0400

    Time: 297 ms (99.45%), Space: 22.5 MB (32.48%) - LeetHub

--- 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.py
+++ 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.py
@@ -5,7 +5,6 @@ class Solution:
             div_sum=0
             for num in nums:
                 div_sum+=ceil(num/divisor)
-            
             return div_sum<=threshold
 
         left=1
@@ -13,14 +12,12 @@ class Solution:
 
         while left<=right:
             mid=(left+right)//2
-
             if check(mid):
                 right=mid-1
-                
             else:
                 left=mid+1
         
-        return right+1
+        return left
 
 
 
