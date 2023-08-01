62a66f4f023c3db74579f221ea3ddba5e20eeb25
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2023-08-01 11:46:54 -0400

    Time: 1099 ms (84.73%), Space: 29.9 MB (46.31%) - LeetHub

--- 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.py
+++ 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.py
@@ -5,12 +5,7 @@ class Solution:
             # print(f'max_candy:{max_candy}')
             count_children=0
             for candy in candies:
-                # print(candy>=max_candy)
-                # while candy>=max_candy:
-                #     candy-=max_candy
-                #     count_children+=1
                 count_children+=candy//max_candy
-            # print(count_children)
             return count_children>=k
         
 
