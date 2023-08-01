70e1ce6eb2c66d935e62fc27988235dc2bfda775
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2023-08-01 10:45:32 -0400

    Time: 4307 ms (29%), Space: 38.3 MB (30%) - LeetHub

---
+++ 2333-count-number-of-rectangles-containing-each-point/2333-count-number-of-rectangles-containing-each-point.py
@@ -0,0 +1,38 @@
+class Solution:
+    def countRectangles(self, rectangles: List[List[int]], points: List[List[int]]) -> List[int]:
+        
+        def binary_search(arr, target):
+            left, right = 0, len(arr) - 1
+            while left <= right:
+                mid = (left + right) // 2
+                if arr[mid] == target:
+                    return mid
+                elif arr[mid] > target:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            return left
+        
+        height_to_lengths = defaultdict(list)
+        for length, height in rectangles:
+            height_to_lengths[height].append(length)
+        
+        max_height=max(height_to_lengths.keys())
+        ans=[]
+
+        for lengths in height_to_lengths.values():
+            lengths.sort()
+        
+        print(max_height)
+        for x,y in points:
+            count=0
+            for height in range(y,max_height+1):
+                if height in height_to_lengths.keys():
+                    count+=len(height_to_lengths[height])-binary_search(height_to_lengths[height],x)
+        
+            ans.append(count)
+        return ans
+
+        
+
+        
