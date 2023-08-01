f5198b86f664b56082166c3467c945c471752970
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2023-08-01 11:02:17 -0400

    Time: 4286 ms (31%), Space: 38.6 MB (21%) - LeetHub

--- 2333-count-number-of-rectangles-containing-each-point/2333-count-number-of-rectangles-containing-each-point.py
+++ 2333-count-number-of-rectangles-containing-each-point/2333-count-number-of-rectangles-containing-each-point.py
@@ -1,6 +1,7 @@
 class Solution:
     def countRectangles(self, rectangles: List[List[int]], points: List[List[int]]) -> List[int]:
         
+        
         def binary_search(arr, target):
             left, right = 0, len(arr) - 1
             while left <= right:
@@ -17,17 +18,27 @@ class Solution:
         for length, height in rectangles:
             height_to_lengths[height].append(length)
         
+        # print(height_to_lengths)
+        
         max_height=max(height_to_lengths.keys())
         ans=[]
 
         for lengths in height_to_lengths.values():
             lengths.sort()
-        
-        print(max_height)
+
+        # print(height_to_lengths)
+        # print(max_height)
+
         for x,y in points:
             count=0
+            # print(x,y)
             for height in range(y,max_height+1):
+                # print(height)
                 if height in height_to_lengths.keys():
+                    # print(height, x)
+                    # print(len(height_to_lengths[height]))
+                    # print(f'x:{x}')
+                    # print(binary_search(height_to_lengths[height],x))
                     count+=len(height_to_lengths[height])-binary_search(height_to_lengths[height],x)
         
             ans.append(count)
