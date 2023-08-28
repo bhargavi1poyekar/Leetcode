4ffa1acedd6dda8de9ea55507423a9f4d7e5f318
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2023-08-28 12:15:14 -0400

    Time: 58 ms (84.39%), Space: 16.5 MB (45.01%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -3,22 +3,19 @@ class Solution:
 
         if len(s)==0:
             return 0
-        hash={}
+        
         left=0
+        hash_idx={}
         max_len=0
+
         for right in range(len(s)):
-            
-            if s[right] in hash:
-                max_len=max(max_len,right-left)
-                left=max(left,hash[s[right]]+1)
+            if s[right] in hash_idx:
+                left=max(left,hash_idx[s[right]])
 
-            hash[s[right]]=right
-            # print(left,right,max_len, hash)
-
-        # print(left,right,max_len, hash)
-        max_len=max(max_len,right+1-left)
+            hash_idx[s[right]]=right+1
+            max_len=max(max_len,right-left+1)
+        
         return max_len
-
         
 
         
