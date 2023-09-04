d469584272307a97b121ccf7bf588c38ae7e5f42
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2023-09-04 19:03:27 -0400

    Time: 863 ms (88.82%), Space: 21.4 MB (11.93%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -2,13 +2,14 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         
         def twoSum(nums,target):
-
+            
+            # print("hello")
             left=0
             right=len(nums)-1
-
+            
             while left<right:
                 if nums[left]+nums[right]==target:
-                    ans.append([-target,nums[left],nums[right]])
+                    ans.append([nums[left],nums[right],-target])
                     left+=1
                     right-=1
                     while left<right and nums[left]==nums[left-1]:
@@ -17,57 +18,18 @@ class Solution:
                     left+=1
                 else:
                     right-=1
-
-        ans=[]
+            
         nums.sort()
+        ans=[]
         for i in range(len(nums)):
             if nums[i]>0:
+                print("hello")
                 break
             elif i!=0 and nums[i]!=nums[i-1]:
                 twoSum(nums[i+1:],-nums[i])
             elif i==0:
                 twoSum(nums[i+1:],-nums[i])
-
+        
         return ans
 
-    #     def threeSum(self, nums: List[int]) -> List[List[int]]:
-
-    #     ans=[]
-    #     nums=sorted(nums)
-    #     for i in range(len(nums)):
-    #         if nums[i]>0:
-    #             break
-    #         elif i!=0 and nums[i]!=nums[i-1]:
-    #             ans.extend(self.twoSum(nums[i+1:len(nums)],-nums[i]))
-    #         elif i==0:
-    #             ans.extend(self.twoSum(nums[i+1:len(nums)],-nums[i]))
-
-    #     return ans
-
-
-    # def twoSum(self,nums, target):
-
-    #     left=0
-    #     right=len(nums)-1
-    #     ans=[]
-
-    #     while(left<right):
-
-    #         if nums[left]+nums[right]==target:
-    #             ans.append([-target,nums[left],nums[right]])
-    #             left+=1
-    #             right-=1
-    #             while(left<right and nums[left]==nums[left-1]):
-    #                 left+=1
-                
-    #         elif nums[left]+nums[right]>target:
-    #             right-=1
-    #         else:
-    #             left+=1
-        
-    #     return ans
-
-        
-        
-            
 
