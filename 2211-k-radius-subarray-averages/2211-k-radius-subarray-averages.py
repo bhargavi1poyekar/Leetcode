92c7dd4200abd88cbf1b1948cfc28fcb242266e7
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2023-09-04 20:10:54 -0400

    Time: 1362 ms (67.75%), Space: 34.6 MB (74.58%) - LeetHub

--- 2211-k-radius-subarray-averages/2211-k-radius-subarray-averages.py
+++ 2211-k-radius-subarray-averages/2211-k-radius-subarray-averages.py
@@ -3,17 +3,16 @@ class Solution:
 
         prefix=[]
         prefix.append(0)
-
-        num_len=2*k+1
-        avg=[-1]*len(nums)
-
         for i in range(len(nums)):
             prefix.append(prefix[i]+nums[i])
         
+        num=2*k+1
+
+        avg=[-1]*len(nums)
         for i in range(len(nums)):
             if i-k>=0 and i+k<len(nums):
-                avg[i]=(prefix[i+k+1]-prefix[i-k])//num_len
-        
+                avg[i]=int((prefix[i+k+1]-prefix[i-k])/num)
+
         return avg
 
-
+        
