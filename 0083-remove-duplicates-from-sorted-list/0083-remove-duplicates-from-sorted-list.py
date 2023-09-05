f4b70616deb87ce61602d238b5f7a79f30f322b8
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2023-09-05 13:08:37 -0400

    Time: 53 ms (38.92%), Space: 16.4 MB (46.6%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.py
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.py
@@ -8,10 +8,12 @@ class Solution:
 
         curr=head
 
-        while(curr and curr.next):
-            if curr.next.val==curr.val:
+        while curr and curr.next:
+            if curr.val==curr.next.val:
                 curr.next=curr.next.next
             else:
                 curr=curr.next
         
         return head
+
+        
