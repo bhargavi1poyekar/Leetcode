ca6631cfb0f60081402a9d5a90b0b1105bdaf36a
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2023-09-07 12:30:31 -0400

    Time: 259 ms (67.39%), Space: 19 MB (58.97%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,14 +1,13 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
 
-        hash=collections.Counter()
-        hash[0]=1
+        prefix_hash=Counter()
+        prefix_hash[0]=1
         count=0
         prefix_sum=0
-        for i in range(len(nums)):
-            prefix_sum+=nums[i]
-            count+=hash[prefix_sum-k]
-            hash[prefix_sum]+=1
+        for i in nums:
+            prefix_sum+=i
+            count+=prefix_hash[prefix_sum-k]
+            prefix_hash[prefix_sum]+=1
+        
         return count
-            
-            
