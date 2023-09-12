7972de4c7093c72e142ee4948c2adea7a0a1636e
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2023-09-12 00:01:14 -0400

    Time: 39 ms (81.18%), Space: 16.2 MB (67.08%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,23 +1,41 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
 
-        num=n
-        numsum=set()
-        
-        while(num!=1 and num not in numsum):
-            numsum.add(num)
-            digi_sq_sum=0
-            while(num!=0):
-                rem=num%10
-                digi_sq_sum+=rem*rem
-                num=num//10
-            # print(digi_sq_sum)
+        sum_hash=set()
+
+        while n!=1 and n not in sum_hash:
+            sum_hash.add(n)
+            dig_sum=0
+
+            while n!=0:
+                rem=n%10
+                dig_sum+=rem*rem
+                n=n//10
             
-            num=digi_sq_sum
+            n=dig_sum
         
-        if num==1:
+        if n==1:
             return True
         return False
-        
 
+
+    # num=n
+    #     numsum=set()
+        
+    #     while(num!=1 and num not in numsum):
+    #         numsum.add(num)
+    #         digi_sq_sum=0
+    #         while(num!=0):
+    #             rem=num%10
+    #             digi_sq_sum+=rem*rem
+    #             num=num//10
+    #         # print(digi_sq_sum)
             
+    #         num=digi_sq_sum
+        
+    #     if num==1:
+    #         return True
+    #     return False
+
+
+        
