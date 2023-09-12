ac6d4266876d5eedca0c95bee126aa38ae053872
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2023-09-12 00:01:26 -0400

    Time: 44 ms (54.93%), Space: 16.3 MB (67.08%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -17,25 +17,3 @@ class Solution:
         if n==1:
             return True
         return False
-
-
-    # num=n
-    #     numsum=set()
-        
-    #     while(num!=1 and num not in numsum):
-    #         numsum.add(num)
-    #         digi_sq_sum=0
-    #         while(num!=0):
-    #             rem=num%10
-    #             digi_sq_sum+=rem*rem
-    #             num=num//10
-    #         # print(digi_sq_sum)
-            
-    #         num=digi_sq_sum
-        
-    #     if num==1:
-    #         return True
-    #     return False
-
-
-        
