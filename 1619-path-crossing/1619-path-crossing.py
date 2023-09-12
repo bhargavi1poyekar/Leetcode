5839c9ab2ba3ea5c1bff4a7c28aa35d89692df47
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2023-09-11 23:34:09 -0400

    Time: 36 ms (81%), Space: 16.3 MB (96.67%) - LeetHub

--- 1619-path-crossing/1619-path-crossing.py
+++ 1619-path-crossing/1619-path-crossing.py
@@ -1,23 +1,45 @@
 class Solution:
     def isPathCrossing(self, path: str) -> bool:
 
+        visited=set()
         x=0
         y=0
-        coord={}
-        coord[(0,0)]=1
-
-        for i in path:
-            if i=='N':
-                y+=1
-            elif i=='S':
+        visited.add((0,0))
+        for i in range(len(path)):
+            if path[i]=='N':
+                y+=1                
+            elif path[i]=='S':
                 y-=1
-            elif i=='E':
+            elif path[i]=='E':
                 x+=1
             else:
                 x-=1
-            # print(x,y,coord)
-            if (x,y) in coord:
+
+            if (x,y) in visited:
                 return True
-            else: coord[(x,y)]=1
+            else:
+                visited.add((x,y))
         
         return False
+    
+# x=0
+#         y=0
+#         coord={}
+#         coord[(0,0)]=1
+
+#         for i in path:
+#             if i=='N':
+#                 y+=1
+#             elif i=='S':
+#                 y-=1
+#             elif i=='E':
+#                 x+=1
+#             else:
+#                 x-=1
+#             # print(x,y,coord)
+#             if (x,y) in coord:
+#                 return True
+#             else: coord[(x,y)]=1
+        
+#         return False
+        
