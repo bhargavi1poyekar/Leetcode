029ed9f709e275a7b981e8aa49bf34073145946a
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2023-09-12 23:44:05 -0400

    Time: 48 ms (10.33%), Space: 16.4 MB (37.58%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.py
+++ 0290-word-pattern/0290-word-pattern.py
@@ -1,20 +1,23 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
 
+        s=s.split(' ')
         
-        s_list=s.split(' ')
-        if len(pattern)!=len(s_list):
+        pattern_map=Counter()
+        s_map=Counter()
+
+        if len(pattern)!=len(s):
             return False
-        
-        pattern_map={}
-        s_map={}
 
         for i in range(len(pattern)):
-            if pattern[i] not in pattern_map and s_list[i] not in s_map:
-                pattern_map[pattern[i]]=s_list[i]
-                s_map[s_list[i]]=pattern[i]
-
-            elif  pattern_map.get(pattern[i])!=s_list[i] or s_map.get(s_list[i])!=pattern[i]:
-                return False
+            if pattern[i] not in pattern_map and s[i] not in s_map:
+                pattern_map[pattern[i]]=s[i]
+                s_map[s[i]]=pattern[i]
             
+            elif pattern_map.get(pattern[i])!=s[i] or s_map.get(s[i])!=pattern[i]:
+                return False
+        
         return True
+
+
+        
