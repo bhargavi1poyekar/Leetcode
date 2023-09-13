bf1b0f2a816a40c8e5f43aea6b928dd4ff26f5d9
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2023-09-12 23:56:50 -0400

    Time: 98 ms (82.53%), Space: 16.1 MB (96.9%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,38 +1,33 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
 
-        n=9
-        rows=[set() for i in range(n)]
-        cols=[set() for i in range(n)]
-        boxes=[set() for i in range(n)]
+        rows_hash=[set() for i in range(9)]
+        cols_hash=[set() for i in range(9)]
+        boxes_hash=[set() for i in range(9)]
 
-        for r in range(9):
-            for c in range(9):
-                val=board[r][c]
-
+        for row in range(len(board)):
+            for col in range(len(board[0])):
+                val=board[row][col]
                 if val=='.':
                     continue
-                
-                if val in rows[r]:
-                    return False
-                
-                rows[r].add(val)
 
-                if val in cols[c]:
+                if val in rows_hash[row]:
                     return False
                 
-                cols[c].add(val)
-
-                box_idx=(r//3)*3+(c//3)
+                rows_hash[row].add(val)
                 
-                if val in boxes[box_idx]:
+                if val in cols_hash[col]:
                     return False
                 
-                boxes[box_idx].add(val)
+                cols_hash[col].add(val)
+                
+                box_idx=(row//3)*3+(col//3)
+                if val in boxes_hash[box_idx]:
+                    return False
+                
+                boxes_hash[box_idx].add(val)
         
         return True
 
 
-
-
-
+        
