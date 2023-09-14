98bdcee24093d97b2fab28a941b0468464141532
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2023-09-14 13:57:41 -0400

    Time: 1017 ms (97.37%), Space: 30.2 MB (92.5%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,15 +1,16 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
 
-        stack_index=[]
-        future_day=[0 for i in range(len(temperatures))]
-
+        stack=[]
+        warmer_idx=[0]*len(temperatures) 
         for i in range(len(temperatures)):
-
-            while(stack_index and temperatures[stack_index[-1]]<temperatures[i]):
-                idx=stack_index.pop()
-                future_day[idx]=i-idx
             
-            stack_index.append(i)
+            while stack and temperatures[stack[-1]]<temperatures[i]:
+                idx=stack.pop()
+                warmer_idx[idx]=i-idx
+            
+            stack.append(i)
         
-        return(future_day)
+        return warmer_idx
+
+        
