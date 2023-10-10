a41e315fd766bc4b3e447aa4328919a32652ff64
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2023-10-10 11:54:58 -0400

    Time: 50 ms (97.26%), Space: 18.7 MB (10.95%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
@@ -3,13 +3,17 @@ class Solution:
 
         def dp(i):
 
-            if i<2:
+            if i<=1:
                 return 0
+            
             if i in memo:
                 return memo[i]
             
-            memo[i]=min(cost[i-1]+dp(i-1),cost[i-2]+dp(i-2))
+            memo[i]=min(dp(i-1)+cost[i-1], dp(i-2)+cost[i-2])
             return memo[i]
-
+        
         memo={}
         return dp(len(cost))
+        
+
+        
