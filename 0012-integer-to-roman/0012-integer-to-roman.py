467bf5b89bef94f6a204f7110c48787e777f608d
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2023-11-10 17:32:55 -0500

    Time: 98 ms (5.11%), Space: 13.2 MB (62.11%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -1,36 +1,39 @@
-class Solution:
-    def intToRoman(self, num: int) -> str:
-        roman_to_int={
-            1:'I',
-            4:'IV',
-            5:'V',
-            9:'IX',
-            10:'X',
-            40:'XL',
-            50:"L",
-            90:'XC',
-            100:'C',
-            400:'CD',
-            500:'D',
-            900:'CM',
-            1000:'M',
-        }
-
-        int_num=[1000,900,500,400,100,90,50,40,10,9,5,4,1]
-        roman=''
-        i=0
-        while i<len(int_num):
-            # print(roman)
-            if num==0:
-                break
-            elif num>=int_num[i]:
-                roman+=roman_to_int[int_num[i]]
-                num-=int_num[i]
-                i-=1
-            i+=1
+class Solution(object):
+    def intToRoman(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
         
-        return(roman)
-
-
-
-
+        romanint=  {1:'I',
+                     4:'IV',
+                     5:'V',
+                     9:'IX',
+                     10:'X',
+                     40:'XL',
+                     50:'L',
+                     90:'XC',
+                     100:'C',
+                     400:'CD',
+                     500:'D',
+                     900:'CM',
+                     1000:'M'}
+        
+        rom=[]
+        numerals=[1000,900,500,400,100,90,50,40,10,9,5,4,1]
+        rem=num
+        i=0
+        while i<len(numerals):
+            print(i)
+            if rem==0:
+                break
+            if rem>=numerals[i]:
+                rom.append(romanint.get(numerals[i]))
+                rem=rem-numerals[i]
+                i=i-1
+            i=i+1        
+                
+        
+        return(''.join(rom))
+                    
+    
