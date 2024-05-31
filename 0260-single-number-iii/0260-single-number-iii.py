bc7cee0050f0eba42a60e40a58accee1fd45ab27
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2024-05-31 12:44:29 -0400

    Time: 48 ms (95.4%), Space: 18.6 MB (41.53%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.py
@@ -0,0 +1,13 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+
+        freq = defaultdict(int)  # number -> appear_number
+        for n in nums:
+            freq[n] += 1
+
+        res: list[int] = []
+        for num, f in freq.items():
+            if f == 1:
+                res.append(num)
+
+        return res
