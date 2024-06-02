c2995e90d4ffc5bcfc6c3d7ca5492d3a307002a3
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2024-06-02 19:59:25 -0400

    Time: 456 ms (58.49%), Space: 19.4 MB (92.71%) - LeetHub

--- 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.py
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.py
@@ -1,23 +1,24 @@
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
 
-        left=0
-        num_0=0
-
-        max_len=0
+        left = 0
+        max_length = 0
+        delete = 0
 
         for right in range(len(nums)):
+            if nums[right] == 0:
+                delete += 1
+            
+            while delete > 1:
+                if nums[left] == 0:
+                    delete -= 1
 
-            if nums[right]==0:
-                num_0+=1
-            
-            while num_0>1:
-                if nums[left]==0:
-                    num_0-=1
-                left+=1
-            
-            max_len=max(max_len,right-left)
+                left += 1
+
+            max_length = max(max_length, right - left)
+
+        return max_length 
+
         
-        return max_len
-
-
+        
+        
