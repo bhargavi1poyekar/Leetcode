3dfb8ccce850e76929a2369e97ec010fd8294130
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2024-06-02 20:09:34 -0400

    Time: 35 ms (90.75%), Space: 16.6 MB (72.23%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.py
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.py
@@ -1,9 +1,19 @@
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
-
-        arr_count=Counter(arr)
-
-        unique=len(set(arr_count.values()))
-
-        return unique==len(arr_count)
         
+        freq_map = Counter(arr)
+
+        count_freq = {}
+
+        for key in freq_map:
+            if freq_map[key] not in count_freq:
+                count_freq[freq_map[key]] = 1
+            else:
+                count_freq[freq_map[key]] += 1
+
+        
+        for key in count_freq:
+            if count_freq[key]!=1:
+                return False
+        
+        return True
