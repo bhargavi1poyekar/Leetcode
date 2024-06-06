2dec0ccd58164b082cd750b9b368702feb8d8eb1
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2024-06-06 15:43:12 -0400

    Time: 52 ms (72.41%), Space: 20.5 MB (33.7%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -1,25 +1,45 @@
 class MinStack:
 
     def __init__(self):
-        self.stack=[]
-        
+        self.stack = []
+        self.min_stack = []
+
     def push(self, val: int) -> None:
-        if not self.stack:
-            self.stack.append([val,val])
+        if self.min_stack:
+            curr_min = min(self.min_stack[-1], val)
         else:
-            min_elem=min(val,self.stack[-1][1])
-            self.stack.append([val,min_elem])
+            curr_min = val
+
+        self.stack.append(val)
+        self.min_stack.append(curr_min) 
 
     def pop(self) -> None:
         self.stack.pop()
+        self.min_stack.pop()
 
     def top(self) -> int:
-        return self.stack[-1][0]
+        return self.stack[-1]
+        
 
     def getMin(self) -> int:
-        return self.stack[-1][1]
-        
-        
+        return self.min_stack[-1]
+
+'''        
+min = [-2, -2, -3]
+stack =[-2, 0, -3]
+
+[[-2, -2], [0, -2], [-3, -3]]
+
+Plan:
+
+1. initialize a stack 
+2. push -> append in 2 arrays -> min, and stack
+3. Pop -> remove the top element
+4. Top -> check stack[-1]
+5. Set min[-1]
+
+
+'''
 
 
 # Your MinStack object will be instantiated and called as such:
