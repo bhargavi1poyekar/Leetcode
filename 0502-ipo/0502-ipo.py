e4ce35ef8ec05187495159f007250805afa027dc
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2024-06-15 11:57:08 -0400

    Time: 686 ms (84.54%), Space: 41.3 MB (55.37%) - LeetHub

--- 0502-ipo/0502-ipo.py
+++ 0502-ipo/0502-ipo.py
@@ -1,6 +1,5 @@
 class Solution:
     def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
-
         num_proj=len(profits)
         projects=list(zip(capital,profits))
         projects.sort() # Sorts by capital
@@ -19,5 +18,3 @@ class Solution:
                 w+=-heappop(max_heap)
         
         return w
-
-
