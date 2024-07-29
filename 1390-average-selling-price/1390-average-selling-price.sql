fd7fe8d55f32503365dd39f3b3d4b0bc9fe81dc4
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2024-07-29 18:45:57 -0400

    Time: 646 ms (79.64%), Space: 0B (100%) - LeetHub

--- 1390-average-selling-price/1390-average-selling-price.sql
+++ 1390-average-selling-price/1390-average-selling-price.sql
@@ -1,13 +1,6 @@
 # Write your MySQL query statement below
 
--- Select d.product_id, round((sum(price*units)+0.00)/(sum(units)+0.00),2) as average_price
--- from(
--- Select *
--- from prices p
--- natural join 
--- unitssold u
--- where u.purchase_date between p.start_date and p.end_date) d
--- group by d.product_id 
+
 
 SELECT p.product_id, IFNULL(ROUND(SUM(units*price)/SUM(units),2),0) AS average_price
 FROM Prices p LEFT JOIN UnitsSold u
