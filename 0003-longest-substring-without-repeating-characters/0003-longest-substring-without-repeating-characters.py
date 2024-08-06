7ecf34d0403f10ed784af2784c301706d44f1c14
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2024-08-06 18:29:43 -0400

    Time: 61 ms (36.33%), Space: 16.7 MB (34.82%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,19 +1,29 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-
-        if len(s)<=1:
-            return len(s)
         
-        max_len=0
-        hash={}
-        left=0
+        left =  0
+        longest_length = 0
+        hash_index = {}
+
         for right in range(len(s)):
-            if s[right] in hash:
-                left=max(left,hash[s[right]])
-
-            max_len=max(max_len,right-left+1)
-            hash[s[right]]=right+1
+            if s[right] in hash_index:
+                left = max(left, hash_index[s[right]])
+            
+            longest_length = max(longest_length, right-left+1) 
+            hash_index[s[right]] = right + 1 
         
-        # max_len=max(max_len,right-left)
-        return max_len
+        return longest_length
+
+        # max_length = 0
+        # left = 0
+        # hash = {}
+        # for right in range(len(s)):
+
+        #     if s[right] in hash and hash[s[right]]>=left:
+        #         left = max(left, hash[s[right]])
+            
+        #     hash[s[right]] = right+1
+        #     max_length = max(max_length, right-left+1)
+
+        # return max_length
             
