0cb5b3fcdfa1089852d8cca95f003a7f4ffaece0
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2024-10-23 00:06:29 -0400

    Time: 7 ms (96.46%), Space: 17.8 MB (93.55%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -6,11 +6,13 @@ class Solution:
         
         slow = 0
         fast = 0
-
-        while fast < len(nums):
-            if nums[fast] != 0:
-                nums[slow] = nums[fast]
+        n = len(nums)
+        while slow<n and fast<n:
+            if nums[fast] == 0:
+                fast += 1
+            else:
+                nums[slow], nums[fast] = nums[fast], nums[slow]
                 slow += 1
-            fast += 1
+                fast += 1
         
-        nums[slow:] = [0] * (fast - slow)
+        return nums
