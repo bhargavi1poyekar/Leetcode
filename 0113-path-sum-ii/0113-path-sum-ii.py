26a366e9798dbb03c4292281d529aa22c92a716a
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2024-10-24 04:26:26 -0400

    Time: 0 ms (100%), Space: 17.8 MB (33.88%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.py
+++ 0113-path-sum-ii/0113-path-sum-ii.py
@@ -6,39 +6,21 @@
 #         self.right = right
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
-
-        valid_paths=[]
-       
-
-        def dfs(root,curr_path, remain_sum):
-
-            if not root: # If no root return
-                return
+        
+        valid_paths = [] 
+        def path_sum(root, curr_path, remain_sum):
+            if not root:
+                return 
             
-            curr_path.append(root.val) # add current node in curr_path
+            curr_path.append(root.val)
+            if not root.left and not root.right:
+                if remain_sum == root.val:
+                    valid_paths.append(list(curr_path))
             
-            # if curr node is equal to remain sum for target, and if lead node
-            if root.val==remain_sum and not root.left and not root.right: 
-                valid_paths.append(list(curr_path)) # append curr path to valid paths
-                
-            else:
-                # if not leaf node, go to left or right
-                dfs(root.left,curr_path,remain_sum-root.val) 
-                dfs(root.right,curr_path,remain_sum-root.val)
+            path_sum(root.left, curr_path, remain_sum - root.val)
+            path_sum(root.right, curr_path, remain_sum - root.val)
 
-            # Remove curr node from curr_path
             curr_path.pop()
-            
         
-       
-        dfs(root,[],targetSum)
+        path_sum(root, [], targetSum)
         return valid_paths
-
-        
-
-        
-    
-
-
-
-
