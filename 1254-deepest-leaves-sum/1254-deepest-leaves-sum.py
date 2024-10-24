145b9f7431819f7b2d54006ee387e5a90975efe7
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2024-10-24 03:15:37 -0400

    Time: 15 ms (96.36%), Space: 19.9 MB (7.07%) - LeetHub

--- 1254-deepest-leaves-sum/1254-deepest-leaves-sum.py
+++ 1254-deepest-leaves-sum/1254-deepest-leaves-sum.py
@@ -6,20 +6,21 @@
 #         self.right = right
 class Solution:
     def deepestLeavesSum(self, root: Optional[TreeNode]) -> int:
-        if not root:
-            return 0
 
-        queue=deque([root])
-        ans=[]
-        while queue:
-            length=len(queue)
-            level=[]
-            for _ in range(length):
-                node=queue.popleft()
+        if not root:
+            return []
+
+        Q =  deque([root])
+        # deep_leaves = []
+
+        while Q:
+            level = []
+            for i in range(len(Q)):
+                node =  Q.popleft()
                 level.append(node.val)
                 if node.left:
-                    queue.append(node.left)
+                    Q.append(node.left)
                 if node.right:
-                    queue.append(node.right)
-            ans=level
-        return sum(ans)
+                    Q.append(node.right)
+
+        return sum(level)
