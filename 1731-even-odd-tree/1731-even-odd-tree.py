fe159ea924669ac05870e16d41a4493e69132478
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2024-10-24 04:16:46 -0400

    Time: 535 ms (5.01%), Space: 43.3 MB (24.28%) - LeetHub

--- 1731-even-odd-tree/1731-even-odd-tree.py
+++ 1731-even-odd-tree/1731-even-odd-tree.py
@@ -6,34 +6,35 @@
 #         self.right = right
 class Solution:
     def isEvenOddTree(self, root: Optional[TreeNode]) -> bool:
+        
         queue=deque([root])
         level=0
 
         while queue:
-            
-            size=len(queue)
-            if level%2==1:
-                queue.append(TreeNode(float('-inf')))
-            else:
+            length=len(queue)
+            if level%2==0:
                 queue.append(TreeNode(float('inf')))
-           
-            for _ in range(size):
+            else:
+                queue.append(TreeNode(float('-inf')))
+            
+            for _ in range(length):
                 node=queue.popleft()
-
-                if level%2!=0:
-                    if (node.val)%2!=0 or node.val<=queue[0].val:
-                        return False 
-
-                if level%2==0:
-                    if (node.val)%2==0 or node.val>=queue[0].val:
-                        return False 
+                # print(level,node.val,queue[0].val)
+                if level%2==0: 
+                    if node.val%2!=1 or node.val>=queue[0].val:
+                        return False
+                elif level%2==1: 
+                    if node.val%2!=0 or node.val<=queue[0].val:
+                        return False
                 
                 if node.left:
                     queue.append(node.left)
                 if node.right:
                     queue.append(node.right)
-
-            queue.popleft()
+                
             level+=1
-            
+            queue.popleft()
+        
         return True
+                    
+
