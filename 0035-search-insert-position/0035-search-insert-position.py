18d4f4cdf76cec2c8635cd6204411d35fa866fdb
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2024-10-25 08:24:23 -0400

    Time: 0 ms (100%), Space: 17.2 MB (94.8%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -6,10 +6,10 @@ class Solution:
         while left < right:
 
             mid = (left + right) // 2
-            if nums[mid] >= target:
-                right = mid
-            else:
+            if nums[mid] < target:
                 left = mid + 1
+            else:
+                right = mid
         
         return left
              
