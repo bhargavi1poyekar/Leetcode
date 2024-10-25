fb4206494d510794018f1abf43c964fbf23e7bd5
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2024-10-25 08:23:59 -0400

    Time: 0 ms (100%), Space: 17.2 MB (94.8%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,13 +1,15 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums)
 
-        left,right=0,len(nums)
+        while left < right:
 
-        while left<right:
-            mid=(left+right)//2
-            if nums[mid]>=target:
-                right=mid
+            mid = (left + right) // 2
+            if nums[mid] >= target:
+                right = mid
             else:
-                left=mid+1
+                left = mid + 1
         
         return left
+             
