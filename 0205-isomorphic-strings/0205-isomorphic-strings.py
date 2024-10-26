babfd8e8c27f172f85b04ae0be4606107544d8fd
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2024-10-26 04:57:23 -0400

    Time: 7 ms (95.42%), Space: 16.5 MB (99.95%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,16 +1,17 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
+        if len(s)!= len(t):
+            return False
 
-        s_map=Counter()
-        t_map=Counter()
+        s_map = defaultdict()
+        t_map = defaultdict()
 
         for i in range(len(s)):
-            
             if s[i] not in s_map and t[i] not in t_map:
-                s_map[s[i]]=t[i]
-                t_map[t[i]]=s[i]
+                s_map[s[i]] = t[i]
+                t_map[t[i]] = s[i]
             
-            elif s_map.get(s[i])!=t[i] or t_map.get(t[i])!=s[i]:
+            elif s_map.get(s[i]) != t[i] or t_map.get(t[i]) != s[i]:
                 return False
         
         return True
