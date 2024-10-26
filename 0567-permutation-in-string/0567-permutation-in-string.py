ab386268fb33d257c4bf17704d10b6dee250bb2f
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2024-10-26 04:40:10 -0400

    Time: 19 ms (98.43%), Space: 16.7 MB (35.41%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,45 +1,48 @@
-from collections import Counter
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s2) < len(s1):
+            return False
         
-        if len(s1)>len(s2):return False
-    
-        s1_count=[0 for i in range(26)]
-        s2_count=[0 for i in range(26)]
+        match_count = 0
+        left = 0
+
+        s1_hash = [0 for i in range(26)]
+        s2_hash = [0 for i in range(26)]
 
         for i in range(len(s1)):
-            s1_count[ord(s1[i])-ord('a')]+=1
-            s2_count[ord(s2[i])-ord('a')]+=1
-
-        match_count=0
+            s1_hash[ord(s1[i]) - ord('a')] += 1
+            s2_hash[ord(s2[i]) - ord('a')] += 1
+        
+        match_count = 0
 
         for i in range(26):
-            if s1_count[i]==s2_count[i]:
-                match_count+=1
-        
-        left=0
-        for right in range(len(s1),len(s2)):
-            if match_count==26:
+            if s1_hash[i] == s2_hash[i]:
+                match_count += 1
+            
+        for right in range(len(s1), len(s2)):
+            if match_count == 26:
                 return True
 
-            idx=ord(s2[right])-ord('a')
-            s2_count[idx]+=1
-            if s2_count[idx]==s1_count[idx]:
-                match_count+=1
-            elif s2_count[idx]-1==s1_count[idx]:
-                match_count-=1
+            right_idx = ord(s2[right]) - ord('a')
+            s2_hash[right_idx] += 1
             
-            idx=ord(s2[left])-ord('a')
-            s2_count[idx]-=1
+            if s2_hash[right_idx] == s1_hash[right_idx]:
+                match_count += 1
+            elif s2_hash[right_idx] - 1 == s1_hash[right_idx]:
+                match_count -= 1
+            
+            left_idx = ord(s2[left]) - ord('a')
+            s2_hash[left_idx] -= 1
 
-            if s2_count[idx]==s1_count[idx]:
-                match_count+=1
-            elif s2_count[idx]+1==s1_count[idx]:
-                match_count-=1
+            if s2_hash[left_idx] == s1_hash[left_idx]:
+                match_count += 1
+            elif s2_hash[left_idx] + 1 == s1_hash[left_idx]:
+                match_count -= 1
             
-            left+=1
+            left += 1
         
-        return match_count==26
+        return match_count == 26
+            
 
             
             
