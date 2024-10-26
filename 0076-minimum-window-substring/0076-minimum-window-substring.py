a72f45d0afda67a4badd225821733e5ff301b7bb
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2024-10-26 01:33:03 -0400

    Time: 96 ms (82.55%), Space: 17 MB (93.88%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,42 +1,35 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-
-        if len(t)>len(s): return ""
-
+        
+        min_length = float('inf')
+        min_right = 0
+        min_left = 0
+        win_freq = Counter()
         t_freq = Counter(t)
         distinct_t = len(t_freq)
+        match_count = 0
 
-        win_freq = Counter()
-
-        min_left = 0
-        min_right = 0
-
-        match = 0
         left = 0
-        min_length = float('inf')
-        
 
         for right in range(len(s)):
             win_freq[s[right]] += 1
-
             if t_freq[s[right]] == win_freq[s[right]]:
-                match += 1
+                match_count += 1
             
-            while distinct_t == match:
-                if right - left + 1 < min_length:
+            while match_count == distinct_t:
+                if min_length > right - left + 1:
+                    min_length = right - left + 1
                     min_left = left
                     min_right = right
-                    min_length = right - left + 1
-
+                
                 if t_freq[s[left]] == win_freq[s[left]]:
-                    match -= 1 
+                    match_count -= 1
                 
                 win_freq[s[left]] -= 1
                 left += 1
         
-        return s[min_left:min_right+1] if min_length != float('inf') else ""
+        if min_length == float('inf'): return ''
+        else:
+            return s[min_left: min_right + 1]
+                
 
-
-
-
-        
