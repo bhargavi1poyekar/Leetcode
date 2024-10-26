cb64f023bd7bd83c337b77eec4a6232f2787ecb0
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2024-10-26 05:13:34 -0400

    Time: 0 ms (100%), Space: 16.8 MB (14.75%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,19 +1,25 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-
-        sum_hash=set()
-
-        while n!=1 and n not in sum_hash:
-            sum_hash.add(n)
-            dig_sum=0
-
-            while n!=0:
-                rem=n%10
-                dig_sum+=rem*rem
-                n=n//10
-            
-            n=dig_sum
         
-        if n==1:
-            return True
-        return False
+
+        def square_sum(num):
+            ans = 0
+            while num:
+                quot = num % 10
+                ans += quot ** 2
+                num = num // 10
+            
+            return ans
+
+        cycle_check = set()
+        num = n
+        while num != 1:
+            if num in cycle_check:
+                return False
+            else:
+                cycle_check.add(num)
+                num = square_sum(num)
+                
+
+        return True
+    
