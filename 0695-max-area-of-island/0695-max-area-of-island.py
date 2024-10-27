ed6cc9c52af42aaf48e767d915128cd2ef2df2db
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2024-10-26 23:41:51 -0400

    Time: 31 ms (36.12%), Space: 17.3 MB (45.31%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,27 +1,23 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-
-        def valid(row,col):
-            return 0<=row<m and 0<=col<n and grid[row][col]==1
-
-        directions=[[0,1],[1,0],[0,-1],[-1,0]]
-
-        def dfs(row,col):
-
-            if valid(row,col):
-                grid[row][col]=0
-                return 1+dfs(row,col+1)+dfs(row+1,col)+dfs(row-1,col)+dfs(row,col-1)
-            return 0
-
-        seen=set()
-        m=len(grid)
-        n=len(grid[0])
-        max_area=0
-
-        for i in range(m):
-            for j in range(n):
-                max_area=max(max_area,dfs(i,j))
-            
-        return max_area
         
+        def valid(row, col):
+            return 0 <= row < Row and 0 <= col < Col and grid[row][col] == 1
+        
+        # directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
+        Row = len(grid)
+        Col = len(grid[0])
+
+        def dfs(row, col):
+            if valid(row, col):
+                grid[row][col] = 0
+                return 1 + dfs(row, col+1) + dfs(row + 1, col) + dfs(row, col-1) + dfs(row-1, col)
+            return 0
+        
+        max_area = 0
+        for i in range(Row):
+            for j in range(Col):
+                max_area = max(max_area, dfs(i, j))
+        
+        return max_area
