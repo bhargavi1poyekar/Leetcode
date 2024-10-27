95728070928181f7996ab884df7e3bd826e94746
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2024-10-26 23:28:39 -0400

    Time: 10 ms (37.19%), Space: 20.9 MB (21.57%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -3,44 +3,43 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        def valid(row,col):
-            return 0<=row<m and 0<=col<n and board[row][col]=='O'
+        def isValid(row, col):
+            return 0 <= row < Row and 0 <= col < Col and board[row][col] == 'O'
         
-        def dfs(row,col):
-            seen.add((row,col))
-            for x,y in directions:
-                nextr,nextc=row+x,col+y
-                if valid(nextr,nextc) and (nextr,nextc) not in seen:
-                    dfs(nextr,nextc)
+        def dfs(row, col):
+            seen.add((row, col))
+            for dx, dy in nghbrs:
+                nextr, nextc = row + dx, col + dy
+                if isValid(nextr, nextc) and (nextr, nextc) not in seen:
+                    dfs(nextr, nextc)
 
-        directions=[(0,1),(1,0),(-1,0),(0,-1)]
+        nghbrs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
-        seen=set()
+        Row = len(board)
+        Col = len(board[0])
 
-        m=len(board)
-        n=len(board[0])
+        seen = set()
 
-        for row in range(m):
-           
-            if board[row][0]=='O':
-                dfs(row,0)
-            
-            if board[row][n-1]=='O':
-                dfs(row,n-1)
+        for row in range(Row):
+            if board[row][0] == 'O':
+                dfs(row, 0)
+            if board[row][Col - 1] == "O":
+                dfs(row, Col - 1)
         
-        for col in range(n):
-            if board[0][col]=='O':
-                dfs(0,col)
-            if board[m-1][col]=='O':
-                # print(board[m-1][col])
-                dfs(m-1,col)
+        for col in range(Col):
+            if board[0][col] == 'O':
+                dfs(0, col)
+            if board[Row - 1][col] == "O":
+                dfs(Row - 1, col)
         
-        for row in range(1,m-1):
-            for col in range(1,n-1):
-                if board[row][col]=='O' and (row,col) not in seen:
-                    board[row][col]='X'
+        for row in range(1, Row-1 ):
+            for col in range(1, Col-1):
+                if board[row][col] == 'O' and (row, col) not in seen:
+                    board[row][col] = 'X'
         
         return board
+
         
 
+        
 
