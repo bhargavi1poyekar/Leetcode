14dbdf0935ac640e8938bcb5f966cdacb3441354
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2024-10-28 10:05:24 -0400

    Time: 5 ms (26.45%), Space: 19.1 MB (5.68%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
@@ -1,26 +1,26 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
 
-        # def dp(i):
+        def dp(i):
 
-        #     if i<=1:
-        #         return 0
+            if i<=1:
+                return 0
             
-        #     if i in memo:
-        #         return memo[i]
+            if i in memo:
+                return memo[i]
             
-        #     memo[i]=min(dp(i-1)+cost[i-1], dp(i-2)+cost[i-2])
-        #     return memo[i]
+            memo[i]=min(dp(i-1)+cost[i-1], dp(i-2)+cost[i-2])
+            return memo[i]
         
-        # memo={}
-        # return dp(len(cost))
+        memo={}
+        return dp(len(cost))
 
-        n=len(cost)
+        # n=len(cost)
 
-        dp=[0]*(n+1)
+        # dp=[0]*(n+1)
 
-        for i in range(2,n+1):
-            dp[i]=min(dp[i-1]+cost[i-1],dp[i-2]+cost[i-2])
+        # for i in range(2,n+1):
+        #     dp[i]=min(dp[i-1]+cost[i-1],dp[i-2]+cost[i-2])
         
-        return dp[n]
+        # return dp[n]
         
