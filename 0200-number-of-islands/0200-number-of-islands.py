0c186bf0d7c171227a2f40802f172494d7cc9e74
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2024-12-05 10:12:53 +0530

    Time: 293 ms (16.61%), Space: 27.8 MB (9.84%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,30 +1,33 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         
-        num_islands = 0
-
         Row = len(grid)
         Col = len(grid[0])
-        seen = set()
 
-        def isvalid(row, col):
+        def isValid(row, col):
             return 0 <= row < Row and 0 <= col < Col and grid[row][col] == '1'
 
-        nghbrs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        seen = set()
+
+        nghbrs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
         def dfs(row, col):
+
             for dx, dy in nghbrs:
-                next_r, next_c = row + dx, col + dy
-                if isvalid(next_r, next_c) and (next_r, next_c) not in seen:
-                    seen.add((next_r, next_c))
-                    dfs(next_r, next_c)
+                nextr, nextc = row + dx, col + dy
+                if isValid(nextr, nextc) and (nextr, nextc) not in seen:
+                    seen.add((nextr, nextc))
+                    dfs(nextr, nextc)
+            
+            return 
+
+        num_islands = 0
 
         for row in range(Row):
             for col in range(Col):
-                if isvalid(row, col) and (row, col) not in seen:
+                if isValid(row, col) and (row, col) not in seen:
                     num_islands += 1
                     seen.add((row, col))
                     dfs(row, col)
-            
+        
         return num_islands
-
