8180110965159b90ea63a0b6887953b202a13073
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-01-05 13:55:21 +0530

    Time: 58 ms (72.04%), Space: 41.4 MB (20.36%) - LeetHub

--- 2465-shifting-letters-ii/2465-shifting-letters-ii.py
+++ 2465-shifting-letters-ii/2465-shifting-letters-ii.py
@@ -2,18 +2,18 @@ class Solution:
     def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
         
         n = len(s)
-        shift = [0] * (n + 1)
+        diff_arr = [0] * (n + 1)
 
         for shiftOp in shifts:
             start, end, direction = shiftOp
-            shift[start] += (1 if direction == 1 else -1)
+            diff_arr[start] += (1 if direction == 1 else -1)
             if end + 1 < n:
-                shift[end + 1] -= (1 if direction == 1 else -1)
+                diff_arr[end + 1] -= (1 if direction == 1 else -1)
 
         currentShift = 0
         shiftList = list(s)
         for i in range(n):
-            currentShift += shift[i]
+            currentShift += diff_arr[i]
             netShift = (currentShift % 26 + 26) % 26
             shiftList[i] = chr((ord(shiftList[i]) - ord('a') + netShift) % 26 + ord('a'))
 
