7aee70467b28ca9307ee0e0f14ea5e9b589aa08a
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-01-31 19:52:48 -0500

    Time: 117 ms (42.08%), Space: 40.7 MB (20.33%) - LeetHub

--- 2196-reverse-nodes-in-even-length-groups/2196-reverse-nodes-in-even-length-groups.py
+++ 2196-reverse-nodes-in-even-length-groups/2196-reverse-nodes-in-even-length-groups.py
@@ -6,44 +6,46 @@
 class Solution:
     def reverseEvenLengthGroups(self, head: Optional[ListNode]) -> Optional[ListNode]:
 
-        if not head or not head.next:
-            return head
+        if not head and head.next:
+            return None
         
-        grp=2
-        node_count=0
-        count_ptr=head.next
-        curr=head.next
         prev=head
+        curr=head.next
 
-        while(curr):
-            while(count_ptr and node_count<grp):
-                count_ptr=count_ptr.next
-                node_count+=1
-            print(node_count)
-            
-            if node_count%2==0:
-                con,tail=prev,curr
-                while(node_count and curr):
+        grp=2
+        grp_len=0
+        grp_ptr=head.next
+    
+        while curr:
+            while grp_ptr and grp_len<grp:
+                grp_len+=1
+                grp_ptr=grp_ptr.next
+
+            if grp_len%2==0:
+                con=prev
+                tail=curr
+                while grp_len>0:
                     nn=curr.next
                     curr.next=prev
                     prev=curr
                     curr=nn
-                    node_count-=1
+                    grp_len-=1
                 
                 con.next=prev
                 tail.next=curr
                 prev=tail
             
             else:
-                curr=count_ptr
+                curr=grp_ptr
                 while(prev.next!=curr):
                     prev=prev.next
-                    
-            node_count=0
+            
+            grp_len=0
             grp+=1
         
         return head
 
+                 
 
 
-
+        
