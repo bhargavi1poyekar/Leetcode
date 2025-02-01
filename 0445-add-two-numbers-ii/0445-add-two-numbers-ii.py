fbceac82dfcee3d2e2e4d1f92c64098d1d963cda
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-01-31 19:18:52 -0500

    Time: 3 ms (84.68%), Space: 18 MB (12.63%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.py
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.py
@@ -6,44 +6,40 @@
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
 
-        def reverseLists(head):
+        def reverseList(head):
 
-            prev=None
-            curr=head
-
+            curr = head
+            prev = None
             while curr:
-                nn=curr.next
-                curr.next=prev
-                prev=curr
-                curr=nn
-            
+                nn = curr.next
+                curr.next = prev
+                prev = curr
+                curr = nn
             return prev
+
+        l1_reverse = reverseList(l1)
+        l2_reverse = reverseList(l2)
+
+        carry = 0
+        newlist = newhead = ListNode()
+        while l1_reverse or l2_reverse or carry:
+            num1 = l1_reverse.val if l1_reverse else 0
+            num2 = l2_reverse.val if l2_reverse else 0
+
+            sum = num1 + num2 + carry
+            carry = sum // 10
+            rem = sum % 10 
+
+            newnode = ListNode(rem)
+            newlist.next = newnode
+
+            newlist = newlist.next
+
+            l1_reverse = l1_reverse.next if l1_reverse else None
+            l2_reverse = l2_reverse.next if l2_reverse else None
         
-        l1=reverseLists(l1)
-        l2=reverseLists(l2)
+        return reverseList(newhead.next)
 
-        carry=0
-
-        head=curr=ListNode()
-
-        while l1 or l2 or carry:
-
-            val1=l1.val if l1 else 0
-            val2=l2.val if l2 else 0
-
-            ans=val1+val2+carry
-
-            sum=ans%10
-            carry=ans//10
-
-            node=ListNode(sum)
-
-            curr.next=node
-            curr=curr.next
-
-            l1=l1.next if l1 else 0
-            l2=l2.next if l2 else 0
         
-        return reverseLists(head.next)
 
-
+        
