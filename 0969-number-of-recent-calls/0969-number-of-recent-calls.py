b2db95b4c43c4b65e668910b41702c9e27155d14
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-02-01 20:27:16 -0500

    Time: 37 ms (65.81%), Space: 23.1 MB (64.68%) - LeetHub

--- 0969-number-of-recent-calls/0969-number-of-recent-calls.py
+++ 0969-number-of-recent-calls/0969-number-of-recent-calls.py
@@ -1,40 +1,15 @@
 class RecentCounter:
-    '''
-    Plan:
 
-    1. Initialize an empty queue
-    2. At every new ping:
-        Check for requests t-3000 and remove them
-    3. APpend current t
-    3. Return len of current queue.
-
-    '''
     def __init__(self):
         self.queue = deque()
 
     def ping(self, t: int) -> int:
+        self.queue.append(t)
 
-        while self.queue and self.queue[0] < t-3000:
+        while self.queue[0] < t - 3000:
             self.queue.popleft()
         
-        self.queue.append(t)
         return len(self.queue)
-
-
-        
-
-'''
-Understand:
-
-
-
-Match: queue
-
-
-
-
-
-'''
 
 
 # Your RecentCounter object will be instantiated and called as such:
