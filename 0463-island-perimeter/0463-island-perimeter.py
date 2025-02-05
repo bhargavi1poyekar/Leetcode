95fd0408fe4dc255591c7404cca8355b260aece1
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-02-05 12:08:35 -0500

    Time: 177 ms (12.21%), Space: 26.9 MB (6.9%) - LeetHub

---
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -0,0 +1,24 @@
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        
+        def isValid(row, col):
+            return 0 <= row < Row and 0 <= col < Col and grid[row][col] == 1
+        
+        def dfs(row, col):
+            if not isValid(row, col):
+                return 1
+            if (row, col) in seen:
+                return 0
+            seen.add((row, col))
+            return dfs(row, col+1) + dfs(row+1, col) + dfs(row-1, col) + dfs(row, col-1)
+        
+        seen = set()
+        Row = len(grid)
+        Col = len(grid[0])
+
+        for row in range(Row):
+            for col in range(Col):
+                if grid[row][col] == 1:
+                    return dfs(row, col)
+            
+        
