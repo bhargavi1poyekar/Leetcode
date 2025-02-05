fe5f0c873b693a5fd738efe81a1f1575c8c74025
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-02-05 12:25:36 -0500

    Time: 304 ms (14.7%), Space: 28.2 MB (12.18%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,33 +1,30 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         
-        Row = len(grid)
-        Col = len(grid[0])
-
-        def isValid(row, col):
+        def isValid(row, col): 
             return 0 <= row < Row and 0 <= col < Col and grid[row][col] == '1'
-
-        seen = set()
-
         nghbrs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
         def dfs(row, col):
-
             for dx, dy in nghbrs:
-                nextr, nextc = row + dx, col + dy
-                if isValid(nextr, nextc) and (nextr, nextc) not in seen:
-                    seen.add((nextr, nextc))
-                    dfs(nextr, nextc)
-            
-            return 
+                newr = row + dx
+                newc = col + dy
 
+                if isValid(newr, newc) and (newr, newc) not in seen:
+                    seen.add((newr, newc))
+                    dfs(newr, newc)
+        
+        Row = len(grid)
+        Col = len(grid[0])
+
+        seen = set()
         num_islands = 0
 
         for row in range(Row):
             for col in range(Col):
                 if isValid(row, col) and (row, col) not in seen:
-                    num_islands += 1
                     seen.add((row, col))
+                    num_islands += 1
                     dfs(row, col)
         
         return num_islands
