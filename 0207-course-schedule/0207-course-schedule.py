a9c1029bc3c3caf7fc6b3b6e7a2a927839e2386b
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-02-10 20:08:36 -0500

    Time: 7 ms (49.8%), Space: 18.9 MB (80.11%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -2,7 +2,6 @@ class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         
         indegree = [0] * numCourses
-
         graph = defaultdict(list)
 
         for course, pre in prerequisites:
@@ -10,19 +9,19 @@ class Solution:
             indegree[course] += 1
 
         queue = deque()
-        for course in range(numCourses):
-            if indegree[course] == 0:
-                queue.append(course)
-        
+
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                queue.append(i)
+
         num_visit = 0
         while queue:
             num_visit += 1
-            course = queue.popleft()
-            for nghbr in graph[course]:
+            node = queue.popleft()
+
+            for nghbr in graph[node]:
                 indegree[nghbr] -= 1
                 if indegree[nghbr] == 0:
                     queue.append(nghbr)
         
         return num_visit == numCourses
-
-
