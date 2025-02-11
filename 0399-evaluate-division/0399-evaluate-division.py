2950e84235da83e1a7ce6efa1d4318c242112c5e
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-02-10 19:27:54 -0500

    Time: 0 ms (100%), Space: 18.1 MB (27.33%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -1,27 +1,31 @@
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        
         def dfs(start, end, ratio, seen):
-            # no info on this node
             if start in graph:
-                if start==end:
+                if start == end:
                     return ratio
                 for nghbr in graph[start]:
-                    if nghbr not in seen: 
+                    if nghbr not in seen:
                         seen.add(nghbr)
-                        result=dfs(nghbr,end,ratio*graph[start][nghbr],seen)
-                        if result!=-1:
+                        result = dfs(nghbr, end, ratio * graph[start][nghbr], seen)
+                        if result != -1:
                             return result
             return -1
-        
+
         graph = defaultdict(dict)
+
         for i in range(len(equations)):
-            numerator, denominator = equations[i]
+            num, denom = equations[i]
             val = values[i]
-            graph[numerator][denominator] = val
-            graph[denominator][numerator] = 1 / val
+
+            graph[num][denom] = val
+            graph[denom][num] = 1 / val
+
         
-        ans = []
-        for numerator, denominator in queries:
-            ans.append(dfs(numerator, denominator,1, {numerator}))
+        query_ans = []
+
+        for num, denom in queries:
+            query_ans.append(dfs(num, denom, 1, {num}))
         
-        return ans
+        return query_ans
