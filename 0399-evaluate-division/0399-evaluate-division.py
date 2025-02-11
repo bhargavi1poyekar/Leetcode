07398b7427e508fb246a74c0ffb783986badaf24
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-02-10 19:30:19 -0500

    Time: 0 ms (100%), Space: 18.1 MB (27.33%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -2,15 +2,15 @@ class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
         
         def dfs(start, end, ratio, seen):
-            if start in graph:
-                if start == end:
-                    return ratio
-                for nghbr in graph[start]:
-                    if nghbr not in seen:
-                        seen.add(nghbr)
-                        result = dfs(nghbr, end, ratio * graph[start][nghbr], seen)
-                        if result != -1:
-                            return result
+            # if start in graph:
+            if start == end:
+                return ratio
+            for nghbr in graph[start]:
+                if nghbr not in seen:
+                    seen.add(nghbr)
+                    result = dfs(nghbr, end, ratio * graph[start][nghbr], seen)
+                    if result != -1:
+                        return result
             return -1
 
         graph = defaultdict(dict)
@@ -26,6 +26,9 @@ class Solution:
         query_ans = []
 
         for num, denom in queries:
-            query_ans.append(dfs(num, denom, 1, {num}))
-        
+            if num in graph and denom in graph:
+                query_ans.append(dfs(num, denom, 1, {num}))
+            else:
+                query_ans.append(-1)
+
         return query_ans
