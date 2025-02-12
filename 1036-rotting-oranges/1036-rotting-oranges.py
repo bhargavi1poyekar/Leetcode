7b26c4e52967c7d993bc7d84f7c856b0bdc1ffc6
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-02-12 12:47:25 -0500

    Time: 7 ms (28.16%), Space: 17.9 MB (43.34%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -1,37 +1,38 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         
-        def valid(row, col):
-            return 0 <= row < Row and 0 <= col < Col and grid[row][col] == 1
+        def isValid(row, col):
+            return 0 <= row < Row and 0 <= col < Col and grid[row][col] != 0
         
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+
+        fresh_count = 0
+        seen = set()
+        queue = deque()
+
         Row = len(grid)
         Col = len(grid[0])
 
-        directions = [(0,1), (1, 0), (0, -1), (-1, 0)] 
-
-        queue = deque()
-        fresh_count = 0
-
         for row in range(Row):
             for col in range(Col):
-                if grid[row][col] == 2:
-                    queue.append((row, col, 0))
                 if grid[row][col] == 1:
                     fresh_count += 1
-        steps = 0 
+                elif grid[row][col] == 2:
+                    seen.add((row, col))
+                    queue.append((row, col, 0))
+
         while queue:
             row, col, steps = queue.popleft()
-
-            for x,y in directions:
-                nextr, nextc = row + x, col + y
-                if valid(nextr, nextc):
-                    fresh_count -= 1
-                    grid[nextr][nextc] = 2
-                    queue.append((nextr, nextc, steps+1))
+            if grid[row][col] == 1:
+                fresh_count -= 1
+            
+            if not fresh_count:
+                return steps
+            
+            for dx, dy in directions:
+                next_r, next_c = row + dx, col + dy
+                if isValid(next_r, next_c) and (next_r, next_c) not in seen:
+                    seen.add((next_r, next_c))
+                    queue.append((next_r, next_c, steps+1))
         
-        return steps if not fresh_count else -1 
-                
-
-
-
-        
+        return -1 if fresh_count != 0 else 0
