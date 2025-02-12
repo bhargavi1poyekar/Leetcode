88454850418e0e8c990a7b886a665dbcc8c05fc9
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-02-12 11:28:45 -0500

    Time: 300 ms (78.68%), Space: 33.8 MB (33.47%) - LeetHub

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.py
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.py
@@ -1,22 +1,25 @@
 class Solution:
     def maximumSum(self, nums: List[int]) -> int:
-
-        maxsum=-1
-        digsum_hash={}
-        for num in nums:
-            digsum=0
-            rem=0
-            num_org=num
-            while(num):
-                rem=num%10
-                digsum+=rem
-                num=num//10
-            # print(digsum, digsum_hash, maxsum)
-            if digsum in digsum_hash:
-                maxsum=max(maxsum,num_org+digsum_hash[digsum])
-                digsum_hash[digsum]=max(digsum_hash[digsum],num_org)
-            
-            else:
-                digsum_hash[digsum]=num_org
         
-        return maxsum 
+        def sum_of_digits(num):
+            sum = 0
+            while(num):
+                sum += num % 10
+                num = num // 10
+            return sum
+
+        hash_sum = {}
+        max_sum_of_nums = -1
+
+        for i in range(len(nums)):
+            digit_sum = sum_of_digits(nums[i])
+            if digit_sum in hash_sum:
+                j = hash_sum[digit_sum]
+                num_sum = nums[i] + nums[j]
+                max_sum_of_nums = max(max_sum_of_nums, num_sum)
+                if nums[i] >= nums[j]:
+                    hash_sum[digit_sum] = i
+            else:
+                hash_sum[digit_sum] = i
+        
+        return max_sum_of_nums
