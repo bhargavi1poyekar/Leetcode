02714319fd8180a2554c49c83a423c39c5a0ed72
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-02-12 12:29:29 -0500

    Time: 7 ms (60.83%), Space: 21.8 MB (57.11%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -5,41 +5,35 @@ class Solution:
         """
         def isValid(row, col):
             return 0 <= row < Row and 0 <= col < Col and board[row][col] == 'O'
-        
+
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+
         def dfs(row, col):
             seen.add((row, col))
-            for dx, dy in nghbrs:
-                nextr, nextc = row + dx, col + dy
-                if isValid(nextr, nextc) and (nextr, nextc) not in seen:
-                    dfs(nextr, nextc)
-
-        nghbrs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-
+            for dx, dy in directions:
+                next_r, next_c = row + dx, col + dy
+                if isValid(next_r, next_c) and (next_r, next_c) not in seen: 
+                    dfs(next_r, next_c)
+            
         Row = len(board)
         Col = len(board[0])
-
         seen = set()
 
         for row in range(Row):
             if board[row][0] == 'O':
                 dfs(row, 0)
-            if board[row][Col - 1] == "O":
-                dfs(row, Col - 1)
+            if board[row][Col-1] == 'O':
+                dfs(row, Col-1)
         
         for col in range(Col):
             if board[0][col] == 'O':
                 dfs(0, col)
-            if board[Row - 1][col] == "O":
-                dfs(Row - 1, col)
-        
-        for row in range(1, Row-1 ):
-            for col in range(1, Col-1):
+            if board[Row-1][col] == 'O':
+                dfs(Row-1, col)
+
+        for row in range(Row):
+            for col in range(Col):
                 if board[row][col] == 'O' and (row, col) not in seen:
                     board[row][col] = 'X'
         
         return board
-
-        
-
-        
-
