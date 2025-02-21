4ef959557e656d6e0776ad2bc0532b6676338dbc
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-02-20 19:47:52 -0500

    Time: 61 ms (33.52%), Space: 45.5 MB (70.5%) - LeetHub

--- 1731-even-odd-tree/1731-even-odd-tree.py
+++ 1731-even-odd-tree/1731-even-odd-tree.py
@@ -6,35 +6,43 @@
 #         self.right = right
 class Solution:
     def isEvenOddTree(self, root: Optional[TreeNode]) -> bool:
-        
-        queue=deque([root])
-        level=0
+
+        queue = deque([root])
+
+        level_idx = 0
 
         while queue:
-            length=len(queue)
-            if level%2==0:
+
+            length = len(queue) # getting the length, before appending to queue
+
+            # Append to the end of level
+            if level_idx % 2 == 0:
+                # increasing order, last element should be max
                 queue.append(TreeNode(float('inf')))
             else:
+                # increasing order, last element should be min
                 queue.append(TreeNode(float('-inf')))
             
             for _ in range(length):
-                node=queue.popleft()
-                # print(level,node.val,queue[0].val)
-                if level%2==0: 
-                    if node.val%2!=1 or node.val>=queue[0].val:
-                        return False
-                elif level%2==1: 
-                    if node.val%2!=0 or node.val<=queue[0].val:
-                        return False
+                node = queue.popleft()
+
+                if level_idx % 2 == 0:
+                    # check wether val is evem or decreasing order
+                    if node.val % 2 != 1 or node.val >= queue[0].val:
+                        return False 
                 
+                elif level_idx % 2 == 1:
+                    # check wether val is odd or increasing order
+                    if node.val % 2 != 0 or node.val <= queue[0].val:
+                        return False 
+                    
                 if node.left:
                     queue.append(node.left)
                 if node.right:
                     queue.append(node.right)
                 
-            level+=1
-            queue.popleft()
-        
-        return True
-                    
 
+            level_idx += 1
+            queue.popleft()
+    
+        return True
