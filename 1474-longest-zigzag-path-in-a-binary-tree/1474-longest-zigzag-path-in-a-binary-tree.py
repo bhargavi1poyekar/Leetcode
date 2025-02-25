ce61e0f3aa248feb346f85ef31453d2c557b7c36
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-02-24 19:51:45 -0500

    Time: 139 ms (7.62%), Space: 33.3 MB (56.35%) - LeetHub

--- 1474-longest-zigzag-path-in-a-binary-tree/1474-longest-zigzag-path-in-a-binary-tree.py
+++ 1474-longest-zigzag-path-in-a-binary-tree/1474-longest-zigzag-path-in-a-binary-tree.py
@@ -17,14 +17,14 @@ class Solution:
 
             if Left:
                 dfs(root.left, 1, 1)
-                dfs(root.right, 1, curr_length + 1)
+                dfs(root.right, 0, curr_length + 1)
             else:
-                dfs(root.left, 0, curr_length + 1)
+                dfs(root.left, 1, curr_length + 1)
                 dfs(root.right, 0, 1)
 
             return
 
-        dfs(root, 1, 1)
-        dfs(root, 0, 1) 
+        dfs(root, 1, 0)
+        dfs(root, 0, 0) 
 
-        return self.longest-1
+        return self.longest
