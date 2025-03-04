709b32c969faa5ac17b02fb7f97b991faca9b799
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-03-03 20:40:43 -0500

    Time: 480 ms (82.2%), Space: 21.4 MB (14.16%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,49 +1,39 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-
-        def two_sum(arr, target):
-            left = 0
-            right = len(arr) - 1
-            triplets = []
-            while left < right:
-                if arr[left] + arr[right] == target:
-                    triplets.append([arr[left], arr[right], -target])
-                    left += 1
-                    right -= 1
-                    while left < right and arr[left] == arr[left-1]:
-                        left += 1
-                
-                elif arr[left] + arr[right] > target:
-                    right -= 1
-                else:
-                    left += 1
-            
-            return triplets
         
         nums.sort()
-        results = []
+        ans = []
 
+        def two_sum(arr, target):
+            start = 0
+            end = len(arr) - 1
+            triplets = []
+
+            while start < end:
+                if arr[start] + arr[end] == target:
+                    triplets.append([arr[start], arr[end], -target])
+                    start += 1
+                    end -= 1
+
+                    while arr[start] == arr[start - 1] and start < end:
+                        start += 1  
+
+                elif arr[start] + arr[end] > target:
+                    end -= 1
+                else:
+                    start += 1
+            
+            return triplets
+
+        
         for i in range(len(nums)-1):
             if nums[i] > 0:
                 break
-            elif i != 0 and nums[i-1] != nums[i]:
-                results.extend(two_sum(nums[i+1:], -nums[i]))
-            elif i == 0:
-                results.extend(two_sum(nums[i+1:], -nums[i]))
+            elif i != 0 and nums[i] != nums[i-1]:
+                ans.extend(two_sum(nums[i+1:], -nums[i]))
+            elif i==0:
+                ans.extend(two_sum(nums[i+1:], -nums[i]))
         
-        return results
+        return ans
 
-
-
-            
-
-
-
-                    
-
-    
-        
-
-
-
-        
+       
