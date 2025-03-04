c2f2fd6f80342b741c209764544361a5674ee4c6
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-03-04 12:24:02 -0500

    Time: 9 ms (60.78%), Space: 28.3 MB (37.93%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -9,10 +9,8 @@ class Solution:
             curr_sum += nums[right]
 
             while curr_sum >= target:
-                min_length = min(min_length, right - left + 1)
+                min_length = min(min_length, right-left+1)
                 curr_sum -= nums[left]
                 left += 1
-            
-        return min_length if min_length!=len(nums) + 1 else 0
-            
-
+        
+        return min_length if min_length != len(nums)+1 else 0
