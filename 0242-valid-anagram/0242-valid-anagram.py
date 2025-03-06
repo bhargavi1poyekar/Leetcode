c4bfde4f05ff097082b3066c0757130cf808623d
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-03-06 14:17:51 -0500

    Time: 4 ms (94.61%), Space: 18 MB (33.25%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,17 +1,4 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-
-        if len(s) != len(t): return False
-
-        t_count = [0 for i in range(26)]
-
-        for i in range(len(s)):
-            t_count[ord(t[i]) - ord('a')] += 1
-            t_count[ord(s[i]) - ord('a')] -= 1
         
-        for i in range(26):
-            if t_count[i] != 0:
-                return False
-        
-        return True
-        
+        return Counter(s) == Counter(t)
