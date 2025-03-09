4a70273733b4d04ce8534946424a86434b36fcb0
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-03-09 12:47:25 -0400

    Time: 4 ms (77.76%), Space: 19.3 MB (74.64%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -6,25 +6,33 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        # self.idx = 0
+        
         preorder = deque(preorder)
-        def createTree(left_bound, right_bound):
-            if left_bound > right_bound:
+        index_hash = {}
+
+        for i in range(len(inorder)):
+            index_hash[inorder[i]] = i
+        
+        def construct(left, right):
+            if left >= right:
                 return None
             
-            val  = preorder.popleft()
-            # self.idx += 1
+            val = preorder.popleft()
+            node = TreeNode(val)
 
-            root = TreeNode(val)
-            index = hash_idx[val]
-
-            root.left = createTree(left_bound, index-1)
-            root.right = createTree(index+1, right_bound)
-
-            return root
+            index = index_hash[val]
+            node.left = construct(left, index)
+            node.right = construct(index+1, right)
         
-        hash_idx = {}
-        for i in range(len(inorder)):
-            hash_idx[inorder[i]] = i
+            return node
         
-        return createTree(0, len(inorder)-1)
+        return construct(0, len(inorder))
+
+
+
+
+
+
+
+
+
