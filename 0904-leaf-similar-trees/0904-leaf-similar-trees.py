30020c67e9e4aac6114bd986f378b43a189d63f5
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-03-09 13:15:51 -0400

    Time: 0 ms (100%), Space: 17.8 MB (75.21%) - LeetHub

--- 0904-leaf-similar-trees/0904-leaf-similar-trees.py
+++ 0904-leaf-similar-trees/0904-leaf-similar-trees.py
@@ -6,14 +6,14 @@
 #         self.right = right
 class Solution:
     def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
-        def checkleaves(root):
-            
-            if not root: return []
-             
+
+        def dfs(root):
+            if not root:
+                return []
             if not root.left and not root.right:
-                return [root.val]
-                
-            return checkleaves(root.left)+checkleaves(root.right)
+                return [root.val]            
         
-        # print(checkleaves(root1))
-        return checkleaves(root1)==checkleaves(root2)
+            return dfs(root.left) + dfs(root.right)
+        
+        return dfs(root1) == dfs(root2)
+        
