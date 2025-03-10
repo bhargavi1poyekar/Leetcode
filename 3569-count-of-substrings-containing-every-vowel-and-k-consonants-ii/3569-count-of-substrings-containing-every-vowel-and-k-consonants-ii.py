872453b876104babc0fb01abf6b228a704b37b17
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-03-10 10:34:13 -0400

    Time: 2600 ms (19.66%), Space: 18.7 MB (67.05%) - LeetHub

--- 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii.py
+++ 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii.py
@@ -2,45 +2,46 @@ class Solution:
     def _isVowel(self, c: str) -> bool:
         return c in ["a", "e", "i", "o", "u"]
 
-    def _atLeastK(self, word: str, k: int) -> int:
-        num_valid_substrings = 0
-        start = 0
-        end = 0
-        # keep track of counts of vowels and consonants
-        vowel_count = {}
-        consonant_count = 0
-
-        # start sliding window
-        while end < len(word):
-            # insert new letter
-            new_letter = word[end]
-
-            # update counts
-            if self._isVowel(new_letter):
-                vowel_count[new_letter] = vowel_count.get(new_letter, 0) + 1
-            else:
-                consonant_count += 1
-
-            # shrink window while we have a valid substring
-            while len(vowel_count) == 5 and consonant_count >= k:
-                num_valid_substrings += len(word) - end
-                start_letter = word[start]
-                if self._isVowel(start_letter):
-                    vowel_count[start_letter] = (
-                        vowel_count.get(start_letter) - 1
-                    )
-                    if vowel_count.get(start_letter) == 0:
-                        vowel_count.pop(start_letter)
-                else:
-                    consonant_count -= 1
-                start += 1
-
-            end += 1
-
-        return num_valid_substrings
-
     def countOfSubstrings(self, word: str, k: int) -> int:
-        return self._atLeastK(word, k) - self._atLeastK(word, k + 1)
+
+        def _isVowel(c):
+            return c in ["a", "e", "i", "o", "u"]
+
+        def atLeastK(word, k):
+            num_valid_substrings = 0
+            left = 0
+            
+            vowel_count = Counter()
+            consonant_count = 0
+
+            # start sliding window
+            for right in range(len(word)):
+                # insert new letter
+                new_letter = word[right]
+
+                # update counts
+                if _isVowel(new_letter):
+                    vowel_count[new_letter] += 1
+                else:
+                    consonant_count += 1
+
+                # shrink window while we have a valid substring
+                while len(vowel_count) == 5 and consonant_count >= k:
+                    num_valid_substrings += len(word) - right
+                    start_letter = word[left]
+                    if _isVowel(start_letter):
+                        vowel_count[start_letter] -= 1
+                        if vowel_count[start_letter] == 0:
+                            vowel_count.pop(start_letter)
+                    else:
+                        consonant_count -= 1
+                    left += 1
+
+                # end += 1
+
+            return num_valid_substrings
+
+        return atLeastK(word, k) - atLeastK(word, k + 1)
         ''' because atleast k contains
         exactly k, exactly k+1, k+2 and so on
         atleast k+1 contains:
