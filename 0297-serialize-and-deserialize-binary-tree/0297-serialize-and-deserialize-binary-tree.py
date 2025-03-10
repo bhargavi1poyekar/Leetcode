a40a0ebc97e0b6bc015e9b9b89e1952e0fd4868c
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-03-10 12:51:06 -0400

    Time: 74 ms (96.78%), Space: 21.4 MB (54.85%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
@@ -13,19 +13,18 @@ class Codec:
         :type root: TreeNode
         :rtype: str
         """
-        ans=[]
+        tree_str=[]
         queue=deque([root])
         while queue:
-            for _ in range(len(queue)):
-                node=queue.popleft()
-                if not node:
-                    ans.append('Null')
-                else:
-                    ans.append(str(node.val))
-                    queue.append(node.left)
-                    queue.append(node.right)
-       
-        return '/'.join(ans)
+            node=queue.popleft()
+            if not node:
+                tree_str.append('Null')
+            else:
+                tree_str.append(str(node.val))
+                queue.append(node.left)
+                queue.append(node.right)
+        
+        return '/'.join(tree_str)
 
     def deserialize(self, data):
         """Decodes your encoded data to tree.
@@ -33,31 +32,34 @@ class Codec:
         :type data: str
         :rtype: TreeNode
         """
-        tree_str=data.split('/')
-        if tree_str[0]=='Null':
+        data=data.split('/')
+        if data[0]=='Null':
             return None
 
-        root=TreeNode(int(tree_str[0]))
-        stack=[root]
         idx=1
-        while stack:
-            if tree_str[idx]!='Null':
-                stack[0].left=TreeNode(int(tree_str[idx]))
+        root=TreeNode(int(data[0]))
+        queue=deque([root])
+        while queue:
+            if data[idx]=='Null':
+                queue[0].left=None
                 idx+=1
-                stack.append(stack[0].left)
-            else:
-                stack[0].left=None
+            else:  
+                queue[0].left=TreeNode(int(data[idx]))
+                queue.append(queue[0].left)
                 idx+=1
-            if tree_str[idx]!='Null':
-                stack[0].right=TreeNode(int(tree_str[idx]))
+
+            if data[idx]=='Null':
+                queue[0].right=None
                 idx+=1
-                stack.append(stack[0].right)
-            else:
-                stack[0].right=None
+            else:    
+                queue[0].right=TreeNode(int(data[idx]))
+                queue.append(queue[0].right)
                 idx+=1
-            stack.pop(0)
-        
+            queue.popleft()
         return root
+
+        
+
 # Your Codec object will be instantiated and called as such:
 # ser = Codec()
 # deser = Codec()
