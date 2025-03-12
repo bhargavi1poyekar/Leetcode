5f82fec43d80c1dd733ad8bfaa6ddd0ca76be55c
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-03-12 11:55:50 -0400

    Time: 3 ms (31.43%), Space: 18.1 MB (41.77%) - LeetHub

--- 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.py
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.py
@@ -1,6 +1,8 @@
 class Solution:
     def maximumCount(self, nums: List[int]) -> int:
         
+        '''
+        # Linear Complexity
         count_pos = 0
         count_neg = 0
 
@@ -11,3 +13,45 @@ class Solution:
                 count_pos += 1
         
         return max(count_neg, count_pos)
+        '''
+
+        # Binary Search: log complexity. 
+
+        # Search for 0: get index, from index find, num pos, and num neg 
+
+        def lower_bound(nums):
+            start, end = 0, len(nums) - 1
+            index = len(nums)
+
+            while start <= end:
+                mid = (start + end) // 2
+                if nums[mid] < 0:
+                    start = mid + 1
+                else:
+                    end = mid - 1
+                    index = mid
+
+            return index
+        
+        def upper_bound(nums):
+            start, end = 0, len(nums) - 1
+            index = len(nums)
+
+            while start <= end:
+                mid = (start + end) // 2
+
+                if nums[mid] <= 0:
+                    start = mid + 1
+                else:
+                    end = mid - 1
+                    index = mid
+
+            return index
+
+        positiveCount = len(nums) - upper_bound(nums)
+        # All integers from the index 0 to index before the first zero index
+        # will be negative.
+        negativeCount = lower_bound(nums)
+
+        return max(positiveCount, negativeCount)
+            
