cb9e93a8ff13d5d089d39a4cd8c5298380501721
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-03-12 12:10:47 -0400

    Time: 37 ms (47.05%), Space: 19.3 MB (80.46%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -2,33 +2,32 @@ class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         
         def isValid(row, col):
-            return 0 <= row < Row_len and 0 <= col < Col_len
+            return 0 <= row < Row and 0 <= col < Col
         
         directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
+        Row = len(heights)
+        Col = len(heights[0])
+        
         def dfs(row, col, seen):
-            for dx, dy in directions:
-                next_r, next_c = row + dx, col + dy
-                if (next_r, next_c) not in seen and isValid(next_r, next_c):
-                    if heights[next_r][next_c] >= heights[row][col]:
-                        seen.add((next_r, next_c))
-                        dfs(next_r, next_c, seen)
-        
-        Row_len = len(heights)
-        Col_len = len(heights[0])
-        Pacific_seen = set()
-        Atlantic_seen = set()
+            seen.add((row, col))
 
-        for row in range(Row_len):
-            Pacific_seen.add((row, 0))
-            dfs(row, 0, Pacific_seen)
-            Atlantic_seen.add((row, Col_len-1))
-            dfs(row, Col_len-1, Atlantic_seen)
+            for dx, dy in directions:
+                nextr, nextc = row + dx, col + dy
+                if isValid(nextr, nextc) and (nextr, nextc) not in seen and heights[nextr][nextc] >= heights[row][col]:
+                    dfs(nextr, nextc, seen)
         
-        for col in range(Col_len):
-            Pacific_seen.add((0, col))
-            dfs(0, col, Pacific_seen)
-            Atlantic_seen.add((Row_len-1, col))
-            dfs(Row_len-1, col, Atlantic_seen)
+            
         
-        return list(Pacific_seen.intersection(Atlantic_seen))
+        Pacific = set()
+        Atlantic = set()
+
+        for i in range(Row):
+            dfs(i, 0, Pacific)
+            dfs(i, Col-1, Atlantic)
+        
+        for i in range(Col):
+            dfs(0, i, Pacific)
+            dfs(Row-1, i, Atlantic)
+    
+        return list(Pacific.intersection(Atlantic))
