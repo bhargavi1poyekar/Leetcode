0c037616e2c688990c3de4aea39ae8e22967b1a7
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-03-12 19:39:10 -0400

    Time: 0 ms (100%), Space: 17.7 MB (65.39%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
@@ -1,16 +1,14 @@
 class Solution:
     def minMutation(self, startGene: str, endGene: str, bank: List[str]) -> int:
-
-        def find_nghbr(gene):
-            ans = []
-            # print(gene)
-            for i in range(8):
-                for char in ['A', 'C', 'G', 'T']:
-                    new_gene = gene[:i] + char + gene[i+1:]
-                    ans.append(new_gene)
-
-            return ans 
-                
+        
+        def find_nghbrs(gene):
+            nghbrs = []
+            for i in range(len(gene)):
+                for choice in ['A', 'C', 'G', 'T']:
+                    new_gene = gene[:i] + choice + gene[i+1:]
+                    nghbrs.append(new_gene)
+                    
+            return nghbrs
 
         seen = {startGene}
         queue = deque([(startGene, 0)])
@@ -21,10 +19,9 @@ class Solution:
             if gene == endGene:
                 return steps
             
-            for nghbr in find_nghbr(gene):
-                # print(nghbr)
-                if nghbr in bank and nghbr not in seen:
-                    seen.add(nghbr)
-                    queue.append((nghbr, steps+1))
-        
+            for new_gene in find_nghbrs(gene):
+                if new_gene in bank and new_gene not in seen:
+                    seen.add(new_gene)
+                    queue.append((new_gene, steps+1))
+
         return -1
