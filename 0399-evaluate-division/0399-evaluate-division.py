30bcaf2d57d395da6f8b3588128bb88a5383911b
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-03-12 12:59:38 -0400

    Time: 0 ms (100%), Space: 18.1 MB (30.1%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -1,33 +1,32 @@
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
         
-        def dfs(start, end, ratio, seen):
-            if start == end:
-                return ratio
-            for nghbr in graph[start]:
-                if nghbr not in seen:
-                    seen.add(nghbr)
-                    result = dfs(nghbr, end, ratio * graph[start][nghbr], seen)
-                    if result != -1:
-                        return result
-            return -1
-
         graph = defaultdict(dict)
 
         for i in range(len(equations)):
-            num, denom = equations[i]
-            val = values[i]
+            num = equations[i][0]
+            denom = equations[i][1]
+            graph[num][denom] = values[i]
+            graph[denom][num] = 1/values[i]
 
-            graph[num][denom] = val
-            graph[denom][num] = 1 / val
 
-        
-        query_ans = []
-
-        for num, denom in queries:
+        def dfs(num, denom, ratio, seen):
+            if num == denom: return ratio
+            for nghbr in graph[num]:
+                if nghbr not in seen:
+                    seen.add(nghbr)
+                    result = dfs(nghbr, denom, ratio*graph[num][nghbr], seen)
+                    if result != -1:
+                        return result
+            
+            return -1
+        ans = []
+        for query in queries:
+            num = query[0]
+            denom = query[1]
             if num in graph and denom in graph:
-                query_ans.append(dfs(num, denom, 1, {num}))
+                ans.append(dfs(num, denom, 1, set()))
             else:
-                query_ans.append(-1)
-
-        return query_ans
+                ans.append(float(-1))
+        
+        return ans
