f16ce49d22737040f0735e5a536cce31c91b908a
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-03-12 19:53:31 -0400

    Time: 116 ms (24.98%), Space: 20.1 MB (26.4%) - LeetHub

--- 2038-nearest-exit-from-entrance-in-maze/2038-nearest-exit-from-entrance-in-maze.py
+++ 2038-nearest-exit-from-entrance-in-maze/2038-nearest-exit-from-entrance-in-maze.py
@@ -1,18 +1,20 @@
 class Solution:
     def nearestExit(self, maze: List[List[str]], entrance: List[int]) -> int:
+        
         def isValid(row, col):
             return 0 <= row < Row and 0 <= col < Col and maze[row][col] == '.'
+
         def isExit(row, col):
             return row == 0 or row == Row-1 or col == 0 or col == Col-1
 
         directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
+        startr, startc = entrance
+        seen = {(startr, startc)}
+        queue = deque([(startr, startc, 1)])
+
         Row = len(maze)
         Col = len(maze[0])
-
-        startr, startc = entrance
-        queue = deque([(startr, startc, 1)])
-        seen = {(startr, startc)}
 
         while queue:
             row, col, steps = queue.popleft()
