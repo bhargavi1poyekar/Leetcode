c24312c4b83b8e1caf1223b1a25c50e3264f23f3
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-03-12 20:11:29 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -2,37 +2,40 @@ class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         
         def isValid(row, col):
-            return 0 <= row < Row and 0 <= col < Col and grid[row][col] != 0
+            return 0 <= row < Row and 0 <= col < Col and grid[row][col] == 1 
         
-        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-
-        fresh_count = 0
-        seen = set()
-        queue = deque()
-
         Row = len(grid)
         Col = len(grid[0])
+
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+
+        queue = deque()
+        fresh_count = 0
 
         for row in range(Row):
             for col in range(Col):
                 if grid[row][col] == 1:
                     fresh_count += 1
                 elif grid[row][col] == 2:
-                    seen.add((row, col))
                     queue.append((row, col, 0))
-
+        
+        mins = 0
         while queue:
-            row, col, steps = queue.popleft()
-            if grid[row][col] == 1:
-                fresh_count -= 1
-            
-            if not fresh_count:
-                return steps
+            row, col, mins = queue.popleft()
+
+            if fresh_count == 0:
+                return mins
             
             for dx, dy in directions:
-                next_r, next_c = row + dx, col + dy
-                if isValid(next_r, next_c) and (next_r, next_c) not in seen:
-                    seen.add((next_r, next_c))
-                    queue.append((next_r, next_c, steps+1))
+                nextr, nextc = row + dx, col + dy
+                if isValid(nextr, nextc):
+                    fresh_count -= 1
+                    grid[nextr][nextc] = 2
+                    queue.append((nextr, nextc, mins+1))
         
-        return -1 if fresh_count != 0 else 0
+        return mins if not fresh_count else -1
+
+
+
+
+
