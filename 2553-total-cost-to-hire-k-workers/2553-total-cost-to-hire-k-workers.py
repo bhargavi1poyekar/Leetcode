55da87a814a4504c168d23c6fb4047c4c9860773
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-03-13 15:23:38 -0400

    Time: 200 ms (39.02%), Space: 32.1 MB (34.32%) - LeetHub

--- 2553-total-cost-to-hire-k-workers/2553-total-cost-to-hire-k-workers.py
+++ 2553-total-cost-to-hire-k-workers/2553-total-cost-to-hire-k-workers.py
@@ -1,33 +1,32 @@
 class Solution:
     def totalCost(self, costs: List[int], k: int, candidates: int) -> int:
-
-        min_heap=[]
-
-        for i in range(candidates):
-            min_heap.append((costs[i],0))
         
-        for i in range(len(costs)-1,max(len(costs)-candidates-1,candidates-1),-1):
-            min_heap.append((costs[i],1))
+        min_heap = []   
+        n = candidates
 
+        for i in range(n):
+            heapq.heappush(min_heap, (costs[i], 0))
+
+        for i in range(len(costs)-1, max(len(costs)-n-1, n-1), -1):
+            heapq.heappush(min_heap, (costs[i], 1))
         
-        nextHead,nextTail=candidates,len(costs)-candidates-1
-        heapq.heapify(min_heap)
-        
-        total_cost=0
+        left, right = n, len(costs)-n-1
+        total_cost = 0
+
         for _ in range(k):
-            # print(min_heap)
 
-            cost,half=heapq.heappop(min_heap)
-            total_cost+=cost
-            
-            if nextHead<=nextTail:
+            cost, worker = heapq.heappop(min_heap)
+            total_cost += cost
 
-                if half==0:
-                    heapq.heappush(min_heap,(costs[nextHead],0))
-                    nextHead+=1
+            if left <= right:
+                if worker == 0:
+                    heapq.heappush(min_heap, (costs[left], 0))
+                    left += 1
                 else:
-                    heapq.heappush(min_heap,(costs[nextTail],1))
-                    nextTail-=1
+                    heapq.heappush(min_heap, (costs[right], 1))
+                    right -= 1
         
         return total_cost
+            
         
+
