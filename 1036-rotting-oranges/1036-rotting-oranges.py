4b0a8b2604552ed0b6122dbfd7b4aac274cf15d6
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-03-12 20:12:21 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -1,70 +1,40 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         
-        # def isValid(row, col):
-        #     return 0 <= row < Row and 0 <= col < Col and grid[row][col] == 1 
+        def isValid(row, col):
+            return 0 <= row < Row and 0 <= col < Col and grid[row][col] == 1 
         
-        # Row = len(grid)
-        # Col = len(grid[0])
+        Row = len(grid)
+        Col = len(grid[0])
 
-        # directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
-        # queue = deque()
-        # fresh_count = 0
+        queue = deque()
+        fresh_count = 0
 
-        # for row in range(Row):
-        #     for col in range(Col):
-        #         if grid[row][col] == 1:
-        #             fresh_count += 1
-        #         elif grid[row][col] == 2:
-        #             queue.append((row, col, 0))
+        for row in range(Row):
+            for col in range(Col):
+                if grid[row][col] == 1:
+                    fresh_count += 1
+                elif grid[row][col] == 2:
+                    queue.append((row, col, 0))
         
-        # mins = 0
-        # while queue:
-        #     row, col, mins = queue.popleft()
-
-        #     if fresh_count == 0:
-        #         return mins
-            
-        #     for dx, dy in directions:
-        #         nextr, nextc = row + dx, col + dy
-        #         if isValid(nextr, nextc):
-        #             fresh_count -= 1
-        #             grid[nextr][nextc] = 2
-        #             queue.append((nextr, nextc, mins+1))
-        
-        # return mins if not fresh_count else -1
-
-        def valid(row,col):
-            return 0<=row<m and 0<=col<n and grid[row][col]==1
-        
-        directions=[(0,1),(1,0),(0,-1),(-1,0)]
-
-        m=len(grid)
-        n=len(grid[0])
-
-        queue=deque()
-        fresh_count=0
-
-        for row in range(m):
-            for col in range(n):
-                if grid[row][col]==2:
-                    queue.append((row,col,0))
-                if grid[row][col]==1:
-                    fresh_count+=1
-
-        steps=0
+        mins = 0
         while queue:
-            row,col,steps=queue.popleft()
+            row, col, mins = queue.popleft()
+
+            if fresh_count == 0:
+                return mins
             
-            for x,y in directions:
-                nextr,nextc=row+x,col+y
-                if valid(nextr,nextc):
-                    fresh_count-=1
-                    grid[nextr][nextc]=2
-                    queue.append((nextr,nextc,steps+1))
+            for dx, dy in directions:
+                nextr, nextc = row + dx, col + dy
+                if isValid(nextr, nextc):
+                    fresh_count -= 1
+                    grid[nextr][nextc] = 2
+                    queue.append((nextr, nextc, mins+1))
         
-        return steps if not fresh_count else -1
+        return mins if not fresh_count else -1
+
 
 
 
