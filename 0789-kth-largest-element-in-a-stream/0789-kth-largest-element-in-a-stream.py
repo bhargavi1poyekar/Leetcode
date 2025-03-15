3057fa0da666a808883e5aa3eafc08c1715bb983
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-03-15 19:48:55 -0400

    Time: 12 ms (80.5%), Space: 23.8 MB (30.09%) - LeetHub

--- 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.py
+++ 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.py
@@ -1,31 +1,27 @@
 class KthLargest:
 
     def __init__(self, k: int, nums: List[int]):
+        self.min_heap = []
         self.k = k
-        self.heap = nums
+        self.nums = nums
 
-        heapq.heapify(self.heap)
+        for num in self.nums:
+            heapq.heappush(self.min_heap, num)
+            if len(self.min_heap) > k:
+                heapq.heappop(self.min_heap)
 
-        while len(self.heap) > self.k:
-            heapq.heappop(self.heap)
-        
     def add(self, val: int) -> int:
+        heapq.heappush(self.min_heap, val)
+        if len(self.min_heap) > self.k:
+            heapq.heappop(self.min_heap)
+    
+        return self.min_heap[0]
 
-        heapq.heappush(self.heap, val)
-        # heapq.heapify()
 
-        if len(self.heap) > self.k:
-            heapq.heappop(self.heap)
-        return self.heap[0]
 
         
+
 
 # Your KthLargest object will be instantiated and called as such:
 # obj = KthLargest(k, nums)
 # param_1 = obj.add(val)
-
-'''
-Understand:
-
-
-'''
