634720e1fd697fee50e7f0a66492c2b9c9977d79
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-03-15 20:44:15 -0400

    Time: 175 ms (44.91%), Space: 31 MB (37.42%) - LeetHub

--- 2553-total-cost-to-hire-k-workers/2553-total-cost-to-hire-k-workers.py
+++ 2553-total-cost-to-hire-k-workers/2553-total-cost-to-hire-k-workers.py
@@ -1,25 +1,27 @@
 class Solution:
     def totalCost(self, costs: List[int], k: int, candidates: int) -> int:
         
-        min_heap = []   
+        min_heap = []
         n = candidates
 
         for i in range(n):
-            heapq.heappush(min_heap, (costs[i], 0))
-
-        for i in range(len(costs)-1, max(len(costs)-n-1, n-1), -1):
-            heapq.heappush(min_heap, (costs[i], 1))
+            min_heap.append((costs[i], 0))
         
-        left, right = n, len(costs)-n-1
+        for i in range(len(costs)-1, max(len(costs)-n-1, n-1), -1):
+            min_heap.append((costs[i], 1))
+
+        heapq.heapify(min_heap)
+
+        left = n
+        right = len(costs)-n-1
         total_cost = 0
 
         for _ in range(k):
-
-            cost, worker = heapq.heappop(min_heap)
+            cost, half = heapq.heappop(min_heap)
             total_cost += cost
 
             if left <= right:
-                if worker == 0:
+                if half == 0:
                     heapq.heappush(min_heap, (costs[left], 0))
                     left += 1
                 else:
@@ -27,6 +29,4 @@ class Solution:
                     right -= 1
         
         return total_cost
-            
-        
 
