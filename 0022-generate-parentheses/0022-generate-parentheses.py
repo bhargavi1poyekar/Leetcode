bb5529a21093d9d932769238616859c279c12b67
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-03-17 11:48:52 -0400

    Time: 0 ms (100%), Space: 17.9 MB (69.91%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,37 +1,21 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         
-        # def dfs(left, right, s):
-        #     if len(s) == n * 2:
-        #         res.append(s)
-        #         return 
-
-        #     if left < n:
-        #         dfs(left + 1, right, s + '(')
-
-        #     if right < left:
-        #         dfs(left, right + 1, s + ')')
-
-        # res = []
-        # dfs(0, 0, '')
-        # return res
-
-        def backtrack(current,left,right):
-            if len(current) == 2 * n:
-                answer.append("".join(current))
-                return
+        def backtrack(curr, left, right):
+            if len(curr) == 2*n:
+                ans.append(''.join(curr))
+                return 
             
             if left < n:
-                current.append("(")
-                backtrack(current,left + 1,right)
-                current.pop()
+                curr.append('(')
+                backtrack(curr, left+1, right)
+                curr.pop()
+            
             if right < left:
-                current.append(")")
-                backtrack(current,left,right + 1)
-                current.pop()
+                curr.append(')')
+                backtrack(curr, left, right+1)
+                curr.pop()
         
-        answer = []
-        backtrack([],0,0)
-        return answer
-
-
+        ans = []
+        backtrack([], 0, 0)
+        return ans
