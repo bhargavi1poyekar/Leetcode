cfdf7efdb6319380ee946961051224736a4cc190
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-03-17 11:42:39 -0400

    Time: 5795 ms (18.04%), Space: 17.8 MB (87.11%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -4,30 +4,33 @@ class Solution:
         def isValid(row, col):
             return 0 <= row < Row and 0 <= col < Col
         
-        directions = [(0, 1), (0, -1), (-1, 0), (1, 0)]
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
         
-        def backtrack(row, col, curr_idx, seen):
-            if curr_idx == len(word):
+        def backtrack(row, col, i, seen):
+            if i == len(word):
                 return True
             
             for dx, dy in directions:
                 nextr, nextc = row + dx, col + dy
                 if isValid(nextr, nextc) and (nextr, nextc) not in seen:
-                    if board[nextr][nextc] == word[curr_idx]:
+                    if board[nextr][nextc] == word[i]:
                         seen.add((nextr, nextc))
-                        if backtrack(nextr, nextc, curr_idx + 1, seen):
+                        if backtrack(nextr, nextc, i+1, seen):
                             return True
                         seen.remove((nextr, nextc))
-
+            
             return False
-
+        
         Row = len(board)
         Col = len(board[0])
 
         for row in range(Row):
             for col in range(Col):
-                if board[row][col] == word[0] and backtrack(row, col, 1, {(row, col)}):
-                    return True
-            
+                if board[row][col] == word[0]: 
+                    if backtrack(row, col, 1, {(row, col)}):
+                        return True
         return False
 
+
+            
+            
