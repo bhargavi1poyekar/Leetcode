606386c2030228d861828869af5d224044192b6c
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-03-17 11:53:10 -0400

    Time: 0 ms (100%), Space: 17.7 MB (59.82%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.py
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -1,19 +1,17 @@
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
         
-        def backtrack(remaining, combination, start, end):
-            if remaining == 0 and len(combination) == k:
-                answer.append(combination[:])
-                return
+        def backtrack(curr, i, currsum):
+            if len(curr) == k and currsum == n:
+                ans.append(curr[:])
+                return 
             
-            if remaining < 0 or len(combination) == k:
-                return
-            
-            for digit in range(start,end+1):
-                combination.append(digit)
-                backtrack(remaining - digit, combination, digit + 1, min(remaining-digit,9))
-                combination.pop()
+            for j in range(i+1, 10):
+                if currsum + j <= n:
+                    curr.append(j)
+                    backtrack(curr, j, currsum + j)
+                    curr.pop()
         
-        answer = []
-        backtrack(n,[],1, 9)
-        return answer
+        ans = []
+        backtrack([], 0, 0)
+        return ans
