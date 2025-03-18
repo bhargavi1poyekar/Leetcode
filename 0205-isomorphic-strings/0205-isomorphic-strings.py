330610734ac38f5da9e9e82dc3d6e2b926c83465
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-03-17 20:29:22 -0400

    Time: 7 ms (57.23%), Space: 17.9 MB (80.3%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,9 +1,9 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        
+
         if len(s) != len(t):
             return False
-        
+
         s_map = defaultdict()
         t_map = defaultdict()
 
@@ -11,8 +11,10 @@ class Solution:
             if s[i] not in s_map and t[i] not in t_map:
                 s_map[s[i]] = t[i]
                 t_map[t[i]] = s[i]
-            
-            elif s_map.get(s[i]) != t[i] or t_map.get(t[i]) != s[i]:
+
+            elif (s[i] in s_map and s_map[s[i]] != t[i]) or (t[i] in t_map and t_map[t[i]] != s[i]):
                 return False
 
         return True 
+
+    
