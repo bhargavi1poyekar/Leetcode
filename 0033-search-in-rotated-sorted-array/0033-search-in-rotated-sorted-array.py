24bf2ef691f340d56188cddbac17065c4fff0c22
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-03-18 12:48:44 -0400

    Time: 0 ms (100%), Space: 17.9 MB (91.62%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,35 +1,32 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-
-        def binary_search(left,right,target):
-            while left<=right:
-                mid=(left+right)//2
-                if nums[mid]==target:
+        
+        def binary_search(left, right):
+            while left <= right:
+                mid = (left + right) // 2
+                if nums[mid] == target:
                     return mid
-                elif nums[mid]>target:
-                    right=mid-1
+                elif nums[mid] > target:
+                    right = mid - 1
                 else:
-                    left=mid+1
-
+                    left = mid + 1
+            
             return -1
 
-        n=len(nums)
-        left,right=0,n-1
+        left = 0
+        right = len(nums)-1
 
-        while left<=right:
-            mid=(left+right)//2
-            if nums[mid]>nums[-1]:
-                left=mid+1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] > nums[-1]:
+                left = mid + 1
             else:
-                right=mid-1
+                right = mid - 1
         
-        pivot=left
+        pivot = left
 
-        left_half=binary_search(0,pivot-1,target)
-        if left_half!=-1:
-            return left_half
-        
-        return binary_search(pivot,n-1,target)
+        left_search = binary_search(0, pivot-1)
+        if left_search != -1:
+            return left_search
+        return binary_search(pivot, len(nums)-1)
 
-
-        
