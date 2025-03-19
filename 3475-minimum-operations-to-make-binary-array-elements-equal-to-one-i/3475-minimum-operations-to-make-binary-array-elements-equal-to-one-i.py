ca78d88e1740e0e3da275c4b43fbd9bdba2a59e6
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-03-19 18:26:06 -0400

    Time: 179 ms (22.45%), Space: 21.7 MB (55.09%) - LeetHub

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.py
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.py
@@ -1,16 +1,17 @@
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
+        queue = deque()
         count = 0
-        for i in range(2, len(nums)):
 
-            # only looking forward to the last element
-            if nums[i - 2] == 0:
+        for i in range(len(nums)):
+            while queue and i > queue[0] + 2:
+                queue.popleft()
+            
+            if (nums[i] + len(queue)) % 2 == 0:
+                if i + 2 >= len(nums):
+                    return -1
+                
                 count += 1
-                # flip i-2 and i-1 and i
-                nums[i - 2] = nums[i - 2] ^ 1
-                nums[i - 1] = nums[i - 1] ^ 1
-                nums[i] = nums[i] ^ 1
-
-        if sum(nums) == len(nums):
-            return count
-        return -1
+                queue.append(i)
+        
+        return count
