46e40486fd3e32aba8d3c402c0492623a2e807a1
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-03-20 17:43:02 -0400

    Time: 11 ms (71.59%), Space: 18.3 MB (68.79%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.py
+++ 0437-path-sum-iii/0437-path-sum-iii.py
@@ -7,25 +7,28 @@
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
         
+        self.hash_sum = Counter()
+        self.hash_sum[0] = 1
         self.num_paths = 0
-        self.hash = Counter()
-        self.hash[0] = 1 # -> very important. 
 
         def dfs(root, curr_sum):
             if not root:
                 return 
-            
+
             curr_sum += root.val
-            # if (curr_sum - targetSum) in self.hash:
-            self.num_paths += self.hash[curr_sum - targetSum]
-            
-            self.hash[curr_sum] += 1
-            
+            self.num_paths += self.hash_sum[curr_sum - targetSum] 
+            self.hash_sum[curr_sum] += 1
+
             dfs(root.left, curr_sum)
             dfs(root.right, curr_sum)
 
-            self.hash[curr_sum] -= 1
+            self.hash_sum[curr_sum] -= 1
+
+            return 
         
         dfs(root, 0)
         return self.num_paths
-        
+            
+            
+
+            
