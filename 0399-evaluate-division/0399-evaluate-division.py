7e8b6bb120e39a824046fc3dfc094a9dd9b06cdb
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-03-21 19:08:49 -0400

    Time: 0 ms (100%), Space: 17.8 MB (94.25%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -1,32 +1,32 @@
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        
-        graph = defaultdict(dict)
 
+        graph = defaultdict(dict)
         for i in range(len(equations)):
-            num = equations[i][0]
-            denom = equations[i][1]
+            num, denom = equations[i]
             graph[num][denom] = values[i]
             graph[denom][num] = 1/values[i]
-
-
+        
+        # print(graph)
+        
         def dfs(num, denom, ratio, seen):
-            if num == denom: return ratio
+            if num == denom:
+                return ratio
+            
             for nghbr in graph[num]:
                 if nghbr not in seen:
                     seen.add(nghbr)
                     result = dfs(nghbr, denom, ratio*graph[num][nghbr], seen)
                     if result != -1:
                         return result
-            
             return -1
-        ans = []
-        for query in queries:
-            num = query[0]
-            denom = query[1]
-            if num in graph and denom in graph:
-                ans.append(dfs(num, denom, 1, set()))
-            else:
-                ans.append(float(-1))
         
-        return ans
+        query_ans = [] 
+        for query in queries:
+            num, denom = query
+            if num not in graph or denom not in graph:
+                query_ans.append(-1)
+            else:
+                query_ans.append(dfs(num, denom, 1, {num}))
+
+        return query_ans 
