0a89b959eb5a351916540f44d8ec70a86129e32c
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-03-22 19:14:44 -0400

    Time: 119 ms (24.19%), Space: 20.2 MB (17.01%) - LeetHub

--- 2038-nearest-exit-from-entrance-in-maze/2038-nearest-exit-from-entrance-in-maze.py
+++ 2038-nearest-exit-from-entrance-in-maze/2038-nearest-exit-from-entrance-in-maze.py
@@ -3,27 +3,32 @@ class Solution:
         
         def isValid(row, col):
             return 0 <= row < Row and 0 <= col < Col and maze[row][col] == '.'
-
+        
         def isExit(row, col):
-            return row == 0 or row == Row-1 or col == 0 or col == Col-1
+            return (row == 0 or row == Row-1 or col == 0 or col == Col-1)
+        
+        er, ec = entrance
+        seen = {(er, ec)}
+        queue = deque([(er, ec, 0)])
 
         directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-
-        startr, startc = entrance
-        seen = {(startr, startc)}
-        queue = deque([(startr, startc, 1)])
 
         Row = len(maze)
         Col = len(maze[0])
 
         while queue:
+            # print(queue)
             row, col, steps = queue.popleft()
+
+            # print(row, col)
+            # print(isExit(row, col))
+            # print((row != er and col != er))
+            if isExit(row, col) and (row, col) != (er, ec):
+                return steps
             
             for dx, dy in directions:
                 nextr, nextc = row + dx, col + dy
                 if isValid(nextr, nextc) and (nextr, nextc) not in seen:
-                    if isExit(nextr, nextc):
-                        return steps
                     seen.add((nextr, nextc))
                     queue.append((nextr, nextc, steps+1))
         
