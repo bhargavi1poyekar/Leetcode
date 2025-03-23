2cad313619d04cdaa70a3d3c3e2d988ddc4c86ea
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-03-23 17:44:35 -0400

    Time: 71 ms (72.06%), Space: 25.5 MB (11.77%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -1,30 +1,32 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        
-        if endWord not in wordList:
-            return 0
-
-        word_length = len(beginWord)
+        length = len(beginWord)
         graph = defaultdict(list)
 
         for word in wordList:
-            for i in range(word_length):
-                graph[word[:i] + '*' + word[i+1:]].append(word)
-
+            for i in range(length):
+                intermediate_word = word[:i] + '*' + word[i+1:]
+                graph[intermediate_word].append(word)
+        
         queue = deque([(beginWord, 1)])
         seen = {beginWord}
 
         while queue:
-            word, steps = queue.popleft()
+            word, seq = queue.popleft()
 
             if word == endWord:
-                return steps
-
-            for i in range(word_length):
-                inter_word = word[:i] + '*' + word[i+1:]
-                for nghbr in graph[inter_word]:
+                return seq
+            
+            for i in range(length):
+                inter = word[:i] + '*' + word[i+1:]
+                for nghbr in graph[inter]:
                     if nghbr not in seen:
                         seen.add(nghbr)
-                        queue.append((nghbr, steps+1))
+                        queue.append((nghbr, seq+1))
         
         return 0
+
+
+
+
+
