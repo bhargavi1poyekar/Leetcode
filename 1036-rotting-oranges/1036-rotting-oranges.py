d95d05d50bfbf990c218b716db4eb318fcef2c2e
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-03-23 17:35:54 -0400

    Time: 4 ms (50.06%), Space: 18 MB (27.39%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -2,40 +2,37 @@ class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         
         def isValid(row, col):
-            return 0 <= row < Row and 0 <= col < Col and grid[row][col] == 1 
+            return 0 <= row < Row and 0 <= col < Col and grid[row][col] == 1
         
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
         Row = len(grid)
         Col = len(grid[0])
-
-        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-
+        
         queue = deque()
+        seen = set()
         fresh_count = 0
 
         for row in range(Row):
-            for col in range(Col):
+            for col in range(Col): 
                 if grid[row][col] == 1:
                     fresh_count += 1
                 elif grid[row][col] == 2:
-                    queue.append((row, col, 0))
-        
-        mins = 0
+                    queue.append((row, col, 1))
+                    seen.add((row, col))
+
+        if not fresh_count:
+            return 0
+            
         while queue:
             row, col, mins = queue.popleft()
 
-            if fresh_count == 0:
-                return mins
-            
             for dx, dy in directions:
                 nextr, nextc = row + dx, col + dy
-                if isValid(nextr, nextc):
+                if isValid(nextr, nextc) and (nextr, nextc) not in seen:
                     fresh_count -= 1
-                    grid[nextr][nextc] = 2
-                    queue.append((nextr, nextc, mins+1))
+                    if fresh_count == 0:
+                        return mins
+                    seen.add((nextr, nextc))
+                    queue.append((nextr, nextc, mins + 1))
         
-        return mins if not fresh_count else -1
-
-
-
-
-
+        return -1
