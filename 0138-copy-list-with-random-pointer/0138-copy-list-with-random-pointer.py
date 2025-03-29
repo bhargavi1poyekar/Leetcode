11f4b4576cea474d3baf25b8b1b4cd741ac330d2
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-03-29 19:25:12 -0400

    Time: 43 ms (46.62%), Space: 18.5 MB (84.98%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -9,10 +9,29 @@ class Node:
 
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        
+
+        '''Psedocode/ Plan
+        7 -> 7`-> 13 -> 13` -> 11 -> 11` -> 10 -> 10` -> 1 -> 1`
+
+        7 
+        deep_copy(7) -> create new node with val 7. 
+        copy.next = orig.next
+        orig.next = copy
+        orig = orig.next.next
+
+        traverse the orig nodes. 
+        (orig.next == its copy)
+        orig.next.random = orig.random.next if orig.random else None
+        orig = orig.next.next
+
+        Remove the orig nodes from the list. 
+        we start frop copyHead -> orighead.next
+        copy.next = copy.next.next
+        copy = copy.next.next
+        '''
         if not head:
             return None
-            
+
         orig = head
 
         while orig:
@@ -22,14 +41,20 @@ class Solution:
             orig = orig.next.next
         
         orig = head
+
         while orig and orig.next:
             orig.next.random = orig.random.next if orig.random else None
             orig = orig.next.next
         
-        copy = copy_head = head.next
-        
+        copyHead = head.next
+        copy = head.next
+
         while copy and copy.next:
             copy.next = copy.next.next
             copy = copy.next
         
-        return copy_head
+        return copyHead
+        
+
+
+
