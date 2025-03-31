346fa62ba413eccabf1c73c6717148aa18c2c8ef
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-03-31 12:05:52 -0400

    Time: 512 ms (20.13%), Space: 80.9 MB (16.77%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -1,23 +1,18 @@
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
-        min_price = prices[0] + fee
-        # Initialize the maximum profit as 0
-        max_profit = 0
+        @lru_cache
+        def dp(i, holding):
+            if i == len(prices):
+                return 0
+            
+            doNothing = dp(i+1, holding)
+            doSomething = 0
+
+            if holding:
+                doSomething = prices[i] - fee + dp(i+1, 0)
+            else:
+                doSomething = -prices[i] + dp(i+1, 1)
+            
+            return max(doSomething, doNothing)
         
-        # Iterate over each stock price
-        for price in prices:
-            # If the current price is greater than the minimum price,
-            # a profit can be made by selling the stock
-            if min_price < price:
-                # Add the difference between the current price and the minimum price
-                # to the maximum profit
-                max_profit += price - min_price
-                # Update the minimum price to the current price
-                min_price = price
-            # If the current price plus fee is less than the minimum price,
-            # update the minimum price to the current price plus fee
-            elif price + fee < min_price:
-                min_price = price + fee
-        
-        # Return the maximum profit
-        return max_profit
+        return dp(0, 0)
