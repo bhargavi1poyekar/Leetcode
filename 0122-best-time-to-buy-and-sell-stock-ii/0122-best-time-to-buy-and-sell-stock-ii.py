3a6228e8583fc0cc13873d6625cfb948ae4ed92c
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-03-31 12:18:30 -0400

    Time: 31 ms (5.04%), Space: 41.1 MB (5.29%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,12 +1,18 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        @lru_cache
+        def dp(i, holding):
+            if i == len(prices):
+                return 0
+            
+            doNothing = dp(i+1, holding)
+            doSomething = 0
 
-        profit=0
-
-        for i in range(len(prices)-1):
-
-            if prices[i+1]>prices[i]:
-                profit+=prices[i+1]-prices[i]
+            if holding:
+                doSomething = prices[i] + dp(i+1, 0)
+            else:
+                doSomething = -prices[i] + dp(i+1, 1)
+            
+            return max(doSomething, doNothing)
         
-        return profit
-       
+        return dp(0, 0)
