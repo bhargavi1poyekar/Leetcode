d5cbe8edad7e2807342e3b130282f6a3040e06a4
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-03-31 18:10:02 -0400

    Time: 0 ms (100%), Space: 17.7 MB (60.75%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,24 +1,70 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
+
+        '''
+        Understand:
+
+        Given: n
+
+        Todo:
+
+        check if n is happy.
+        conditions to be happy:
+        any pos int n -> replace by sum of squares of its digit. 
+        repeat this until 
+        either n == 1 or loop in cycle -> which doesnt have 1. 
+
+        if ends in n == 1 -> happy
+
+        Return : true if happy, false if not.  
+
+        cases: 
+            if n == 0: return False. 
+            if n -> single digit number -> return false. 
+
+        Match:
+
+        to check the cycle -> if the current number -> has been seen before -> we can use hash data structure. 
+
+        if not, it will end in n==1. 
+
+        Plan:
+
+        fun sum_of_square_of_digits():
+            first it will find digit by digit, and add its square to sum. 
+            return the sum. 
         
-        def sum_square_of_digits(n):
+        seen = set  -> any seen number is added to it. 
+        while n != 1:
+            if n in seen:
+                return False -> cycle detected. 
+            seen.add(n)
+            n = sum_of_square_of_digits(n)
+        
+        return True
+        '''
+
+        def sum_of_square_of_digits(n):
             sum_square = 0
 
             while n:
                 digit = n % 10
                 n = n // 10
                 sum_square += digit ** 2
-
+            
             return sum_square
-
-        hash_map = set()
+        
+        seen = set()
 
         while n != 1:
-            if n in hash_map:
-                break
-                
-            hash_map.add(n)
-            n = sum_square_of_digits(n)
+            if n in seen:
+                return False
+            seen.add(n)
+            n = sum_of_square_of_digits(n)
+
+        return True        
+
+
+
 
         
-        return n == 1
