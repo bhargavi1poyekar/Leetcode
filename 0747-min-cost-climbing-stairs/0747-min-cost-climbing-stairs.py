2723acd45c1b85ec5499e6055b5e1826b10f6856
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-04-01 10:51:22 -0400

    Time: 3 ms (73.68%), Space: 17.8 MB (63.25%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
@@ -1,6 +1,8 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        
+        # Top Down:
+
+        '''
         def dp(i):
             if i <= 1:
                 return 0
@@ -11,3 +13,15 @@ class Solution:
         
         memo = {}
         return dp(len(cost))
+        '''
+
+        # Bottom Up:
+        dp = [0] * (len(cost)+1)
+        dp[0] = 0
+        dp[1] = 0
+
+        for i in range(2, len(cost)+1):
+            dp[i] = min(dp[i-1] + cost[i-1], dp[i-2] + cost[i-2])
+
+        return dp[len(cost)]
+
