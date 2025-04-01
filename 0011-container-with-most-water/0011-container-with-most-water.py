bf00d8445b27e25eea9e3b3022f396cfb49c43c5
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-04-01 18:09:56 -0400

    Time: 106 ms (41.86%), Space: 28.5 MB (24.1%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,19 +1,38 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        
-        start = 0
-        end = len(height) - 1
-        max_area = 0  
+        '''
+        Understand:
 
-        while start < end:
-            hgt = min(height[start], height[end])
-            width = end - start
-            area = hgt * width
+        height -> len n -> n lines
+
+        return: max amt of water. 
+
+        what if height is empty -> no lines -> max amt -> 0?
+
+        Match: 
+        2 pointer -> start from both ends, and then start decreasing the width as we move ahead. 
+
+        Plan:
+
+        amt of water = > area of the container
+        area = width * height
+        width - > right - left + 1 -> left is ptr from left, and right starts from end. 
+
+        height => min(height[left], height[right])
+        ''' 
+
+        left = 0 
+        right = len(height)-1
+        max_area = 0
+        while left < right:
+            width = right - left
+            hgt = min(height[left], height[right])
+            area = width * hgt
             max_area = max(max_area, area)
-            
-            if height[start] >= height[end]:
-                end -= 1
+
+            if height[left] >= height[right]:
+                right -= 1
             else:
-                start += 1
+                left += 1
         
         return max_area
