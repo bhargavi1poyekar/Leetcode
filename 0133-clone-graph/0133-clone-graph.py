24d926360bdd57673d4c5d2f4216e697ca2bb130
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-04-01 18:01:08 -0400

    Time: 47 ms (32.63%), Space: 18.1 MB (57.95%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -6,26 +6,51 @@ class Node:
         self.neighbors = neighbors if neighbors is not None else []
 """
 
+'''
+Understand: Deep copy of graph, without making any references to original graph. All the neighbours should be the copy of the neighbors. 
+
+What is given graph is empty -> then return None. 
+
+Match:
+Graph -> BFS/DFS -> DFS
+
+PLan:
+hash -> clone of each nodes. 
+
+for every node -> that we traverse using dfs. 
+
+dfs(node):
+    if not node:
+        return None
+    if node in clone:
+        return clone[node]
+    
+    clone_node = Node(node.val)
+    clone[node] = clone_node
+
+    do the dfs for each neighbor.  
+'''
+
 from typing import Optional
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
         self.clone = {}
+
         def dfs(node):
             if not node:
-                return node
+                return None
             
             if node in self.clone:
                 return self.clone[node]
             
-            clone_node = Node(node.val, [])
+            clone_node = Node(node.val)
             self.clone[node] = clone_node
 
-            if node.neighbors:
-                clone_node.neighbors = [dfs(nghbr) for nghbr in node.neighbors]
-            
-            return clone_node
+            clone_node.neighbors = [dfs(nghbr) for nghbr in node.neighbors]
+
+            return self.clone[node]
         
         return dfs(node)
 
+
         
-    
