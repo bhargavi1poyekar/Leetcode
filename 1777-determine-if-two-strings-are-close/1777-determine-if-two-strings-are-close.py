86a056fc2e287b61aae23323644b3fa86e7326ac
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-04-02 19:18:12 -0400

    Time: 84 ms (75.73%), Space: 18.4 MB (20.99%) - LeetHub

--- 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.py
+++ 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.py
@@ -4,10 +4,11 @@ class Solution:
         word1_count = Counter(word1)
         word2_count = Counter(word2)
 
-        word1_keys = word1_count.keys()
-        word2_keys = word2_count.keys()
+        word1_val = sorted(word1_count.values())
+        word2_val = sorted(word2_count.values())
 
-        word1_vals = sorted(word1_count.values())
-        word2_vals = sorted(word2_count.values())
+        word1_key = word1_count.keys()
+        word2_key = word2_count.keys()
 
-        return word1_keys == word2_keys and word1_vals == word2_vals
+        return word1_key == word2_key and word1_val == word2_val
+
