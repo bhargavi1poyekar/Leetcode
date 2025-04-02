6900652d0abb2a17a88f1e0f6f7b4e6329829fa5
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-04-02 11:07:17 -0400

    Time: 0 ms (100%), Space: 17.9 MB (26.75%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,13 +1,13 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        oldrow = [1]*n
-        for i in range(m-1):
-            #for each row we are calculating newrow
-            newrow = [1]*n
-            #traverse through columns in reverse order excluding last column 
-            for j in range(n-2, -1, -1):
-                #at each postion 'i' answer is answer at 'i+1' block of same row + answer
-                #at ith position of bottom row 
-                newrow[j] = newrow[j+1] + oldrow[j]
-            oldrow = newrow
-        return oldrow[0]
+        dp = [[0] * n for _ in range(m)]
+        dp[0][0] = 1 # Base case
+        
+        for row in range(m):
+            for col in range(n):
+                if row > 0:
+                    dp[row][col] += dp[row - 1][col]
+                if col > 0:
+                    dp[row][col] += dp[row][col - 1]
+        
+        return dp[m - 1][n - 1]
