cf487fbf4fc628af8551d5f552a433069ba0c0ab
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-04-05 18:30:37 -0400

    Time: 0 ms (100%), Space: 19.2 MB (29.9%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -6,7 +6,34 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        
+        '''
+        Understand:
+
+        Given root -> return max depth. 
+        max_depth -> num nodes along longest path (root to leaf)
+
+        if no root -> 0
+        if only root -> 1
+
+        Match: Can do a dfs to find the depths. 
+
+        Plan:
+
+        dfs(root):
+
+            if not root:
+                return 0
+
+            left_depth = dfs(left)
+            rightdepth = dfs(right)
+
+            return 1 (adding depth of root) + max(left, right)
+
+        at any junction -> we have 2 choices -> take either left depth and consider that path, or take right depth with that path. 
+
+        So we choose max of it. 
+        '''
+        # Implement
         def dfs(root):
             if not root:
                 return 0
@@ -17,5 +44,24 @@ class Solution:
             return 1 + max(left_depth, right_depth)
         
         return dfs(root)
+
+        '''
+        Review:
+        since we are using postorder traversal. 
+        lets start from leaf nodes. 
+
+        9 -> returns 1 + max(0, 0) -> 1
+        15 -> returns 1 + max(0, 0) -> 1
+        7 -> returns 1 + max(0, 0) -> 1
+
+        20 -> returns 1 + max(1, 1) -> 2
+
+        root -> 3 returns 1 + max(1, 2) -> 3
+
+        Evaluate:
+        Time Complexity: O(n) -> we traverse each node. 
+        Space Complexity: O(1) -> O(n) -> stack overhead in recursion.
+        '''
+
 
 
