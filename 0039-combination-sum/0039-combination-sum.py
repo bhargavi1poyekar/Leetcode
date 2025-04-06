6eb310faf1f603244a5a5292a29503bd46356918
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-04-06 11:52:00 -0400

    Time: 15 ms (29.96%), Space: 17.9 MB (61.86%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,18 +1,52 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        
-        def backtrack(i, curr):
+        '''
+        Understanding:
+
+        arr -> distinct integers and target
+        return : list of all unique combinations of candidates where they sum to target. 
+
+        return in any order. 
+        same number -> unlimited number of times but combination should be unique. 
+
+        test cases -> less than 150 combinations. 
+
+        Match: Can use Backtracking -> input is small. 
+        unique combinations. but same number multiple times. 
+
+        Plan:
+
+        function -> index (curr index position), curr combination
+
+        first base case -> curr sum > target
+        curr combination sum == target
+        curr sum > target -> return 
+        if curr combination sum == target: 
+            add it to all combinations and then return. 
+
+        index wont be out of boundary -> because we are sending curr i -> which is always inside boundary.  (i, len(candidates))
+
+        Then again loop over index till end of candidates. 
+        add curr num to combination, and call recursive function. 
+
+        backtrack by removing the current num from the combination. 
+        '''
+
+        # Implement
+
+        def backtrack(curr_i, curr):
             if sum(curr) == target:
-                ans.append(curr[:])
-                return 
+                combinations.append(curr[:])
+                return
+
             if sum(curr) > target:
-                return 
-            
-            for num in range(i, len(candidates)):
-                curr.append(candidates[num])
-                backtrack(num, curr)
+                return
+
+            for i in range(curr_i, len(candidates)):
+                curr.append(candidates[i])
+                backtrack(i, curr)
                 curr.pop()
         
-        ans = []
+        combinations = []
         backtrack(0, [])
-        return ans
+        return combinations
