7a2a996483dc9c3eb67856b0ffdc1ca4424f2f36
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-04-08 11:52:46 -0400

    Time: 0 ms (100%), Space: 18.1 MB (42.28%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -19,14 +19,17 @@ class Solution:
         while left <= right:
             mid = (left + right) // 2
             if nums[mid] > nums[-1]:
-                left = mid + 1
+                left = mid + 1 
             else:
                 right = mid - 1
         
         pivot = left
+        # print(pivot)
 
-        left_search = binary_search(0, pivot-1)
+        left_search = binary_search(0, pivot - 1)
         if left_search != -1:
             return left_search
         return binary_search(pivot, len(nums)-1)
+        
+
 
