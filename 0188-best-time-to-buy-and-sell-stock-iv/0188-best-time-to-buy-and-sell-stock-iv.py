021c8daf9e6f189ae0df5c3fd79f8f0c484b1536
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-04-09 13:44:33 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
@@ -1,22 +1,20 @@
 class Solution:
     def maxProfit(self, k: int, prices: List[int]) -> int:
-        @lru_cache(None)
-        def dp(i, transactions_remaining, holding):
-            # Base case
-            if transactions_remaining == 0 or i == len(prices): 
+        
+        @lru_cache
+        def dp(i, holding, remainT):
+            if i >= len(prices) or remainT == 0:
                 return 0
             
-            do_nothing = dp(i + 1, transactions_remaining, holding)
-            do_something = 0
+            doNothing = dp(i+1, holding, remainT)
 
             if holding:
-                # Sell stock
-                do_something = prices[i] + dp(i + 1, transactions_remaining - 1, 0)
+                # Sell
+                doSomething = dp(i+1, False, remainT-1) + prices[i]
             else:
-                # Buy stock
-                do_something = -prices[i] + dp(i + 1, transactions_remaining, 1)
-
-            # Recurrence relation
-            return max(do_nothing, do_something)
-
-        return dp(0, k, 0)
+                # Buy
+                doSomething = dp(i+1, True, remainT) - prices[i]
+            
+            return max(doNothing, doSomething)
+        
+        return dp(0, False, k)
