ba422f2a6b0c2c65d04ec051dca2d10ea51e10d0
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-04-09 11:48:07 -0400

    Time: 76 ms (7.55%), Space: 17.7 MB (75.47%) - LeetHub

--- 3621-minimum-operations-to-make-array-values-equal-to-k/3621-minimum-operations-to-make-array-values-equal-to-k.py
+++ 3621-minimum-operations-to-make-array-values-equal-to-k/3621-minimum-operations-to-make-array-values-equal-to-k.py
@@ -21,9 +21,16 @@ class Solution:
         if k > min(value) -> not possible. 
 
         do this, till only 1 distinct value. 
-        find max -> set h = second max -> and then convert all occ of max to h.     
+        find max -> set h = second max -> and then convert all occ of max to h. 
+
+        # Note: -> there is a trick to the question:
+        if k is less tha min value -> then it is not possinle to vonvert to k. 
+        But if k is present in the arr -> then we just need distinct_val - k operation, that is we convert all the numbers greater than k to k. Step by step. 
+
+        So inshort it is just asking us how many distinct elements are greater than k.     
         '''
 
+        # Approach 1
         hash_counter = Counter(nums)
 
         distinct_vals = len(hash_counter)
@@ -34,6 +41,15 @@ class Solution:
         if k in hash_counter:
             return distinct_vals - 1
         return distinct_vals
+
+        # Approach 2
+        count = 0
+        
+        for num in hash_counter:
+            if num > k:
+                count += 1
+        
+        return count
         
 
         
