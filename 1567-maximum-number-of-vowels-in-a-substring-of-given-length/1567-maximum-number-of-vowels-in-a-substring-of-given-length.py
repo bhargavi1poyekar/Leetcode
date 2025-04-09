9f1d47ebcc65091b6e9b3801ed3db1a91208da92
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-04-09 15:58:23 -0400

    Time: 79 ms (61.41%), Space: 18 MB (88.43%) - LeetHub

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -1,27 +1,47 @@
 class Solution:
     def maxVowels(self, s: str, k: int) -> int:
         
-        vowels = set(['a', 'e', 'i', 'o', 'u'])
+        '''
+        Understand:
+
+        given -> s and k 
+        return max vowel chars in any sub of length k. 
+
+        Match:
+        fixed length sliding window. 
+
+        Plan:
+        first fill the substring till length k. 
+        inc the vowel count if any. 
+
+        update max vowel count. 
+
+        Now after k, inc left and right. 
+        and adjust vowel count accpdingly. 
+
+        At every point. udpate the max vowel count. 
+        '''
+
         left = 0
         vowel_count = 0
-        max_vow_count = 0
+        vowels = {'a', 'e', 'i', 'o', 'u'}
 
-        for right in range(k):
-            if s[right] in vowels:
+        max_vowel = 0
+
+        for i in range(k):
+            if s[i] in vowels:
                 vowel_count += 1
         
-        max_vow_count = vowel_count
+        max_vowel = max(max_vowel, vowel_count)
 
         for right in range(k, len(s)):
             if s[right] in vowels:
                 vowel_count += 1
-            
             if s[left] in vowels:
                 vowel_count -= 1
-            
+
             left += 1
-            max_vow_count = max(max_vow_count, vowel_count)
+            
+            max_vowel = max(max_vowel, vowel_count)
         
-        return max_vow_count
-        
-        
+        return max_vowel
