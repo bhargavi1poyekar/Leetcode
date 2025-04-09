2f1b1995fb2ccfd095806aa93abfdfb46341c22e
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-04-09 15:37:31 -0400

    Time: 0 ms (100%), Space: 18.6 MB (5.02%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,15 +1,33 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        left = 0
-        right = len(nums) - 1
+        
+        '''
+        Understand:
 
-        while left <= right:
+        Given sorted arr and target val. 
+        return index if target is found. 
+        else return where it would be inserted. 
+
+        Match:
+        Binary Search - > insert position. 
+        '''
+
+        left = 0
+        right = len(nums) 
+        ''' 
+        we dont take len(nums)-1 because if target not found, 
+        we need to find insert position. 
+        there is possibility, that it needs to be inserted at the end. 
+        SO we require extra index. 
+        '''
+
+        while left < right:
             mid = (left + right) // 2
-            if nums[mid] == target:
-                return mid
-            elif nums[mid] > target:
-                right = mid - 1
+            if nums[mid] >= target:
+                right = mid
             else:
                 left = mid + 1
         
         return left
+
+
