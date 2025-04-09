3e7beb9bb0c95e11027ebd38413597a7f130d2c0
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-04-09 15:09:59 -0400

    Time: 127 ms (73.16%), Space: 33.1 MB (61.17%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.py
+++ 0221-maximal-square/0221-maximal-square.py
@@ -1,14 +1,15 @@
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
-        rows = len(matrix)
-        cols = len(matrix[0]) if rows else 0
-        dp = [[0] * (cols + 1) for _ in range(rows + 1)]
-        maxsqlen = 0
-        # for convenience, we add an extra all zero column and row
-        # outside of the actual dp table, to simpify the transition
-        for i in range(1, rows + 1):
-            for j in range(1, cols + 1):
-                if matrix[i - 1][j - 1] == "1":
-                    dp[i][j] = min(dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1])+1
-                    maxsqlen = max(maxsqlen, dp[i][j])
-        return maxsqlen * maxsqlen
+        
+        Row = len(matrix)
+        Col = len(matrix[0]) if Row else 0
+        dp = [[0] * (Col+1) for _ in range(Row+1)]
+        max_side = 0
+
+        for i in range(1, Row+1):
+            for j in range(1, Col+1):
+                if matrix[i-1][j-1] == "1":
+                    dp[i][j] = min(dp[i-1][j-1], dp[i][j-1], dp[i-1][j]) + 1
+                    max_side = max(max_side, dp[i][j])
+        
+        return max_side * max_side
