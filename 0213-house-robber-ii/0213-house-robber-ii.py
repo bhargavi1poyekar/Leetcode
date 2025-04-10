cf7a74da4c84f32e61c568227c2ae5f904697db2
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-04-10 13:30:23 -0400

    Time: 3 ms (10.69%), Space: 17.8 MB (58.54%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -8,15 +8,26 @@ class Solution:
         
         if len(nums) == 2:
             return max(nums[0], nums[1])
-        
-        def rob_without_circle(nums):
+
+        def regular_house_robber(nums):
+            # def dp(i):
+            #     if i == 0:
+            #         return nums[0]
+            #     if i == 1:
+            #         return max(nums[0], nums[1])
+                
+            #     if i not in memo:
+            #         memo[i] = max(dp(i-1), dp(i-2) + nums[i])
+                
+            #     return memo[i]
+
             dp = [0] * len(nums)
             dp[0] = nums[0]
             dp[1] = max(nums[0], nums[1])
 
             for i in range(2, len(nums)):
                 dp[i] = max(dp[i-1], dp[i-2] + nums[i])
-            
+
             return dp[len(nums)-1]
         
-        return max(rob_without_circle(nums[:-1]), rob_without_circle(nums[1:]))
+        return max(regular_house_robber(nums[1:]), regular_house_robber(nums[:-1]))
