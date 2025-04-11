24341590160771f71d4d00d20d6e72f700e6aa2c
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-04-10 20:06:35 -0400

    Time: 15 ms (63.77%), Space: 23.1 MB (43.22%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
@@ -7,16 +7,36 @@
 class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
         
+        '''
+        Understand:
+        Given -> root. 
+
+        we need to find max path sum -> path does not need to go through root.
+
+        Values can be negative. 
+
+        Match-> Tree dfs. 
+
+        Plan:
+
+        at every node. we check what maxsum we get from left, and what from right.
+        If maxsum from left or right is negative, we just keep it as 0. 
+
+        Then update maxpath with max of curr path sum. 
+        curr path is like leftGain + rightGain + root.val
+
+        now we return the maxpath from either left, or right.
+        '''
+        
         self.maxpath = float('-inf')
+
         def dfs(root):
             if not root:
                 return 0
             
             leftGain = max(0, dfs(root.left))
             rightGain = max(0, dfs(root.right))
-
-            self.maxpath = max(self.maxpath, leftGain + root.val + rightGain)
-
+            self.maxpath = max(self.maxpath, leftGain + rightGain + root.val)
             return max(leftGain + root.val, rightGain + root.val)
         
         dfs(root)
