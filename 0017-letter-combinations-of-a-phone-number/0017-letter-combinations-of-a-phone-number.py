0679af9cedde87d76759fca4cd15633974924446
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-04-12 14:50:04 -0400

    Time: 3 ms (4.83%), Space: 18 MB (23.13%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,14 +1,33 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        
+        '''
+        Understand:
+
+        string with Digits -> 2 - 9
+        Return letter combinations that number could represent. 
+
+        Match:
+        Combinations -> backtracking. 
+
+        Plan:
+
+        Hash -> digits with letters. 
+
+        send index, and current combination as parameters. 
+        if index == len(digits):
+            append combination to answers. 
+
+        traverse over hash of curr digit
+        add it to the curr, call backtrack, and remove from curr. 
+        '''
         if not digits:
             return []
-            
+
         def backtrack(curr, i):
-            if len(curr) == len(digits):
+            if i == len(digits):
                 ans.append(''.join(curr))
                 return 
-
+            
             for letter in hash[digits[i]]:
                 curr.append(letter)
                 backtrack(curr, i+1)
