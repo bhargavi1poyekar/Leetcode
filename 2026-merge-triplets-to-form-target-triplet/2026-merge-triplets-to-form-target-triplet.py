861cdd572b75d47a109b53902102bae7b7274579
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-04-12 18:11:32 -0400

    Time: 27 ms (62.11%), Space: 55.1 MB (56.76%) - LeetHub

--- 2026-merge-triplets-to-form-target-triplet/2026-merge-triplets-to-form-target-triplet.py
+++ 2026-merge-triplets-to-form-target-triplet/2026-merge-triplets-to-form-target-triplet.py
@@ -1,17 +1,13 @@
 class Solution:
     def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
-
-        valid_trip=[0]*3
+        max_triplet = [0] * 3
         for t in triplets:
-            # print(t,target)
-            if t[0]>target[0] or t[1]>target[1] or t[2]>target[2]:
+            if t[0] > target[0] or t[1] > target[1] or t[2] > target[2]:
                 continue
             
             for i in range(3):
-                if t[i]==target[i]:
-                    valid_trip[i]=t[i]
-
-           
-        return valid_trip==target
-
-
+                if t[i] == target[i]:
+                    max_triplet[i] = t[i]
+        
+        return max_triplet == target
+                    
