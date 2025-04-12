d790db27b1f16591cfbfc1f267de51a775e1a28f
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-04-12 14:03:16 -0400

    Time: 0 ms (100%), Space: 17.8 MB (38.52%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -5,24 +5,48 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        
-        curr_ptr = newhead = ListNode()
+        '''
+        Understand:
 
-        while list1 and list2:
-            if list1.val <= list2.val:
-                curr_ptr.next = list1
-                curr_ptr = curr_ptr.next
-                list1 = list1.next
+        Given: 2 sorted lists -> list1 and list2. 
+        Merge them into 1 sorted list. 
+
+        Can there be duplicates in both lists? -> Yes
+        Numbers are > 0 ?
+        can the lists be empty -> yes
+
+        Match:
+        two pointers -> one for each list
+
+        Plan:
+        two pointers. 
+        new head.
+        till you reach at end of either of list:
+            compare the values for both pointers. 
+            Join the pointer with lower value to new head
+            and increment that pointer. 
+
+        Once you reach the end of one of the list: then just join the remaining list
+        '''
+
+        l1 = list1
+        l2 = list2
+
+        newhead = h = ListNode()
+
+        while l1 and l2:
+            if l1.val <= l2.val:
+                h.next = l1
+                l1 = l1.next
             else:
-                curr_ptr.next = list2
-                curr_ptr = curr_ptr.next
-                list2 = list2.next
-            
-        if list1:
-            curr_ptr.next = list1
-        elif list2:
-            curr_ptr.next = list2
+                h.next = l2
+                l2 = l2.next
+            h = h.next
+        
+        if l1:
+            h.next = l1
+        
+        if l2:
+            h.next = l2
         
         return newhead.next
-
-
