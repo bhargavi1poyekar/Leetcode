2744622b8d8192da2e2418fefa0340c3c8180235
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-04-13 10:44:13 -0400

    Time: 58 ms (35.98%), Space: 22.1 MB (45.04%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -8,18 +8,50 @@
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
 
-        if not root:
-            return None
-        if p == root or q == root:
-            return root
+        '''
+        Understand:
 
-        left = self.lowestCommonAncestor(root.left, p, q)
-        right = self.lowestCommonAncestor(root.right, p, q)
+        Given binary tree -> return lowest common ancestor of given nodes. 
+        lowest node, that has both p and q as descendants. 
 
-        if left and right:
-            return root
-        elif left:
-            return left
-        return right
+        Match:
+        dfs
+
+        Plan:
+        ok, so first of all, if any of the p, and q is root -> then it will be the lca -> because -> node can be descendant of itself. 
+
+        now, if none of them is root. 
+        we check in the left tree. 
+        and check in right tree. 
+
+        Now on both sides, if we get something. then one of them is in left and other in right. -> so we return root. 
+
+        if only left, then both are on left, we return left. 
+        otherwise right. 
+        '''
+
+        def dfs(root):
+            if not root:
+                return 
+            
+            if p == root or q == root:
+                return root
+            
+            left = dfs(root.left)
+            right = dfs(root.right)
+
+            if left and right:
+                return root
+            if left:
+                return left
+            return right
+    
+        return dfs(root)
+
+        '''
+        Time Complexity -> O(n)
+        Space -> O(n) -> stack overhead.
+        '''
+            
 
         
