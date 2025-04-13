f76272f05d6e4993d4182478778cc6965fa12ea1
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-04-13 10:36:59 -0400

    Time: 0 ms (100%), Space: 17.7 MB (59.3%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.py
+++ 0290-word-pattern/0290-word-pattern.py
@@ -1,22 +1,49 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
         
-        s_list = s.split(' ')
-        
-        if len(s_list) != len(pattern):
+        '''
+        Understand:
+
+        Given pattern and string.
+
+        each letter -> maps to unique word in s
+        unique word -> maps to exactly one letter. 
+
+        no one to many, many to one. 
+
+        Match: Hash to map the letters and word to each other.
+
+        Plan:
+        if num words != num_letters -> then return False
+
+        s_map and p_map. 
+
+        Now while traversing s and p. 
+        check if s[i] mapped to p[i] and p[i] mapped to s[i]. 
+        if not, return false. 
+
+        if both dont exist in hash yet, add the mapping. 
+
+        at end, if already not returned false, then return True.  
+        '''
+
+        # Implementation
+        s = s.split(' ')
+        s_map = defaultdict()
+        p_map = defaultdict()
+
+        # S and pattern length doest match -> mapping wont match
+        if len(s) != len(pattern):
             return False
         
-        s_map = defaultdict()
-        pattern_map = defaultdict()
-
-        # print(pattern_list)
-
-        for i in range(len(s_list)):
-            if s_list[i] not in s_map and pattern[i] not in pattern_map:
-                s_map[s_list[i]] = pattern[i]
-                pattern_map[pattern[i]] = s_list[i]
+        for i in range(len(s)):
+            # If both are not already map, we map them
+            if s[i] not in s_map and pattern[i] not in p_map:
+                s_map[s[i]] = pattern[i]
+                p_map[pattern[i]] = s[i]
             
-            elif s_map.get(s_list[i]) != pattern[i] or pattern_map.get(pattern[i]) != s_list[i]:
+            # if one of them is mapped -> and mapped to some other character
+            elif s[i] in s_map and s_map[s[i]] != pattern[i] or pattern[i] in p_map and p_map[pattern[i]] != s[i]:
                 return False
         
         return True
