036f7a3662c6c8744b06cf6b0f113ec73b044e61
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-04-13 10:58:15 -0400

    Time: 0 ms (100%), Space: 17.7 MB (81.41%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.py
+++ 0191-number-of-1-bits/0191-number-of-1-bits.py
@@ -1,13 +1,11 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        set_bits = 0
+        
         mask = 1
-
-        # n & mask -> what this does is  -> at every digit -> it tries to & with 1 and if if gives 0 -> then it is not set. (every other bit will anyway result into 0 because we are & them with 0, only the current digit that we are checking is & with 1 -> so if 0 -> not set, if 1 -> set)
-
-        # then mask is left shifted to move to next digit.
-        for _ in range(32):
-            if (n & mask) != 0:
-                set_bits += 1
+        num_set = 0
+        while n:
+            if n & mask:
+                num_set += 1
             n >>= 1
-        return set_bits
+
+        return num_set
