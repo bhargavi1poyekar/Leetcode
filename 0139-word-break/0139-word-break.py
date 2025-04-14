88b44f207ebab81ce42bcc42b60bd4ad9fa3a68c
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-04-14 10:51:26 -0400

    Time: 3 ms (69.28%), Space: 17.9 MB (45.6%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,20 +1,43 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        # @lru_cache(None)
+        '''
+        Given -> string s and dictionary of strings. wordDict. 
+        Return true if s -> segmented into space separated sequen of one or more(not all) dictionary words. 
+
+        we can use same words -> multiple times. 
+
+        Match: Dynamic programming
+
+        Plan:
+        function: -> dp(i) -> i being the current index of the string that we are matching. 
+        dp(i) -> return true if that index character can be found in the wordDict words.
+
+        Relation:
+        dp(i) -> will be true, when for words in wordDict:
+            dp[i - len(word)] -> also true. and s[i - len(word) + 1: i+1] == matches the word.  
+        
+        Base Case: if i < 0 -> return True.
+        '''
+
+        # Implementation:
+
         def dp(i):
-            if i < 0: 
+            if i < 0:
                 return True
+
             if i in memo:
                 return memo[i]
             
             for word in wordDict:
-                if (i >= len(word) - 1) and dp(i - len(word)):
-                    if s[i - len(word) + 1 : i + 1] == word:
+                if i >= len(word)-1 and dp(i - len(word)):
+                    if s[i - len(word) + 1: i+1] == word:
                         memo[i] = True
                         return memo[i]
-            
+                
             memo[i] = False
             return memo[i]
         
         memo = {}
-        return dp(len(s) - 1)
+        return dp(len(s)-1)
+                
+            
