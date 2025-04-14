cedbed181374d3256aebbc410c318b249d7e04be
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-04-14 10:35:06 -0400

    Time: 7 ms (58.46%), Space: 19.1 MB (19.94%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -1,27 +1,98 @@
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
-        
-        stack = []
+        '''
+        Understand: 
+
+        Given arr -> asteroids -> integers. 
+        indices -> relative position 
+
+        abs val -> size, 
+        sign -> direction.  + means right, - means left. 
+
+        speed is same for all. 
+
+        conditions of collision:
+        1. if 2 meet -> smaller will explode
+        2. if both same -> both will explode. 
+        3. asteroids in same direction will never meet. 
+
+        Never meet
+        + +  
+        - - 
+        - + -> opposite direction away from each other. 
+
+        Only asteroids -> + -
+
+        Can size of asteroid be 0 -> ? no
+
+        Match: Stack -> LIFO to get the previous asteroids for collision. 
+
+        Plan:
+
+        we are only concerned about the + - negative collision. 
+
+        explode = False
 
         for ast in asteroids:
-            # print(stack)
-            explode = False
-            while stack and (ast < 0 and stack[-1] > 0):
-                # print(stack, ast)
-                if stack[-1] > abs(ast):
-                    explode = True
+            while curr ast is negative and previous(stack top) is positive:
+                stack top size > curr asteroid
+                    explode = true
                     break
-                elif stack[-1] == abs(ast):
+                stack top size == curr asteroid
                     stack.pop()
                     explode = True
                     break
-                elif stack[-1] < abs(ast):
+                
+                stack top < curr aster
+                    stack.pop
+                    explode = False
+                
+            if not explode:
+                stack.append(curr asteroid)
+        '''
+
+        # Implement:
+        stack = []
+        
+        for ast in asteroids:
+            explode = False # initially the curr asteroid is set to not explode on collision
+
+            # while curr asteroid and previous asteroid are moving towards each other
+            while ast < 0 and stack and stack[-1] > 0:
+
+                # get the size of both asteroids
+                ast_size = abs(ast)
+                prev_size = abs(stack[-1])
+
+                # If prev ast size greater than current, current will explode.
+                if prev_size > ast_size:
+                    explode = True
+                    break
+
+                # If both equal size, then both explode
+                elif prev_size == ast_size:
+                    explode = True
                     stack.pop()
-                    # explode = False
+                    break
+                
+                # if prev is smaller in size, only it will explode. 
+                else:
+                    stack.pop()
             
-            if explode == False:
+            # if explode is set to False, append it to stack
+            if not explode:
                 stack.append(ast)
         
+        # return remaining asteroids
         return stack
 
-                
+        '''
+        Time COmplexity: O(n)
+        Space Complexity: O(n)
+        '''
+        
+            
+
+
+
+
