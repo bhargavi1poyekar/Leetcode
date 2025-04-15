45f25657d8e5113781dda7c15aba8d0801fc21d7
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-04-15 19:31:17 -0400

    Time: 0 ms (100%), Space: 17.8 MB (66.06%) - LeetHub

--- 0086-partition-list/0086-partition-list.py
+++ 0086-partition-list/0086-partition-list.py
@@ -6,24 +6,41 @@
 class Solution:
     def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
         
-        small = shead = ListNode()
-        big = bhead = ListNode()
+        '''
+        Understand: 
+        Given head of LL and val x
+        partition such that all nodes less than x -> come before nodes greater or equal to x. 
 
+        Can the list be empty -> Yes
+        Can the list have duplicate values -> Yes
+        Can the list have negative integers -> Yes
+
+        Match: 2 pointers. one to start linking all nodes less than x 
+        and other to start linking all nodes greater/equal to x. 
+
+        Plan:
+        we traverse the list. 
+        we have 2 pointers. small, and large. 
+        So all nodes smaller to x -> attached to small, 
+        and nodes larger/equal to x -> attached to large. 
+
+        AT the end -> we join small linked list with larger linked list. 
+        '''
+
+        shead = small = ListNode()
+        lhead = large = ListNode()
         curr = head
 
         while curr:
-            if curr.val < x:
-                small.next = curr
-                small, curr = small.next, curr.next
+            if curr.val >= x:
+                large.next = curr
+                large = large.next
             else:
-                big.next = curr
-                big, curr = big.next, curr.next
+                small.next = curr
+                small = small.next
+            curr = curr.next
         
-        small.next = bhead.next
-        big.next = None
-    
+        small.next = lhead.next
+        large.next = None
         return shead.next
 
-        
-
-                
