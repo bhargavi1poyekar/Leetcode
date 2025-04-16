96c7a24b8e5475541f77576671f97bd0be144af1
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-04-15 22:12:38 -0400

    Time: 0 ms (100%), Space: 17.7 MB (56.19%) - LeetHub

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -1,24 +1,49 @@
 class Solution:
     def decodeString(self, s: str) -> str:
-        
+        '''
+        Understand:
+
+        Given -> encoded string
+        k[encoded_string] -> encoded string -> repeated k times. k-> pos integer.
+
+        input-> always valid. No white spaces. brackets are valid. No digits in that. 
+
+        all chars -> lowercase. 
+
+        Match: Stack -> to get the string to be repated, and how many times. 
+
+        Plan:
+
+        We have stack.
+
+        While we have a digit -> we keep on creating the complete number. op = op*10 + num
+
+        When we get [ open bracket -> we push the op to stack, and also push an empty string. 
+
+        When we get a char -> we append it to top of stack
+
+        When open bracket -> pop string first, then k. Multiple k with string, and append back to stack. 
+        '''
+
         stack = [""]
         op = 0
-        curr_str = ""
+
         for ch in s:
-            if ch.isalpha():
-                stack[-1] += ch
+            if ch == ']':
+                string = stack.pop()
+                k = stack.pop()
+                stack[-1] += k*string
             elif ch == '[':
                 stack.append(op)
                 stack.append("")
                 op = 0
-            elif ch == ']':
-                string = stack.pop()
-                count = stack.pop()
-                stack[-1] += string*count    
+            elif ch.isalpha():
+                stack[-1] += ch
             else:
                 op = op*10 + int(ch)
-        
-        return stack[-1]
 
-        
+        return stack[0]
+
+
+            
         
