206e8edcee49cfd565b162826debf1e554d5f8f0
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-04-16 15:20:19 -0400

    Time: 11 ms (83.13%), Space: 21.5 MB (37.91%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,14 +1,26 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         
-        anagrams = defaultdict(list)
+        '''
+        Understand:
 
+        Given: arr of strs -> group anagrams. 
+        Return in any order. 
+
+        Match: Hash -> sort the string and keep it as a key in hash. Then sort
+        '''
+
+        anagrams = defaultdict(list)
         for string in strs:
-            anag = ''.join(sorted(string))
-            anagrams[anag].append(string)
+            sorted_str = sorted(string)
+            anagrams[tuple(sorted_str)].append(string)
         
-        answer = []
-        for anagram in anagrams:
-            answer.append(anagrams[anagram])
+        ans = []
+        for ang in anagrams:
+            ans.append(anagrams[ang])
         
-        return answer
+        return ans
+
+
+
+
