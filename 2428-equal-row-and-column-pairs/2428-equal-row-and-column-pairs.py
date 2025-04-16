2f46e9d69760d18d5696048660cb97729956009f
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-04-16 14:34:43 -0400

    Time: 17 ms (80.01%), Space: 21.8 MB (95.65%) - LeetHub

--- 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.py
+++ 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.py
@@ -1,22 +1,47 @@
 class Solution:
     def equalPairs(self, grid: List[List[int]]) -> int:
         
-        row_count = Counter()
-        col_count = Counter()
+        '''
+        Understand:
 
+        Given: 0-indexed n x n int matrix -> grid. 
+        Return num pairs r,c -> such that row and column are equal.
+
+        same elements in same order -> equal. 
+
+        Match:
+        To check if duplicates -> hash. 
+
+        Plan:
+        We need to have row_hash -> with entire row as key, and num of times its is repepated as val. 
+        Samae for column. 
+
+        Now, at end, number of Pairs -> num rows * num cols -> that are equal. 
+
+        SO we just traverse over row hash -> and multiply it with count of col for the same val. if no val -> then it will be multiplied by 0, hence that pair won't count. 
+        '''
+
+        # Implementation:
+
+        row_hash = Counter()
         for row in grid:
-            row_count[tuple(row)] += 1
+            row_hash[tuple(row)] += 1
         
+        col_hash = Counter()
         for col in range(len(grid)):
             column = []
             for row in range(len(grid)):
                 column.append(grid[row][col])
 
-            col_count[tuple(column)] += 1
+            col_hash[tuple(column)] += 1
+
+        num_pairs = 0
+        for row in row_hash:
+            num_pairs += row_hash[row] * col_hash[row]
         
-        pairs = 0
-
-        for row in row_count:
-            pairs += row_count[row] * col_count[row]
-
-        return pairs
+        return num_pairs
+            
+        '''
+        Time Complexity: O(N*N)
+        Space Complexity: O(N*N)
+        '''
