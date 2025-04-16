f975c6a4d847449d4ad3f8a7020e324a3ed8a500
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-04-16 14:54:24 -0400

    Time: 0 ms (100%), Space: 17.8 MB (55.21%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -6,12 +6,31 @@
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
         
-        dummy = ListNode()
+        '''
+        Understand:
+
+        Given -> head of linked list. 
+        Remove -> nth node from end of list. 
+        Return head. 
+
+        Match:
+        slow and fast pointer.
+
+        Plan:
+        we have slow and fast at head. 
+
+        We also create dummy senteniel node -> since nth node cna be head as well. 
+
+        Then we first start fast -> and keep a gap of n elements between fast and slow. So when fast reached end. slow reaches at nth node from end. 
+
+        we also keep a prev node behind slow. So once slow reaches at nth node from end. we can connect prev with the next of slow. 
+        '''
+
+        prev = dummy = ListNode()
         dummy.next = head
-        prev = dummy
         slow = fast = head
 
-        while fast and n > 0:
+        while n and fast:
             fast = fast.next
             n -= 1
         
@@ -22,3 +41,10 @@ class Solution:
         
         prev.next = slow.next
         return dummy.next
+
+        '''
+        Time Complexity: O(N)
+        Space Complexity: O(1)
+        '''
+
+        
