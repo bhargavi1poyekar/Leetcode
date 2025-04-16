9139b74ebdaee6598ff5cf5d42dc38ebcb6bb739
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-04-16 14:25:34 -0400

    Time: 3 ms (89.28%), Space: 19.1 MB (42.01%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,6 +1,29 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        
+        '''
+        Understand -> n cities
+        Some are connected, some are not
+
+        Transitive Connection. 
+
+        Province -> connceted cities. 
+
+        Given: n * n matrix -> isConnected -> i j = 1 if i and j are connected. 
+
+        else 0
+
+        Return total provinces. 
+
+        Match: Do Graph DFS traversal. Everytime new dfs -> increase num provinces. 
+
+        Plan:
+        First create graph of neighbors based on the given matrix. 
+
+        Then do dfs, and maintain seen set to store nodes already seen. 
+
+        Iterate over the cities, and if not in seen: we start dfs with that city. Go to its neighbors, add them into seen, and do this recursively. 
+        '''
+
         graph = defaultdict(list)
 
         for i in range(len(isConnected)):
@@ -8,20 +31,27 @@ class Solution:
                 if isConnected[i][j]:
                     graph[i].append(j)
                     graph[j].append(i)
-        
-        num_province = 0
+            
+        seen = set()
 
         def dfs(node):
             for nghbr in graph[node]:
                 if nghbr not in seen:
                     seen.add(nghbr)
                     dfs(nghbr)
-
-        seen = set()
-        for node in graph:
-            if node not in seen:
-                seen.add(node)
+        
+        num_province = 0
+        for city in range(len(isConnected)):
+            if city not in seen:
                 num_province += 1
-                dfs(node)
+                seen.add(city)
+                dfs(city)
         
         return num_province
+
+        '''
+        Time Complexity: O(n)
+        Space Complexity: O(n) -> seen
+        '''
+
+        
