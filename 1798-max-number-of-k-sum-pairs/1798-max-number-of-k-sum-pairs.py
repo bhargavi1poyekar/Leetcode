1febfe01006d64da29a2833c532d36c4c69ac234
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-04-16 14:40:45 -0400

    Time: 499 ms (77.18%), Space: 30.1 MB (50.19%) - LeetHub

--- 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.py
+++ 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.py
@@ -1,20 +1,44 @@
 class Solution:
     def maxOperations(self, nums: List[int], k: int) -> int:
+        
+        '''
+        Understand:
+
+        Given: nums and k
+
+        in 1 operation -> pick 2 numbers -> whose sum == k and remove them. 
+
+        Return max operations possible. 
+
+        Match: Sort and 2 pointers. 
+
+        Plan
+        Sort the array. -> have 2 pointers -> left and right
+
+        if left + right num == k:
+            numop += 1
+        elif > k:
+            right -= 1
+        else:
+            left += 1
+        '''
+
         nums.sort()
 
         left = 0
-        right = len(nums) - 1
+        right = len(nums)-1
 
-        max_op = 0
+        num_op = 0
 
         while left < right:
-            if nums[left] + nums[right] == k:
-                max_op += 1
+            total = nums[left] + nums[right]
+            if total == k:
+                num_op += 1
                 left += 1
-                right -=1
-            elif nums[left] + nums[right] < k:
-                left += 1
-            else:
                 right -= 1
-        
-        return max_op
+            elif total > k:
+                right -= 1
+            else:
+                left += 1
+            
+        return num_op
