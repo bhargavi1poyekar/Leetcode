0011127986b674e164e6bce657bb614d04638ab8
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-04-17 15:14:14 -0400

    Time: 113 ms (14.14%), Space: 21.3 MB (84.86%) - LeetHub

--- 0954-maximum-sum-circular-subarray/0954-maximum-sum-circular-subarray.py
+++ 0954-maximum-sum-circular-subarray/0954-maximum-sum-circular-subarray.py
@@ -1,24 +1,31 @@
 class Solution:
     def maxSubarraySumCircular(self, nums: List[int]) -> int:
-        # Helper function to find max subarray sum using Kadane's algorithm
-        def kadane(arr):
-            current_max = max_sum = arr[0]
-            for num in arr[1:]:
-                current_max = max(num, current_max + num)
-                max_sum = max(max_sum, current_max)
+        '''
+        Given:
+        circular int arra -> nums of lenght n
+        return max possible sum of non empty sub of nums.
+
+        Match: Greedy -> Kadane's ALgorithm
+        '''
+
+        def kadane(nums):
+
+            max_sum = float('-inf')
+            curr = 0
+            for num in nums:
+                curr = max(num, curr + num)
+                max_sum = max(max_sum, curr)
+            
             return max_sum
         
-        # Case 1: Maximum Sum of Non-Circular Subarray
-        max_kadane = kadane(nums)
-        
-        # Case 2: Maximum Sum of Circular Subarray
+        max_sum = kadane(nums)
+        neg_nums = [-num for num in nums]
+        min_sum = kadane(neg_nums)
+
         total_sum = sum(nums)
-        # Invert array for minimum subarray sum
-        min_sum = kadane([-num for num in nums])
-        
-        # Handle the case where all numbers are negative
+
         if total_sum == -min_sum:
-            return max_kadane
+            return max_sum
         
-        # Result is the maximum of both cases
-        return max(max_kadane, total_sum + min_sum)  # total_sum + min_sum is total_sum - (-min_sum)
+        return max(max_sum, total_sum + min_sum)
+    
