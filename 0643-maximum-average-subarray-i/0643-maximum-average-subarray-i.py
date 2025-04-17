4bfddf483a3925762bf61522ad06c2d045163206
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-04-17 15:09:07 -0400

    Time: 96 ms (32.93%), Space: 27.3 MB (78.44%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
@@ -1,21 +1,44 @@
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
-        
-        left = 0
-        curr_sum = 0
-        max_sum = float('-inf')
+        '''
+        Understand:
 
-        for right in range(k):
-            curr_sum += nums[right]
+        Given -> int array -> nums consist of n elements. and int k. 
+
+        Find -> continguous sub whose len is k. and has max avg val. 
+        Return max avg. 
+
+        Match: Sliding window -> fixed length
+
+        Plan:
+
+        First fill k elements in the windows. 
+        keep curr sum. 
+
+        Now one by one add one element and remove one element. Adjust the sum accordingly. 
+        Update max sum. 
+
+        At end take average. 
+        '''
+
+        # max_sum = float('-inf')
+        curr_sum = 0
+
+        for i in range(k):
+            curr_sum += nums[i]
         
         max_sum = curr_sum
-        # print(curr_sum)
+        left = 0
 
         for right in range(k, len(nums)):
             curr_sum += nums[right] - nums[left]
-            # print(curr_sum)
             max_sum = max(max_sum, curr_sum)
             left += 1
-        
-        return max_sum / k
-        
+
+        # print(max_sum)
+        return max_sum / k 
+
+        '''
+        Time Complexity: O(N)
+        Space Complexity: O(1)
+        '''
