99225e8c73037425d2d96ee6bee2f3a61844a54b
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-04-17 14:30:30 -0400

    Time: 14 ms (62.59%), Space: 20.2 MB (39.01%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,20 +1,56 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        
-        Row = len(grid)
-        Col = len(grid[0]) if Row else 0
+        '''
+        Understand:
 
-        dp = [[0] * Col for _ in range(Row)]
+        Given -> m x n grid -> non negative num 
+        find path from top left to bottom right. 
+        minimizes sum 
+
+        return sum
+
+        can move only down or right. 
+
+        Match: Overlapping subproblems + next decision based on current. Minimizing somthing -> Dynamic Programming. 
+
+        Plan:
+        
+        Function: dp[m][n] -> path with min sum to reach grid[m][n]
+
+        Recurrence Relation -> 
+        since it can only move down or rihgt. 
+
+        that means, if we are a cell, then we either came from UP or left.
+        dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j]
+
+        Base Case: dp[0][0] == grid[i][j]
+        dp[0][j] => dp[i][j-1] + grid[i][j]
+        dp[i][0] => dp[i-1][j] + grid[i][j]
+        '''
+
+        # Implement
+
+        m = len(grid)
+        n = len(grid[0]) if m else 0
+
+        dp = [[0]*n for _ in range(m)]
 
         dp[0][0] = grid[0][0]
 
-        for i in range(Row):
-            for j in range(Col):
-                if i == 0 and j > 0:
+        for i in range(m):
+            for j in range(n):
+                if i == 0 and j != 0:
                     dp[i][j] = dp[i][j-1] + grid[i][j]
-                elif j == 0 and i > 0:
+                
+                elif j == 0 and i != 0:
                     dp[i][j] = dp[i-1][j] + grid[i][j]
-                elif i > 0 and j > 0:
-                    dp[i][j] = min(dp[i][j-1], dp[i-1][j]) + grid[i][j]
-        
-        return dp[Row-1][Col-1]
+
+                elif i != 0 and j != 0:
+                    dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j]
+
+        return dp[m-1][n-1] 
+
+        '''
+        Time Complexity: O(m x n)
+        Space Complexiyy: O(m x n)
+        '''
