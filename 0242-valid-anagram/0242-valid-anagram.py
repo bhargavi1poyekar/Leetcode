37f5e65fff72981f19ff32e3a148b3bd6a51f6ef
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-04-17 15:16:38 -0400

    Time: 7 ms (89.36%), Space: 18 MB (49.87%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,4 +1,21 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        '''
+        Understand:
+
+        Given s and t -> return True if t is anagram of s. 
         
+        Anagram -> both have same characters -> with same count, but different order
+
+        Brute Force -> sort and compare. 
+        Optimized -> counter -> Match the hash of both strings. 
+
+        '''    
+
         return Counter(s) == Counter(t)
+
+        '''
+        Time Complexity: O(N)
+        Space Complexity: O(N)
+        '''
+    
