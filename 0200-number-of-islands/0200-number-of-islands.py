18547bc6aaeaa0c812ae2cd371da280f59ce5677
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-04-17 15:25:03 -0400

    Time: 297 ms (16.52%), Space: 28.4 MB (9.72%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,10 +1,24 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        
+        '''
+        Understand:
+
+        Given  -> m x n 2d binary grid -> map of 1's land and 0's water
+
+        island -> formed by adjacent lands hori or vertically. 
+
+        Match: Graph Traversal DFS. NUm of connected components. 
+
+        Plan: do normal graph traversal dfs, and for each component increment count. 
+        '''
+
         def isValid(row, col):
             return 0 <= row < Row and 0 <= col < Col and grid[row][col] == '1'
-        
+
         directions = [(0, 1), (0, -1), (-1, 0), (1, 0)]
+        Row = len(grid)        
+        Col = len(grid[0])
+
 
         def dfs(row, col):
             for dx, dy in directions:
@@ -13,10 +27,7 @@ class Solution:
                     seen.add((nextr, nextc))
                     dfs(nextr, nextc)
         
-        Row = len(grid)
-        Col = len(grid[0])
         seen = set()
-
         num_islands = 0
 
         for row in range(Row):
@@ -27,6 +38,10 @@ class Solution:
                     dfs(row, col)
         
         return num_islands
+
+        '''
+        Time Complexity: O(m x n)
+        Space COmplexity: O(m*n)
         
-
-
+        '''
+        
