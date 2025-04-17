89a5fbc0a8ac89f2c2d19f5d0303ec85cc36f644
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-04-16 20:12:50 -0400

    Time: 8 ms (58.71%), Space: 18.5 MB (92.26%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,19 +1,52 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
         
-        l_ptr = 0
-        r_ptr = 0
+        '''
+        Understand:
 
-        max_jump = 0 
-        num_jmps = 0
+        Given -> 0 indexed arr of integers. nums of length n. 
+        Initially at nums[0]
 
-        while r_ptr < len(nums) - 1:
-            for i in range(l_ptr, r_ptr + 1):
+        each element -> represents max length of forward jump from i. 
+
+        return min jumps to reach nums[n-1]. 
+
+        Match: Greedy approach. 
+
+        Plan:
+        initially we have left and right limits. -> set to 0. 
+
+        Now, as we go ahead. we iterate from left till right, and get the max jump that is possible. 
+
+        Then left = right. and right = max jump possible. 
+
+        increament the number of jmp by 1. -> since it is considered that we jumped to max jump. 
+
+        then again this is continued till we reach n-1. 
+        '''
+
+        left = 0
+        right = 0
+        n = len(nums)
+
+        min_jumps = 0
+
+        while right < n-1:
+            max_jump = 0
+
+            for i in range(left, right+1):
                 max_jump = max(max_jump, i + nums[i])
             
-            l_ptr = r_ptr + 1
-            r_ptr = max_jump
-
-            num_jmps += 1
+            left = right
+            right = max_jump
+            min_jumps += 1
         
-        return num_jmps
+        return min_jumps
+
+        '''
+        Time Complexity: O(n)
+        Space Complexity: O(1)
+        '''
+            
+
+ 
