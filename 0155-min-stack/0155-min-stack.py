27605e58888b3456a528ffe766ff6dfccef9316c
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-04-18 19:20:15 -0400

    Time: 9 ms (32.12%), Space: 21.3 MB (64.48%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -1,28 +1,39 @@
 class MinStack:
 
+    '''
+    Understand: Designing a stack -> supports push, pop, top and retriev min element. 
+
+    O(1) time complexity for each function
+
+    Match: Stack
+
+    Plan: Keep 2 stacks -> one for normal elements, and other for storing min values at each step. 
+    '''
+
     def __init__(self):
         self.stack = []
         self.min_stack = []
 
     def push(self, val: int) -> None:
         self.stack.append(val)
-
-        if self.min_stack:        
-            current_min = self.min_stack[-1]
-            self.min_stack.append(min(current_min, val))
+        if self.min_stack:
+            min_val = self.min_stack[-1]
+            if min_val > val:
+                self.min_stack.append(val)
+            else:
+                self.min_stack.append(min_val)
         else:
             self.min_stack.append(val)
 
     def pop(self) -> None:
-        self.stack.pop()
         self.min_stack.pop()
+        self.stack.pop()
 
     def top(self) -> int:
         return self.stack[-1]
 
     def getMin(self) -> int:
         return self.min_stack[-1]
-        
 
 
 # Your MinStack object will be instantiated and called as such:
