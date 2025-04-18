ea744b4f767285ce4951e083b55b95af77c620c9
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-04-18 19:27:14 -0400

    Time: 3 ms (35.6%), Space: 20 MB (8.74%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -7,14 +7,41 @@
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
         
-        def dfs(root, minval, maxval):
+        '''
+        Understand: 
+
+        Given a root -> check if valid binary search tree. 
+
+        Left subtree nodes _> less than nodes key, 
+        right subtree -> greater than. 
+
+        Match: DFS
+
+        Plan:
+
+        while traversing, we also set max val and min val. 
+
+        So like, starting from root -> max and min would be infinte ends. 
+
+        But while going to left of root -> max would be root -> because the left nodes should be less than root. 
+
+        similarly while going right -> change min to root. 
+
+        Continue this and return if any of the node invalidates the constraint.
+        '''
+
+        def dfs(root, max, min):
             if not root:
                 return True
             
-            if root.val >= maxval or root.val <= minval:
+            if root.val >= max or root.val <= min:
                 return False
-            
-            return dfs(root.left, minval, root.val) and dfs(root.right, root.val, maxval)
-        
-        return dfs(root, float('-inf'), float('inf'))
-            
+
+            return dfs(root.left, root.val, min) and dfs(root.right, max, root.val)
+
+        return dfs(root, float('inf'), float('-inf'))
+
+        '''
+        Time Complexity: O(N)
+        Space COmplexity: O(1) => O(N) -> for stack overhead. 
+        '''
