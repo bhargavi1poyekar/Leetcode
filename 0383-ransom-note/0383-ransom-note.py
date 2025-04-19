2b2f223857f7036617f82699a35b5dc1ed3b5946
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-04-19 14:50:23 -0400

    Time: 15 ms (76.67%), Space: 18.2 MB (13.86%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,11 +1,32 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         
-        ransom_count = Counter(ransomNote)
-        magazine_count = Counter(magazine)
+        '''
+        Understand:
+        Given 2 strings -> ransomNote and magazine
+        return True if ransomNote can be made using letters from magazine. 
+        else False
 
-        for letter in ransom_count:
-            if ransom_count[letter] > magazine_count[letter]:
+        Each latter can be used once.
+
+        Match: Hash -> to check count of letters. 
+
+        Plan:
+        for each letter in ransomNote the count should be <= magazine count of that letter. 
+
+        if not, return False
+        '''
+
+        note_count = Counter(ransomNote)
+        mag_count = Counter(magazine)
+
+        for letter in note_count:
+            if note_count[letter] > mag_count[letter]:
                 return False
 
-        return True 
+        return True        
+
+        '''
+        Time Complexity: O(N)
+        Space Complexity: O(N)
+        '''
