d1bb40d605b10c4b51d7a9e3bfb95cf131e6f82c
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-04-24 19:52:39 -0400

    Time: 53 ms (64.76%), Space: 48.5 MB (93.94%) - LeetHub

--- 2216-delete-the-middle-node-of-a-linked-list/2216-delete-the-middle-node-of-a-linked-list.py
+++ 2216-delete-the-middle-node-of-a-linked-list/2216-delete-the-middle-node-of-a-linked-list.py
@@ -5,13 +5,29 @@
 #         self.next = next
 class Solution:
     def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        
-        dummy = ListNode()
-        dummy.next = head
-        prev = dummy
+        '''
+        Understand:
 
-        slow = head
-        fast = head
+        Given head of LL. 
+        Delete the middle node and return head of modified list. 
+
+        Match: Slow and fast. 
+        Plan:
+
+        We need slow, fast and prev pointer. 
+        while fast reaches end, slow reached middle. 
+        Now prev should be one node before slow. 
+
+        So that we can skip slow.
+        '''
+
+        if not head or not head.next:
+            return None
+
+        slow = fast = head
+
+        prev = dummy = ListNode()
+        dummy.next = head
 
         while fast and fast.next:
             prev = slow
@@ -20,4 +36,8 @@ class Solution:
         
         prev.next = slow.next
         return dummy.next
-      
+
+        '''
+        Time Complexity: O(N)
+        Space Complexity: O(N)
+        '''
