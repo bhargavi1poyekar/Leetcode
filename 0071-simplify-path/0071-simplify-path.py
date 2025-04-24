65a0025dc7a899e7edb627b3a1bf8eae20eb8b47
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-04-24 18:25:48 -0400

    Time: 0 ms (100%), Space: 18 MB (7.92%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.py
+++ 0071-simplify-path/0071-simplify-path.py
@@ -1,10 +1,37 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
-        
+        '''
+        Understand:
+
+        Given -> abs path -> begins with slash. 
+        Transform abs path to simplified canonical path. 
+
+        single period -> curr directory. 
+
+        double period -> previos dir
+
+        multiple slash -> single slash
+
+        anything that doesn't match -> are dir/ file names. 
+
+        path must start with slash, and dir separated by slash. 
+
+        Match: Stack -> to go back to prev directory. 
+
+        Plan:
+
+        Since all dir/file are separated by '/' -> split string on '/'. 
+
+        Now if the char -> is . or empty -> it means we shouldn't do anything. Because it means current directory. 
+
+        if .. -> then go to previous directory -> pop from stack. 
+
+        else -> anything -> then we just add to stack -> as it is valid directory name
+        '''
+
         stack = []
 
         for ch in path.split('/'):
-            # print(ch)
             if ch == '.' or ch == '':
                 continue
             elif ch == '..':
@@ -13,5 +40,11 @@ class Solution:
             else:
                 stack.append(ch)
         
-        # print(stack)
         return '/' + '/'.join(stack)
+        
+        '''
+        Time Complexity: O(N)
+        Space COmplexity: O(N)
+        '''
+
+
