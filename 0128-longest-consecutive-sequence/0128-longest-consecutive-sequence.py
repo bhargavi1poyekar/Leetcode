841cc70b125fa15c24b1a088adeae2695eb173da
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-04-24 19:03:52 -0400

    Time: 43 ms (89.51%), Space: 34.3 MB (32.21%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,30 +1,39 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        
+        '''
+        Understand:
+
+        Given -> unsorted array of int -> nums. 
+        return length of longest consecutive elements seq. 
+
+        Solution should -> O(N)
+        Match : convert list to sets -> to check existence of element in O(1) complexity. 
+
+        Plan:
+
+        while we traverse nums:
+            we check if nums-1 also in set -> if yes, then we might have already onsidered nums in that sequence. 
+
+            if nums-1 not in set, then seq starts from nums, 
+            then while num + 1 in set, we keep on increasing the length of sequence. 
+
+            and once the sequence end, we update max seq length. 
+        '''
+
         num_set = set(nums)
-        longest_seq = 0
+        max_length = 0
 
         for num in num_set:
-            if (num - 1) not in num_set:
-                curr_len = 1
+            if num - 1 not in num_set:
+                seq = 1 
                 while num + 1 in num_set:
-                    curr_len += 1
-                    num = num + 1
-                longest_seq = max(longest_seq, curr_len)
+                    seq += 1
+                    num += 1
+                max_length = max(max_length, seq)
         
-        return longest_seq
+        return max_length
 
-        # num_set = set(nums)
-        # longest = 0
-
-        # for n in num_set:
-        #     if n - 1 not in num_set:
-        #         length = 1
-
-        #         while n + length in num_set:
-        #             length += 1
-                
-        #         longest = max(longest, length)
-        
-        # return longest
-
+        '''
+        Time Complexity: O(N)
+        Space COmplexity: O(N)
+        '''
