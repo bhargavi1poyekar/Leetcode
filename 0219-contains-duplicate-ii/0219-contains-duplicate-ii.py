55fcc5fb55a586b8083a8c7771b1593eca256c5d
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-04-24 18:55:41 -0400

    Time: 27 ms (53.92%), Space: 36.7 MB (12.51%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -1,12 +1,32 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        
-        hash_index = {}
+        ''' 
+        Understand: Given an int arr nums and k. 
+        return True if two indices i, j have same val and abs(i - j) <= k
 
-        for i in range(len(nums)):
-            if nums[i] in hash_index:
-                if abs(hash_index[nums[i]] - i) <= k:
+        Match: Hash dict -> keep track of elements encountered. 
+
+        Plan:
+
+        While traversing the arra -> we check if element is already seen, if yes, then we check the abs condition is true or not. if not, then we replace the index of the element in dict with curr elements index -> because the abs diff should be <= k, so anyway, if we are going to find, next j, then i should be the max(i, j)
+        '''
+
+        hash_seen = {}
+
+        for j, num in enumerate(nums):
+            if num in hash_seen:
+                i = hash_seen[num]
+                if abs(i-j) <= k:
                     return True
-            hash_index[nums[i]] = i
+                else:
+                    hash_seen[num] = j
+            else:
+                hash_seen[num] = j
         
         return False
+
+        '''
+        Time Complexity: O(N)
+        Space Complexity: O(N)
+        '''
+
