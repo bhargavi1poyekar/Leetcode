0d01863ae464f9d3b6b0a6cf8fe340795146a5e2
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-04-24 18:44:20 -0400

    Time: 11 ms (17.63%), Space: 18.1 MB (18.74%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,9 +1,29 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
+        '''
+        Understand:
+
+        given 2 strings s and t 
+        isomorphic -> if chars in s -> replaced to get t. 
+
+        mapping should be one to one. 
+        char may map to itself. 
+
+        Match: -> we match the mappings in hash. 
+
+        Plan:
+        if length of both strings not equal -> return False. 
+
+        Now, while traversing both the strings at same time, we check if any of the curr char of both string is already mapped. if not mapped, then we map s[i] => t[i] and vice versa. 
+
+        if mapped -> then check if mapped to same character, else, return false. 
+
+        at end -> return true. 
+        '''
 
         if len(s) != len(t):
             return False
-
+        
         s_map = defaultdict()
         t_map = defaultdict()
 
@@ -11,10 +31,13 @@ class Solution:
             if s[i] not in s_map and t[i] not in t_map:
                 s_map[s[i]] = t[i]
                 t_map[t[i]] = s[i]
-
-            elif (s[i] in s_map and s_map[s[i]] != t[i]) or (t[i] in t_map and t_map[t[i]] != s[i]):
+            elif s[i] in s_map and s_map[s[i]] != t[i] or t[i] in t_map and t_map[t[i]] != s[i]:
                 return False
-
-        return True 
-
     
+
+        return True
+
+        '''
+        Time complexity: O(N)
+        Space Complexity: O(N)
+        '''
