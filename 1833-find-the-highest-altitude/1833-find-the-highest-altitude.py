0851c0cac8ba8b43b48aa23f33f386a63d5e9328
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-04-24 19:18:27 -0400

    Time: 3 ms (7.48%), Space: 17.7 MB (51.45%) - LeetHub

--- 1833-find-the-highest-altitude/1833-find-the-highest-altitude.py
+++ 1833-find-the-highest-altitude/1833-find-the-highest-altitude.py
@@ -1,11 +1,34 @@
 class Solution:
     def largestAltitude(self, gain: List[int]) -> int:
-        
-        curr_alt = 0
-        max_alt = 0
+        '''
+        Understand:
 
-        for inc in gain:
-            curr_alt += inc
-            max_alt = max(max_alt, curr_alt)
-        
+        road trip -> n + 1 points. at diff altitudes. 
+        starts at point 0 -> altitude equal = 0
+
+        given gain -> length n. net gain in altitude between i and i + 1
+
+        Return highest altitude of point.  
+
+        Match: Prefix Sum 
+
+        Plan:
+        Since gain can be negative -> 
+        we keep on adding gain, and update max altitude. 
+        '''
+
+        max_alt = 0
+        curr_alt = 0
+
+        for net in gain:
+            curr_alt += net
+            max_alt = max(curr_alt, max_alt)
+
         return max_alt
+
+        '''
+        Time Complexity: O(N)
+        Space COmplexitY: O(N)
+        '''
+
+        
