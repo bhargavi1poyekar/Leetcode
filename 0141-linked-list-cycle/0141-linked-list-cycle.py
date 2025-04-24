32ca2a9298eb23e7fda5f02bf4c94c2a32c8196c
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-04-24 19:27:46 -0400

    Time: 38 ms (96.49%), Space: 19.9 MB (29.66%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -6,13 +6,37 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        
+        '''
+        Understand:
+
+        Given -> head of LL. 
+        return if ll has cycle. 
+
+        Match: slow and fast pointer. 
+
+        Plan:
+        we start slow and fast from head, but fast at twice speed of slow. 
+        slow moves by 1, and fast moves by 2. 
+
+        if both meet -> then there is a cycle. -> return True
+        else if exists loop, because, fast becomes None -> then no Cycle. 
+        '''
+
+        if not head:
+            return False
+
         slow = fast = head
 
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
+
             if slow == fast:
                 return True
         
         return False
+        
+        '''
+        Time Complexity: O(N)
+        Space COmplexity: O(N)
+        '''
