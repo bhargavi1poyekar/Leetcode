a70141a2ef29624a9b35de03594b98fc215f9ee7
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-04-24 18:49:45 -0400

    Time: 0 ms (100%), Space: 19.2 MB (11.35%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,13 +1,32 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        
-        index_map = {}
+        '''
+        Understand: 
 
-        for i in range(len(nums)):
-            diff = target - nums[i]
-            if diff in index_map:
-                return [i, index_map[diff]]
+        Given int arr nums -> and target. 
+        return indices of 2 nums -> add up to target. 
+
+        exactly 1 solution. 
+        not use same element twice. 
+
+        Match: hash -> to keep track of prev elements 
+
+        Plan:
+        we add target-element in hash and its index. 
+
+        everytime we check if curr element in target. if yes -> return indexes.
+        '''
+
+        hash_sum = {}
+
+        for i, num in enumerate(nums):
+            if num in hash_sum:
+                return [hash_sum[num], i]
             else:
-                index_map[nums[i]] = i
+                hash_sum[target-num] = i
+
+        '''
+        Time Complexity: O(N)
+        Space Complexity: O(N)
+        '''
         
-         
