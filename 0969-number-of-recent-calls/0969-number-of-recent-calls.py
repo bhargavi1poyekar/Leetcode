f3154339a5f625e45e8d2cf18df35e90c657ff9a
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-04-24 20:07:04 -0400

    Time: 28 ms (87.48%), Space: 23.1 MB (76.59%) - LeetHub

--- 0969-number-of-recent-calls/0969-number-of-recent-calls.py
+++ 0969-number-of-recent-calls/0969-number-of-recent-calls.py
@@ -1,14 +1,35 @@
 class RecentCounter:
 
+    '''
+    Understand: Given recent Counter. 
+
+    initializes -> counter with 0 requests
+    ping -> adds a new req at time t (ms).
+
+    and returns the num requests happened in past 3000 ms.
+
+    Match: Queue. t is strictly larger, so we don't need request with time < t-3000
+
+    We can remove the oldest requests. 
+
+    Plan:
+    in ping, remove all the requests with time < t - 3000. 
+    and then add t, and return length of queue. 
+    '''
+
     def __init__(self):
-        self.queue = deque() # Queue with no requests
-        
-    def ping(self, t: int) -> int:
-        while self.queue and self.queue[0] < t-3000:
+        self.queue = deque()
+
+    def ping(self, t: int) -> int: 
+        while self.queue and self.queue[0] < t - 3000:
             self.queue.popleft()
         self.queue.append(t)
         return len(self.queue)
-
+    
+    '''
+    Time Complexity: O(N)
+    Space COmplexity: O(N)
+    '''
 
 # Your RecentCounter object will be instantiated and called as such:
 # obj = RecentCounter()
