797b1b719142ea4811d42224b6a89bcf78d27bb4
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-04-26 19:30:18 -0400

    Time: 0 ms (100%), Space: 19.1 MB (74.58%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.py
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.py
@@ -5,7 +5,27 @@
 #         self.next = next
 class Solution:
     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        
+        '''
+        Understand
+
+        Given head of ll, grp nodes with odd indices together. 
+        follwed by even indices. 
+
+        Return reordered. First is odd, second even. 
+
+        Match: two pointers. 
+
+        Plan:
+
+        o - e - o - e - o
+        We put oddhead, and evenhead and first and second position.
+
+        Then make odd.next = even.next -> so it is connected to next odd. 
+        and move odd pointer to its next. 
+
+        do the same for even. till the end of list.  
+        '''
+
         if not head or not head.next:
             return head
 
@@ -20,4 +40,9 @@ class Solution:
         
         odd.next = evenHead
         # even.next = None
-        return head
+        return oddHead
+        
+        '''
+        Time Complexity: O(N)
+        Space Complexity: O(1)
+        '''
