16601d8372fb0a57d8295d47e5460214a6c67836
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-04-26 18:34:05 -0400

    Time: 0 ms (100%), Space: 18.5 MB (60.34%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
@@ -6,35 +6,61 @@
 class Solution:
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
         
-        if not head and not head.next:
-            return head
+        '''
+        Understand:
+
+        Given head of LL. 
+        Revere nodes of list k at a time. 
+
+        return modified list. 
+
+        k is pos integer <= length of ll. 
+        if num nodes -> not multiple of k -> let them remain as it is. 
+
+        Nodes need to be changed. 
+
+        Match: Reverse ll, count nodes, with con, tail. 
+
+        Plan:
+
+        Since we have to reverse every k -> only if there are k nodes. 
+        So first we have to count k nodes. once we know, there are k nodes.
+
+        Then we reverse from curr to k. 
+
+        Then, again we start from k+1th node, to next k nodes. and so on. 
+
+        We will use reverse Linked list 2 approach, with con tail, for reversing. 
+        '''
 
         count = 0
-        curr = count_ptr = head
+        count_ptr = head
+        curr = head
         prev = None
 
         while count_ptr and count < k:
-            count_ptr = count_ptr.next
+            # count = 1
+            # :
             count += 1
+            count_ptr = count_ptr.next
 
             if count == k:
-                con = prev
-                tail = curr
-
+                con, tail = prev, curr
                 while count:
                     nn = curr.next
                     curr.next = prev
                     prev = curr
                     curr = nn
                     count -= 1
-
-                if not con:
-                    head = prev
-                else:
+                if con:
                     con.next = prev
+                else:
+                    head = prev
+                
                 tail.next = curr
                 prev = tail
-            
+        
         return head
+            
 
-                
+
