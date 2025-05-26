00442fd2ae4915b301f1d4a4515594c8d372a02f
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-05-26 19:58:36 -0400

    Time: 3 ms (24.82%), Space: 17.6 MB (98.93%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -1,28 +1,6 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
         
-        '''
-        Understand:
-
-        Given -> 2 strings. s and t
-        return if s subseq of t
-
-        Match: 2 pointers. 1 for each string. 
-        Plan:
-
-        first of all, for s to be subseq, len(s) <= len(t)
-
-        then, we have pointers at start of both strings. 
-        if s[i] == t[i] :
-            then move s ptr forward. 
-        otherwise move t_ptr anyway. 
-
-        continue this till we reach end of s or t. 
-        if sptr reached end of s -> then s is sub seq of t. 
-        '''
-
-        # Implementation:
-
         if len(s) > len(t):
             return False
         
@@ -34,9 +12,7 @@ class Solution:
                 s_ptr += 1
             t_ptr += 1
         
-        return s_ptr == len(s)
-
-        '''
-        Time Complexity: O(n)
-        Space Complexity: O(1)
-        '''
+        if s_ptr == len(s):
+            return True
+        
+        return False
