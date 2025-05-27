677f1f03b5788f843e874051c2af372c18de05cf
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-05-27 19:58:52 -0400

    Time: 87 ms (50.12%), Space: 18.3 MB (54.92%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,70 +1,37 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         
-        '''
-        Understand: 
-
-        Given : 2 strings s -> len m and t -> len n
-        return -> min win substring -> every char in t (including duplicates) -> is present in the window. 
-
-        if not string -> return empty.
-
-        Answer is unique. 
-
-        Questions:
-        can len t > len s -> is possible -> return empty 
-        is it case_specific -> ? No 
-
-        Match:
-        Sliding window.  
-
-        Plan:
-
-        freq_counter(t) == freq_counter(Window)
-
-        match_count -> should be equal to distinct values of t. -> if yes, then we try to minimize the window. 
-
-        need to return entire substring -> we need to store. min left, min right, and min_length. 
-        
-        left = 0
-        for right in s:
-            add s to freq_s of windows. 
-            if freq_s[right] == freq_win[right]:
-                increment match count
-
-            while match_count ==  distinct t vals:
-                update min_length
-                decr window. before dec- . check if left is included in match count. if yes, dec match count. 
-        
-        return the string. 
-        '''
-
-        left = 0
         min_length = float('inf')
-        min_left = float('inf')
-        min_right = float('inf')
-        win_freq = Counter()
-        t_freq = Counter(t)
+        min_left = 0
+        min_right = 0
 
-        distinct = len(t_freq)
-        match = 0
+        t_freq = Counter(t)
+        win_freq = Counter()
+        unique_t = len(t_freq)
+        match_count = 0
+        left = 0
 
         for right in range(len(s)):
             win_freq[s[right]] += 1
-            if win_freq[s[right]] == t_freq[s[right]]:
-                match += 1
             
-            while match == distinct:
+            if win_freq[s[right]] == t_freq[s[right]]:
+                match_count += 1
+            
+            while match_count == unique_t:
                 curr_length = right - left + 1
                 if curr_length < min_length:
-                    min_length = curr_length
                     min_left = left
                     min_right = right
+                    min_length = curr_length
                 
                 if win_freq[s[left]] == t_freq[s[left]]:
-                    match -= 1
+                    match_count -= 1
                 
                 win_freq[s[left]] -= 1
                 left += 1
-        
-        return s[min_left: min_right+1] if min_length != float('inf') else ""
+            
+        if min_length != float('inf'):
+            return s[min_left: min_right+1]
+        else:
+            return ""
+
