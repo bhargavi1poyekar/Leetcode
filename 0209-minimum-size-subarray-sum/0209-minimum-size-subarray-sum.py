3fe39e5b25702b49fc1f2afc79c6761b88ae45d2
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-05-27 19:23:58 -0400

    Time: 7 ms (92.11%), Space: 28.4 MB (10.9%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -1,26 +1,10 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
         
-        '''
-        Understand:
-
-        Given -> arr of pos int nums. and pos int target. 
-        return min length of subarray whose sum >= target. 
-
-        no subarray -> return 0
-
-        Match:
-        Sliding window -> variable length
-
-        Plan:
-        we keep adding new elements to window and sum. 
-        once sum becomes >= target, we try to remove elements from left. And till the sum >= target, we update min length of window. 
-        '''
-
         left = 0
-        min_length = len(nums) + 1
+        min_length = len(nums)+1
         curr_sum = 0
-
+        
         for right in range(len(nums)):
             curr_sum += nums[right]
 
@@ -28,10 +12,5 @@ class Solution:
                 min_length = min(min_length, right - left + 1)
                 curr_sum -= nums[left]
                 left += 1
-
-        # If no subarray sum greater than target, then min_length will be what we initialized. 
-        return min_length if min_length != len(nums) + 1 else 0
-
-        
-
-        
+            
+        return min_length if min_length != (len(nums)+1) else 0 
