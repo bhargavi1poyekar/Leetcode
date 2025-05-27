a8fda05de7d3ee70121cdf1e8b45b4723452f09e
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-05-27 19:38:48 -0400

    Time: 71 ms (37.15%), Space: 18.4 MB (41.08%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.py
@@ -1,40 +1,19 @@
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
-        '''
-        Understand:
-
-        Given -> binary arr -> elements 0 or 1 and int k
-        return max num of consecutive 1's in arr -> if you can flip at most k 0's
-
-        Match: Variable Size sliding window. 
-
-        Plan: 
-        We keep count of how many 0 we encountered, and we can flip at most k 0's. 
-
-        So if the count > k:
-        then we remove element from left, till count > k. 
-
-        And keep on updating max length of sliding window. 
-        '''
-
+        
         left = 0
+        count_zero = 0
         max_ones = 0
-        count_0 = 0 
 
         for right in range(len(nums)):
             if nums[right] == 0:
-                count_0 += 1
+                count_zero += 1 
             
-            while count_0 > k:
+            while count_zero > k:
                 if nums[left] == 0:
-                    count_0 -= 1
+                    count_zero -= 1
                 left += 1
             
             max_ones = max(max_ones, right - left + 1)
         
         return max_ones
-
-        '''
-        Time Complexity: O(N)
-        Space Complexity: O(N)
-        '''
