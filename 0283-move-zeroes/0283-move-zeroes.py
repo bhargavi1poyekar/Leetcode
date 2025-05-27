89413a5941ab2503add3d233ea0a95319c419700
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-05-26 20:05:27 -0400

    Time: 3 ms (81.41%), Space: 18.8 MB (49.31%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,26 +3,8 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        '''
-        Understand:
-
-        Given -> arr nums -> move all 0's to end. 
-        Inplace. without making a copy of array. 
-
-        Match: slow, fast pointer. 
-
-        Plan:
-        slow pointer at start.
-
-        Inc fast pointer ahead -> till you find a non zero element. Assign it to slow pointer, and inc slow pointer. 
-
-        Do this till fast reaches end of nums. 
-        then for remaining 0's, just replace the remaing element after slow to 0.  
-
-        '''
-
-        slow = 0
-        fast = 0
+        
+        slow = fast = 0
 
         while fast < len(nums):
             if nums[fast] != 0:
@@ -35,9 +17,3 @@ class Solution:
             slow += 1
         
         return nums
-
-        '''
-        Time Complexity: O(N)
-        Space Complexity: O(1)
-        '''
-
