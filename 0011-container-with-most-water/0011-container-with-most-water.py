6b063a5a7ba517413b6b5575413f18445d83936f
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-05-26 20:08:27 -0400

    Time: 115 ms (11.85%), Space: 28.6 MB (24.01%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,38 +1,19 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        '''
-        Understand:
-
-        height -> len n -> n lines
-
-        return: max amt of water. 
-
-        what if height is empty -> no lines -> max amt -> 0?
-
-        Match: 
-        2 pointer -> start from both ends, and then start decreasing the width as we move ahead. 
-
-        Plan:
-
-        amt of water = > area of the container
-        area = width * height
-        width - > right - left + 1 -> left is ptr from left, and right starts from end. 
-
-        height => min(height[left], height[right])
-        ''' 
-
-        left = 0 
-        right = len(height)-1
-        max_area = 0
-        while left < right:
-            width = right - left
-            hgt = min(height[left], height[right])
-            area = width * hgt
-            max_area = max(max_area, area)
-
-            if height[left] >= height[right]:
-                right -= 1
-            else:
-                left += 1
         
+        start = 0
+        end = len(height) - 1
+        max_area = 0
+
+        while start <= end:
+            hght = min(height[start], height[end])
+            width = end - start
+            area = hght*width
+            max_area = max(area, max_area)
+
+            if height[start] < height[end]:
+                start += 1
+            else:
+                end -= 1
+
         return max_area
