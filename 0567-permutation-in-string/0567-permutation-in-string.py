1c192ca4f2d8a98071b4c9025e01476175059853
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-05-27 20:11:46 -0400

    Time: 15 ms (86.1%), Space: 17.8 MB (62.45%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,48 +1,51 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
+        
         if len(s2) < len(s1):
             return False
-        
-        match_count = 0
-        left = 0
 
-        s1_hash = [0 for i in range(26)]
-        s2_hash = [0 for i in range(26)]
+        s1_count = [0] * 26
+        s2_count = [0] * 26
 
         for i in range(len(s1)):
-            s1_hash[ord(s1[i]) - ord('a')] += 1
-            s2_hash[ord(s2[i]) - ord('a')] += 1
+            s1_count[ord(s1[i]) - ord('a')] += 1
+            s2_count[ord(s2[i]) - ord('a')] += 1
         
         match_count = 0
-
         for i in range(26):
-            if s1_hash[i] == s2_hash[i]:
+            if s1_count[i] == s2_count[i]:
                 match_count += 1
-            
+        
+        left = 0
         for right in range(len(s1), len(s2)):
             if match_count == 26:
                 return True
 
-            right_idx = ord(s2[right]) - ord('a')
-            s2_hash[right_idx] += 1
-            
-            if s2_hash[right_idx] == s1_hash[right_idx]:
+            right_index = ord(s2[right]) - ord('a')
+            s2_count[right_index] += 1
+
+            if s1_count[right_index] == s2_count[right_index]:
                 match_count += 1
-            elif s2_hash[right_idx] - 1 == s1_hash[right_idx]:
+            elif s1_count[right_index] == s2_count[right_index] - 1:
                 match_count -= 1
             
-            left_idx = ord(s2[left]) - ord('a')
-            s2_hash[left_idx] -= 1
+            left_index = ord(s2[left]) - ord('a')
+            s2_count[left_index] -= 1
 
-            if s2_hash[left_idx] == s1_hash[left_idx]:
+            if s1_count[left_index] == s2_count[left_index]:
                 match_count += 1
-            elif s2_hash[left_idx] + 1 == s1_hash[left_idx]:
+            elif s1_count[left_index] == s2_count[left_index] + 1:
                 match_count -= 1
             
             left += 1
         
         return match_count == 26
+
             
 
             
+
             
+        
+
+
