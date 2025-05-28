ab275a92c370575bc42ac4eea78fcf560c236f02
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-05-28 12:56:52 -0400

    Time: 0 ms (100%), Space: 17.8 MB (80.74%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,43 +1,23 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        '''
-        Understand:
-
-        Given string -> s with chars -> ()[]{}
-
-        return if string is valid. 
-
-        Match: Stack -> so that we can check what we encountered before. 
-        And we need to close the latest encounterd open bracket first. LIFO. 
-
-        Plan: 
-
-        we maintain hash -> matching brackets. 
-        if we encounter open bracket -> we add its matching close bracket to the stack. 
-        Now, when we encounter a close bracket, we check if we have same at the stack top. if no, then not valid. 
-
-        if yes, then we remove the stack top. 
-        '''
-
-        match_bracket = {
-                            '(':')',
-                            '{':'}',
-                            '[':']'
-                        }
+        
         stack = []
+
+        bracket_match = {
+            '{':'}',
+            '[':']',
+            '(':')'
+            }
+        
         for ch in s:
-            if ch in match_bracket:
-                stack.append(match_bracket[ch])
+            if ch in bracket_match:
+                stack.append(bracket_match[ch])
             else:
                 if stack and stack[-1] == ch:
                     stack.pop()
                 else:
                     return False
         
-        return True if not len(stack) else False
-
-        '''
-        Time Complexity: O(N)
-        Space Complexity: O(N)
-        '''
-
+        if not stack: 
+            return True
+        return False
