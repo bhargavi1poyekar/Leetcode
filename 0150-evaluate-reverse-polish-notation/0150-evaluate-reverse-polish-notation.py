2ca8905404ef8785bf35618206b06fc2a80e8d70
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-05-28 18:14:54 -0400

    Time: 4 ms (30.27%), Space: 19.2 MB (52.02%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -1,55 +1,29 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        '''
-        Understand:
-
-        Given -> arr of tokens. -> represent arithmetic expression.   
-
-        Evaluate and return value of expression. 
-
-        valid ops -> -, +, *, / 
-
-        operand -> int or another expression. 
-
-        division -> truncates toward zero. 
-        no division by 0
-
-        valid arithmetic expression. 
-
-        Match: Stack -> to get the previous executed expression. 
-
-        Plan:
-
-        if we get any operand, we put it in the stack. 
-        if not, then we evaluate by popping last 2 elements of the stack. and performing that operation, and then adding the value back to stack. 
-        '''
+        
+        operators = ['+', '-', '*', '/']
 
         stack = []
-        operators = ('*', '+', '/', '-')
 
-        for ch in tokens:
-            if ch not in operators:
-                stack.append(int(ch))
-            elif ch == '+':
-                num1 = stack.pop()
-                num2 = stack.pop()
-                stack.append(int(num1 + num2))
-            elif ch == '-':
-                num1 = stack.pop()
-                num2 = stack.pop()
-                stack.append(int(num2 - num1))
-            elif ch == '*':
-                num1 = stack.pop()
-                num2 = stack.pop()
-                stack.append(int(num1 * num2))
-            elif ch == '/':
-                num1 = stack.pop()
-                num2 = stack.pop()
-                stack.append(int(num2 / num1))
-
+        for tok in tokens:
+            if tok not in operators:
+                stack.append(int(tok))
+            else:
+                if tok == '+':
+                    num1 = stack.pop()
+                    num2 = stack.pop()
+                    stack.append(int(num1 + num2))
+                elif tok == '*':
+                    num1 = stack.pop()
+                    num2 = stack.pop()
+                    stack.append(int(num1 * num2))
+                elif tok == '-':
+                    num1 = stack.pop()
+                    num2 = stack.pop()
+                    stack.append(int(num2 - num1))
+                else:
+                    num1 = stack.pop()
+                    num2 = stack.pop()
+                    stack.append(int(num2 / num1))
+        
         return stack[-1]
-
-        '''
-        Time Complexity: O(N)
-        Space Complexity: O(N)
-        '''
