9f91ba7bac92cd9d37775004cfde0fd4e1929e5e
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-06-04 13:19:24 -0400

    Time: 4 ms (15.88%), Space: 17.8 MB (60.46%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,70 +1,25 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-
-        '''
-        Understand:
-
-        Given: n
-
-        Todo:
-
-        check if n is happy.
-        conditions to be happy:
-        any pos int n -> replace by sum of squares of its digit. 
-        repeat this until 
-        either n == 1 or loop in cycle -> which doesnt have 1. 
-
-        if ends in n == 1 -> happy
-
-        Return : true if happy, false if not.  
-
-        cases: 
-            if n == 0: return False. 
-            if n -> single digit number -> return false. 
-
-        Match:
-
-        to check the cycle -> if the current number -> has been seen before -> we can use hash data structure. 
-
-        if not, it will end in n==1. 
-
-        Plan:
-
-        fun sum_of_square_of_digits():
-            first it will find digit by digit, and add its square to sum. 
-            return the sum. 
         
-        seen = set  -> any seen number is added to it. 
-        while n != 1:
-            if n in seen:
-                return False -> cycle detected. 
-            seen.add(n)
-            n = sum_of_square_of_digits(n)
-        
-        return True
-        '''
-
-        def sum_of_square_of_digits(n):
-            sum_square = 0
-
-            while n:
-                digit = n % 10
-                n = n // 10
-                sum_square += digit ** 2
+        def sum_square(num):
+            square = 0
+            while num:
+                digit = num % 10
+                num = num // 10
+                square += digit ** 2
             
-            return sum_square
+            return square
         
         seen = set()
+        num = n
 
-        while n != 1:
-            if n in seen:
-                return False
-            seen.add(n)
-            n = sum_of_square_of_digits(n)
-
-        return True        
-
-
-
-
+        while num != 1:
+            if num in seen:
+                break
+            else:
+                seen.add(num)
+                num = sum_square(num)
         
+        if num == 1:
+            return True
+        else: return False
