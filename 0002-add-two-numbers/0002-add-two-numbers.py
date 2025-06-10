b2e5c06eb89520d18ac7a531233e3b6d5703c6d0
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-06-10 18:56:06 -0400

    Time: 4 ms (55.4%), Space: 18 MB (35.87%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -6,55 +6,22 @@
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         
-        '''
-        Understand:
-
-        Given -> 2 non empty ll. -> non negative integers. 
-        digits stored in reverse order. each node -> single digit. 
-
-        Return -> add 2 numbers -> return as linked list -> reverse order. 
-
-        no leading zeros in list. and only single digits. 
-
-        Match: -> we need 2 pointer -> one for each linked list. 
-        And simultaneuosly one for the new Linked list -> that will store the sum.
-
-        Plan:
-
-        we iterate over both lists, till everyone reaches end. 
-        and we also need to store any carry during the sum. And check that as well while iterating. 
-
-        Then get the digits from both the linked list and do sum. 
-        Now carry would be sum // 10 and the remainder will be storeed in the new node. 
-
-        We create new node -> store the remainder in it. and then move all the pointers forward. We do this till both lists are covered, and there is no carry. 
-        '''
-
-        h = newhead = ListNode()
+        head = h = ListNode()
         carry = 0
 
         while l1 or l2 or carry:
             val1 = l1.val if l1 else 0
             val2 = l2.val if l2 else 0
 
-            add = val1 + val2 + carry
-            carry = add // 10
-            rem = add % 10
+            sum = val1 + val2 + carry
+            carry = sum // 10
+            nodeval = sum % 10
 
-            # print(rem)
-
-            newnode = ListNode(rem)
+            newnode = ListNode(nodeval)
             h.next = newnode
 
             h = h.next
-
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
         
-        return newhead.next
-
-        '''
-        Time Complexity: O(N) -> N being max of length of l1 or l2.
-        Space Complexity: O(N)
-        '''
-
+        return head.next 
