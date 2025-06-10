89c32c21662b4e0e2795e2d1e132624b259c1a4a
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-06-10 19:04:04 -0400

    Time: 0 ms (100%), Space: 18 MB (17.86%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -5,53 +5,22 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        '''
-        Understand:
 
-        Given: 2 sorted lists -> list1 and list2. 
-        Merge them into 1 sorted list. 
+        head = h1 = ListNode()
 
-        Can there be duplicates in both lists? -> Yes
-        Numbers are > 0 ?
-        can the lists be empty -> yes
-
-        Match:
-        two pointers -> one for each list
-
-        Plan:
-        two pointers. 
-        new head.
-        till you reach at end of either of list:
-            compare the values for both pointers. 
-            Join the pointer with lower value to new head
-            and increment that pointer. 
-
-        Once you reach the end of one of the list: then just join the remaining list
-        '''
-
-        l1 = list1
-        l2 = list2
-
-        newhead = h = ListNode()
-
-        while l1 and l2:
-            if l1.val <= l2.val:
-                h.next = l1
-                l1 = l1.next
+        while list1 and list2:
+            if list1.val <= list2.val:
+                h1.next = list1
+                h1, list1 = h1.next, list1.next
             else:
-                h.next = l2
-                l2 = l2.next
-            h = h.next
+                h1.next = list2
+                h1, list2 = h1.next, list2.next
         
-        if l1:
-            h.next = l1
+        if list1:
+            h1.next = list1
+        elif list2:
+            h1.next = list2
         
-        if l2:
-            h.next = l2
-        
-        return newhead.next
+        return head.next
 
-        '''
-        Time Complexity: O(n)
-        Space Complexity: O(1)
-        '''
+        
