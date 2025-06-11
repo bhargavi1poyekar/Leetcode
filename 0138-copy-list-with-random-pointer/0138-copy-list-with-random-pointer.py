aa4995d4bcf99f19d81159596df2a8aa65021988
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-06-11 15:27:41 -0400

    Time: 36 ms (86.75%), Space: 18.7 MB (19.1%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -9,44 +9,25 @@ class Node:
 
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        '''Psedocode/ Plan
-        7 -> 7`-> 13 -> 13` -> 11 -> 11` -> 10 -> 10` -> 1 -> 1`
 
-        7 
-        deep_copy(7) -> create new node with val 7. 
-        copy.next = orig.next
-        orig.next = copy
-        orig = orig.next.next
-
-        traverse the orig nodes. 
-        (orig.next == its copy)
-        orig.next.random = orig.random.next if orig.random else None
-        orig = orig.next.next
-
-        Remove the orig nodes from the list. 
-        we start frop copyHead -> orighead.next
-        copy.next = copy.next.next
-        copy = copy.next.next
-        '''
         if not head:
-            return None
+            return head
+        
+        curr = head
 
+        while curr:
+            newnode = Node(curr.val)
+            newnode.next = curr.next
+            curr.next = newnode
+            curr = curr.next.next
+        
         orig = head
 
         while orig:
-            copy = Node(orig.val)
-            copy.next = orig.next
-            orig.next = copy
-            orig = orig.next.next
-        
-        orig = head
-
-        while orig and orig.next:
             orig.next.random = orig.random.next if orig.random else None
             orig = orig.next.next
         
-        copyHead = head.next
-        copy = head.next
+        copyHead = copy = head.next
 
         while copy and copy.next:
             copy.next = copy.next.next
@@ -54,7 +35,5 @@ class Solution:
         
         return copyHead
 
-        '''
-        Time Complexity: O(N)
-        Space Complexity: O(N)
-        '''
+
+        
