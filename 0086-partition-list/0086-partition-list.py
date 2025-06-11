907d346571fdb01f2da71d7a3796048f32092433
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-06-11 16:38:35 -0400

    Time: 0 ms (100%), Space: 17.9 MB (41.21%) - LeetHub

--- 0086-partition-list/0086-partition-list.py
+++ 0086-partition-list/0086-partition-list.py
@@ -6,45 +6,20 @@
 class Solution:
     def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
         
-        '''
-        Understand: 
-        Given head of LL and val x
-        partition such that all nodes less than x -> come before nodes greater or equal to x. 
+        smallHead = s_ptr = ListNode()
+        bigHead = b_ptr = ListNode()
 
-        Can the list be empty -> Yes
-        Can the list have duplicate values -> Yes
-        Can the list have negative integers -> Yes
-
-        Match: 2 pointers. one to start linking all nodes less than x 
-        and other to start linking all nodes greater/equal to x. 
-
-        Plan:
-        we traverse the list. 
-        we have 2 pointers. small, and large. 
-        So all nodes smaller to x -> attached to small, 
-        and nodes larger/equal to x -> attached to large. 
-
-        AT the end -> we join small linked list with larger linked list. 
-        '''
-
-        shead = small = ListNode()
-        lhead = large = ListNode()
         curr = head
 
         while curr:
-            if curr.val >= x:
-                large.next = curr
-                large = large.next
+            if curr.val < x:
+                s_ptr.next = curr
+                s_ptr, curr = s_ptr.next, curr.next
             else:
-                small.next = curr
-                small = small.next
-            curr = curr.next
+                b_ptr.next = curr
+                b_ptr, curr = b_ptr.next, curr.next
         
-        small.next = lhead.next
-        large.next = None # this is important. 
-        return shead.next
+        s_ptr.next = bigHead.next
+        b_ptr.next = None
 
-        '''
-        Time Complexity -> O(N)
-        Space COmplexity -> O(1)
-        '''
+        return smallHead.next
