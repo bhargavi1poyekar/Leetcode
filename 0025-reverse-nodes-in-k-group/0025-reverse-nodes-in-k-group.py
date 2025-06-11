af5b8e164e036946f3b1308f9d57c5deee089d00
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-06-11 15:57:13 -0400

    Time: 0 ms (100%), Space: 18.4 MB (86.08%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
@@ -6,52 +6,24 @@
 class Solution:
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
         
-        '''
-        Understand:
-
-        Given head of LL. 
-        Revere nodes of list k at a time. 
-
-        return modified list. 
-
-        k is pos integer <= length of ll. 
-        if num nodes -> not multiple of k -> let them remain as it is. 
-
-        Nodes need to be changed. 
-
-        Match: Reverse ll, count nodes, with con, tail. 
-
-        Plan:
-
-        Since we have to reverse every k -> only if there are k nodes. 
-        So first we have to count k nodes. once we know, there are k nodes.
-
-        Then we reverse from curr to k. 
-
-        Then, again we start from k+1th node, to next k nodes. and so on. 
-
-        We will use reverse Linked list 2 approach, with con tail, for reversing. 
-        '''
-
-        count = 0
         count_ptr = head
         curr = head
+        num_nodes = 0
         prev = None
 
-        while count_ptr and count < k:
-            # count = 1
-            # :
-            count += 1
+        while count_ptr and num_nodes < k:
             count_ptr = count_ptr.next
-
-            if count == k:
+            num_nodes += 1
+            
+            if num_nodes == k:
                 con, tail = prev, curr
-                while count:
+                while num_nodes:
                     nn = curr.next
                     curr.next = prev
                     prev = curr
                     curr = nn
-                    count -= 1
+                    num_nodes -= 1
+                
                 if con:
                     con.next = prev
                 else:
@@ -59,8 +31,10 @@ class Solution:
                 
                 tail.next = curr
                 prev = tail
-        
-        return head
             
+        return head
 
+        
+
+        
 
