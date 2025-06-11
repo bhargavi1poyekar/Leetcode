144eb4cfa4854dcda01578db8ac7bcc04f6c93c0
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-06-11 15:12:16 -0400

    Time: 99 ms (18.44%), Space: 41.5 MB (51.91%) - LeetHub

--- 2236-maximum-twin-sum-of-a-linked-list/2236-maximum-twin-sum-of-a-linked-list.py
+++ 2236-maximum-twin-sum-of-a-linked-list/2236-maximum-twin-sum-of-a-linked-list.py
@@ -5,22 +5,18 @@
 #         self.next = next
 class Solution:
     def pairSum(self, head: Optional[ListNode]) -> int:
-        '''
-        Understand:
-        given a ll -> we need to break it in middle, reverse the second half, and then have twin sum with first hald and second half. 
-        
-        Match: Middle + reverse + sum
-        '''
 
         slow = fast = head
         prev = None
+
         while fast and fast.next:
             prev = slow
             slow = slow.next
             fast = fast.next.next
+        
+        prev.next = None
 
         curr = slow
-        prev.next = None
         prev = None
 
         while curr:
@@ -30,17 +26,17 @@ class Solution:
             curr = nn
         
         head2 = prev
+
         max_twin_sum = 0
+
         while head and head2:
             twin_sum = head.val + head2.val
-            max_twin_sum = max(max_twin_sum, twin_sum)
+            max_twin_sum = max(twin_sum, max_twin_sum)
             head, head2 = head.next, head2.next
-
-        return max_twin_sum 
         
-        '''
-        Time Complexity: O(N)
-        Space Complexity: O(1)
-        '''
-            
+        return max_twin_sum
+
+
+
+        
         
