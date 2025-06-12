0cf05b2083bfb4569a6ace9083402a07abd22bca
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-06-12 15:21:17 -0400

    Time: 0 ms (100%), Space: 18.2 MB (29.93%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.py
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.py
@@ -11,23 +11,28 @@ class Solution:
             return []
             
         queue = deque([root])
-        lot = []
-        level_idx = 0
+        zigzag = []
+        level_num = 1
 
         while queue:
             level = []
             for _ in range(len(queue)):
                 node = queue.popleft()
                 level.append(node.val)
-
                 if node.left:
                     queue.append(node.left)
                 if node.right:
                     queue.append(node.right)
-            if level_idx % 2 == 0:
-                lot.append(level)
+            if level_num % 2 == 1:
+                zigzag.append(level)
             else:
-                lot.append(level[::-1])
-            level_idx += 1
+                zigzag.append(level[::-1])
+            level_num += 1
+        
+        return zigzag
             
-        return lot
+
+            
+            
+
+        
