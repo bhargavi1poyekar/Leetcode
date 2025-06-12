7c752c4b205c3b7d2c15699d056068772c34bc65
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-06-12 16:48:28 -0400

    Time: 53 ms (63.93%), Space: 21.9 MB (90.91%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -8,31 +8,9 @@
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
 
-        '''
-        Understand:
-
-        Given binary tree -> return lowest common ancestor of given nodes. 
-        lowest node, that has both p and q as descendants. 
-
-        Match:
-        dfs
-
-        Plan:
-        ok, so first of all, if any of the p, and q is root -> then it will be the lca -> because -> node can be descendant of itself. 
-
-        now, if none of them is root. 
-        we check in the left tree. 
-        and check in right tree. 
-
-        Now on both sides, if we get something. then one of them is in left and other in right. -> so we return root. 
-
-        if only left, then both are on left, we return left. 
-        otherwise right. 
-        '''
-
         def dfs(root):
             if not root:
-                return 
+                return None
             
             if p == root or q == root:
                 return root
@@ -42,16 +20,9 @@ class Solution:
 
             if left and right:
                 return root
-            if left:
+            elif left:
                 return left
             return right
-    
+        
         return dfs(root)
-
-        '''
-        Time Complexity -> O(n)
-        Space -> O(n) -> stack overhead.
-        '''
-            
-
         
