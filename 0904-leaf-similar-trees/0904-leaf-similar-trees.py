0e0cc3de4de56942c9367a187bc4a827d53206c2
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-06-12 16:51:52 -0400

    Time: 0 ms (100%), Space: 17.9 MB (44.88%) - LeetHub

--- 0904-leaf-similar-trees/0904-leaf-similar-trees.py
+++ 0904-leaf-similar-trees/0904-leaf-similar-trees.py
@@ -6,39 +6,14 @@
 #         self.right = right
 class Solution:
     def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
-        '''
-        Understand:
-
-        Given: roots of 2 trees
-        get list of leaves of binary tree from left to right. 
-
-        Return if both trees have same leaves in the same order. 
-
-        Match:
-        dfs on the tree -> get leaves
-
-        Plan:
-        Perform dfs on both trees. Get list of leaves of the trees. 
-        Compare both lists. 
-        '''
-
-        # Implmentation. 
-
+        
         def dfs(root):
             if not root:
                 return []
-            # Check if leaves, if yes, then return them in a list
             if not root.left and not root.right:
                 return [root.val]
             
-            # Concatenate the left and right leaves.
             return dfs(root.left) + dfs(root.right)
         
-        # Compare list of leaves of both trees. 
         return dfs(root1) == dfs(root2)
-
-        '''
-        Time Complexity: O(M + N) -> M: num nodes in tree1 and N: num nodes in tree 2
-
-        Space Complexity: O(M + N) -> stack overhead and leaves list. 
-        '''
+            
