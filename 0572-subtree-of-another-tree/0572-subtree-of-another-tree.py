dadd25616498a334a162c74e888acd54dd17cdc7
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-06-12 17:07:26 -0400

    Time: 50 ms (22.69%), Space: 18 MB (58.55%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -7,46 +7,17 @@
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
         
-        '''
-        Understand:
-
-        Given 2 roots -> root and subRoot. 
-        return true if subtree of root same as subRoot. else False
-
-        We need to check if subtree of root is same as subRoot. 
-
-        Match: isSame()
-        and do dfs for root, and its children, to match with subRoot. 
-        
-        Plan: separate function to check if two trees are same. 
-
-        if both roots are None -> then True
-        but if one of them is None -> then False. 
-
-        Else compare the root values, and check left and right children recursively. If all same, only then trees are equal. 
-
-        Now for dfs -> we first compare current root with subRoot. If same return True. 
-        else check for root.left and right. recursively. 
-
-        If any of them matches -> return True. 
-        '''
-        def isSame(root1, root2):
-            if not root1 and not root2:
+        def isSame(p, q):
+            if not p and not q:
                 return True
-            if not root1 or not root2:
-                return False
-            
-            return root1.val == root2.val and isSame(root1.left, root2.left) and isSame(root1.right, root2. right)
+            if not p or not q:
+                return False  
+            return p.val == q.val and isSame(p.left, q.left) and isSame(p.right, q.right)
         
-        def dfs(root, subRoot):
-            if not root:
-                return False
-            if isSame(root, subRoot): return True
-
-            return dfs(root.left, subRoot) or dfs(root.right, subRoot)
+        if not root:
+            return False
         
-        return dfs(root, subRoot)
-
-
-
-            
+        if isSame(root, subRoot):
+            return True
+        
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
