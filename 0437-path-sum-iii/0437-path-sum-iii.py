60fcbdcafdb611232e754873aabfd25a5b332269
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-06-14 19:11:46 -0400

    Time: 7 ms (64.74%), Space: 18.3 MB (70.26%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.py
+++ 0437-path-sum-iii/0437-path-sum-iii.py
@@ -6,64 +6,25 @@
 #         self.right = right
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
-        '''
-        Understand:
-
-        Given: root of binary tree and targetSum
-        Return num paths -> where sum of values along path == targetSUm
         
-        Path doesnt need to start or end at root or leaf. -> but must go downwards.  
+        self.count = 0
+        self.paths = Counter()
+        self.paths[0] = 1
 
-        QuesT:
-        can the values -> be negative -> or 0?  -> Yes
-        Duplicate Values -> Yes
-        can a single node -> constitute as path? yes
 
-        Match: Binary tree DFS. 
-        And Hash to store the path sums. 
-
-        Plan:
-        Maintain a hash -> to store the sums and their count that we encounter. 
-        hash[0] = 1 -> for single node.     
-        self.num_paths. 
-
-        in dfs(root, hash, currsum):
+        def dfs(root, currsum):
             if not root:
                 return
             
-            curr_sum += root.val
-            num_paths += hash[curr_sum - targetSum]   # add num of times the required sum was present. 
+            currsum += root.val
+            self.count += self.paths[currsum - targetSum]
+            self.paths[currsum] += 1
 
-            hash[curr_sum] += 1 # add curr sum to hash. 
+            dfs(root.left, currsum)
+            dfs(root.right, currsum)
 
-            root.left(root.left, hash, curr_sum)
-            root.right(root.right, hash, curr_sum)
-            
-            hash[curr_sum] -= 1
-
-
-        '''
-        self.num_paths = 0
-        hash_count = Counter()
-        hash_count[0] = 1
-
-        def dfs(root, hash_count, curr_sum):
-            if not root:
-                return
-            
-            curr_sum += root.val
-            self.num_paths += hash_count[curr_sum - targetSum]   # add num of times the required sum was present. 
-            hash_count[curr_sum] += 1 # add curr sum to hash. 
-
-            dfs(root.left, hash_count, curr_sum)
-            dfs(root.right, hash_count, curr_sum)
-            
-            hash_count[curr_sum] -= 1
+            self.paths[currsum] -= 1
+            return 
         
-        dfs(root, hash_count, 0)
-        return self.num_paths
-        
-        '''
-        Time Complexity: O(n)
-        Space Complexity: O(n)
-        '''
+        dfs(root, 0)
+        return self.count
