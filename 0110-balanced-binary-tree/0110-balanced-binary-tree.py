3c3dd9fb0fe3dec98c2965944622a334044a8b02
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-06-14 19:38:07 -0400

    Time: 0 ms (100%), Space: 19 MB (26.99%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -8,22 +8,17 @@ class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
         
         def dfs(root):
-            # if null node:
             if not root:
-                return True, 0 # height is balanced and 0
+                return True, 0
             
-            # Check left subtree -> if not balanced return False
-            left_balance, left_ht = dfs(root.left)
-            if not left_balance:
-                return False, 0
-
-            # Check right subtree -> if not balanced return False
-            right_balance, right_ht = dfs(root.right)
-            if not right_balance:
-                return False, 0
-
+            left_balanced, left_ht = dfs(root.left)
+            if not left_balanced:
+                return False, left_ht
             
-            # if left and right subtree are balanced, now check balance of curr node. and return height of curr node.  
-            return abs(left_ht - right_ht) <= 1, 1+ max(left_ht, right_ht)
-        
-        return dfs(root)[0] # 0th index tells true of false for balanced
+            right_balanced, right_ht = dfs(root.right)
+            if not right_balanced:
+                return False, right_ht
+            
+            return abs(left_ht - right_ht) <= 1, 1 + max(left_ht, right_ht)
+
+        return dfs(root)[0]
