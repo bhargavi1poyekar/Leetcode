af7cedc0c29816979abfd1aacf442ba6883dd918
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-06-14 19:47:48 -0400

    Time: 10 ms (5.75%), Space: 21 MB (93.23%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -7,28 +7,9 @@
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
         
-        '''
-        Understood:
-
-        Given root of BST and k. 
-        Return -> kth smallest value (for 0 index -> k-1)   
-
-        Match:
-        inorder -> inorder -> gives sorted list of a BST Tree
-        
-        Plan:
-        Get the sorted list of BST by performing inorder on the tree 
-        Return k-1 st index element in the list. 
-
-        '''
         def inorder(root):
-            if not root:
-                return []
+            if not root: return []
             return inorder(root.left) + [root.val] + inorder(root.right)
-        
-        return inorder(root)[k-1]
 
-        '''
-        Time complexity: O(n) -> traversing all the nodes. 
-        Space: O(n) -> sorted list
-        '''
+        sorted_arr = inorder(root)
+        return sorted_arr[k-1] 
