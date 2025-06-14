e561d7a1c42e4b9c37601a6604a5348884c2201f
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-06-14 14:53:06 -0400

    Time: 45 ms (64.26%), Space: 19.1 MB (17.54%) - LeetHub

--- 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.py
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.py
@@ -10,61 +10,28 @@ class Node:
 
 class Solution:
     def connect(self, root: 'Node') -> 'Node':
-        '''
-        Understand: Populate next pointer -> to its right node. in the same level. 
-        no right node -> set to Null. 
-
-        Match: BFS -> traverse level by level
-
-        Plan:
-        for BFS, once we add the root:
         
-        while queue:
-            store length of queue
-            add NULL pointer in the queue. 
-            
-            for each node in queue (stored length):
-                node = popleft
-                node.right = queue[0]
-
-                add left and right nodes if exist to the queue. 
-            
-            popleft the null pointer. 
-        
-        return the root. 
-        '''
-        # if empty root -> return empty list 
         if not root:
             return root
-        
+
         queue = deque([root])
 
         while queue:
             length = len(queue)
-
-            # Add Null pointer at end of queue. 
             queue.append(None)
 
             for _ in range(length):
                 node = queue.popleft()
-
-                # add the next pointer to the next right node in a level
                 node.next = queue[0]
-            
                 if node.left:
                     queue.append(node.left)
-                
                 if node.right:
                     queue.append(node.right)
-            
-            # Remove null pointer. 
+                
             queue.popleft()
         
         return root
-        '''
-        Time Complexity -> O(N)
-        Space -> O(n)
-        '''
+
 
 
 
