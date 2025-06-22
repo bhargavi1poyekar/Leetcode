25d87a307c2c1dae09ab2c368b8679bd541e9d19
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-06-22 12:48:30 -0400

    Time: 78 ms (66.09%), Space: 25.5 MB (12.45%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -1,40 +1,10 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        '''
-        Understand:
-
-        beginWord to endWord using wordList. 
-
-        adjancent pair of words -> in sequence differ by single letter. 
-        0 if no sequence exists. 
-
-        Questions:
-        endWord needs to be in wordList?
-        all words length -> same
-        words are unique. 
-
-        return: number of words in sequence. 
-
-        Match:
-        Graph BFS
-
-        But how to find neighbors. 
-
-        Create an intermediate words from substituting each letter from words in the wordList with *. 
-
-        Store these intermediate words as key and all the words, that can form this intermediate word will be the values. 
-
-        Now, in bfs, when we need to find neighbors. we create intermediate words of the current word. And then get the neighbors of that intermediate word (values) from the hash. 
-        '''
-
-        if endWord not in wordList:
-            return 0
-
-        word_length = len(beginWord)
+        
         graph = defaultdict(list)
 
         for word in wordList:
-            for i in range(word_length):
+            for i in range(len(word)):
                 inter_word = word[:i] + '*' + word[i+1:]
                 graph[inter_word].append(word)
         
@@ -42,25 +12,17 @@ class Solution:
         seen = {beginWord}
 
         while queue:
-            word, count = queue.popleft()
+            word, steps = queue.popleft()
 
             if word == endWord:
-                return count
+                return steps
 
-            for i in range(word_length):
+            for i in range(len(word)):
                 inter_word = word[:i] + '*' + word[i+1:]
-            
                 for nghbr in graph[inter_word]:
                     if nghbr not in seen:
                         seen.add(nghbr)
-                        queue.append((nghbr, count + 1))
+                        queue.append((nghbr, steps + 1))
         
         return 0
-
-        '''
-        Time Complexity: O(M^2×N)
-        where M is the length of each word and N is the total number of words in the input word list.
-
-        Space Complexity -> same
-        '''
 
