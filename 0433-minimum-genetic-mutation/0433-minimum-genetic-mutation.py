7d9f8152930eab10ff4377a8d7eb3b2af89a42c5
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-06-22 12:07:23 -0400

    Time: 0 ms (100%), Space: 17.7 MB (64.56%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
@@ -1,69 +1,27 @@
 class Solution:
     def minMutation(self, startGene: str, endGene: str, bank: List[str]) -> int:
         
-        '''
-        Understand:
-        string -> 8 char long _> choices A, C, G T
-
-        startGene to endGene. 
-        1 mutation -> 1 char changed. 
-
-        geneBank -> valid genes. 
-
-        return -> min num of mutations -> from startGene to endGene
-        not possible -> return -1
-
-        start might not be in bank
-
-        can startGene == endGene -> return 0? 
-        only these chars -> A C G T. in start and end?
-        lentgh of both start and end would be same?
-
-        Match:
-
-        BFS Traversal -> to get min mutations. 
-
-        Plan:
-
-        Normal BFS traversal using queue. 
-        Here the problem is that we have to create the graph neighbors dynamically. 
-
-        So we generate neighbors using what we know. 
-        8 chars -> and 4 possibilities for each 
-        so in total 4 ** 8 total possibilities. -> but this is constant fixed. 
-        '''
-
-        def get_neighbrs(gene):
-            nghbr_genes = []
+        def find_nghbrs(gene):
+            nghbrs = []
             for i in range(8):
-                for char in ['A', 'C', 'G', 'T']:
-                    inter_gene = gene[:i] + char + gene[i+1:]
-                    nghbr_genes.append(inter_gene)
-
-            return nghbr_genes 
-
-        if endGene not in bank:
-            return -1
+                for ch in ['A', 'C', 'G', 'T']:
+                    newgene = gene[:i] + ch + gene[i+1:]
+                    nghbrs.append(newgene)
+            
+            return nghbrs
 
         queue = deque([(startGene, 0)])
         seen = {startGene}
 
         while queue:
-            gene, num_mut = queue.popleft()
+            gene, steps = queue.popleft()
 
             if gene == endGene:
-                return num_mut
+                return steps
             
-            neighbrs = get_neighbrs(gene)
-            for nghbr in neighbrs:
-                if nghbr in bank and nghbr not in seen:
-                    seen.add(nghbr)
-                    queue.append((nghbr, num_mut + 1))
-            
+            for nextGene in find_nghbrs(gene):
+                if nextGene in bank and nextGene not in seen:
+                    seen.add(nextGene)
+                    queue.append((nextGene, steps + 1))
+        
         return -1
-
-        '''
-        Time Complexity: O(B) -> bank.length
-        Space: O(1) -> constant for the 4 ** 8
-
-        '''
