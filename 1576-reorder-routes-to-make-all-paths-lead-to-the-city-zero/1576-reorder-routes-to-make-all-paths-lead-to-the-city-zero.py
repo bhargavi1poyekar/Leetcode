e006b3740f6de1b92d1d315e61b7630f2bfbb98b
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-06-22 10:26:40 -0400

    Time: 200 ms (55.14%), Space: 51.7 MB (49.01%) - LeetHub

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -1,34 +1,6 @@
 class Solution:
     def minReorder(self, n: int, connections: List[List[int]]) -> int:
-        '''
-        Understand:
-
-        Given -> n cities -> numbered from 0 to n-1 
-        and n-1 roads -> only one way to travel betwen 2 cities. 
-
-        Unidirectional. 
-
-        roads -> connections. [a, b] -> road from a to b.
-
-        want to travel to city 0. 
-
-        reorienting some roads _> so each city can visit city 0. 
-        return min edges changed. guaranteed that each city cna reach 0. 
-
-        Match: DFS traversal. 
-
-        Plan:
         
-        We consider all roads as unidirectional. 
-        and store the real roads as tuple in a set/ list.  
-
-        Now, we traverse from city 0. 
-
-        If we find a road that is present in real road -> we need to reverse this road. because right now we are travelling from 0 to other cities. So current road is going away from 0. so we need to reverse it. 
-
-        Keep count of such roads. 
-        '''
-
         graph = defaultdict(list)
         roads = set()
 
@@ -37,23 +9,17 @@ class Solution:
             graph[b].append(a)
             roads.add((a, b))
         
-        seen = {0}
         self.reorder = 0
-
-        def dfs(city):
-            for nghbr in graph[city]:
+        def dfs(node):
+            for nghbr in graph[node]:
                 if nghbr not in seen:
-                    if (city, nghbr) in roads:
-                        self.reorder += 1    
+                    if (node, nghbr) in roads:
+                        self.reorder += 1
                     seen.add(nghbr)
                     dfs(nghbr)
-        
-        dfs(0)
-        return self.reorder
 
-        '''
-        Time Complexity: O(N)
-        Space Complexity: O(N)
-        
-        '''
-                
+        seen = {0}
+        dfs(0)
+
+        return self.reorder 
+
