eff58cdc32f3c6bad9d44be9707dc0c2f9fc2f14
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-06-22 10:04:20 -0400

    Time: 27 ms (85.03%), Space: 19.3 MB (64.55%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -2,32 +2,34 @@ class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         
         def isValid(row, col):
-            return 0 <= row < Row and 0 <= col < Col
+            return  0 <= row < Row and 0 <= col < Col
         
-        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-
         Row = len(heights)
         Col = len(heights[0])
-        
-        def dfs(row, col, seen):
-            seen.add((row, col))
 
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+
+        pacific_set = set()
+        atlantic_set = set()
+
+        def dfs(row, col, seen):
+            if (row, col) not in seen:
+                seen.add((row, col))
+            else:
+                return
+            
             for dx, dy in directions:
                 nextr, nextc = row + dx, col + dy
-                if isValid(nextr, nextc) and (nextr, nextc) not in seen and heights[nextr][nextc] >= heights[row][col]:
+                if isValid(nextr, nextc) and heights[nextr][nextc] >= heights[row][col]:
                     dfs(nextr, nextc, seen)
-        
-            
-        
-        Pacific = set()
-        Atlantic = set()
 
-        for i in range(Row):
-            dfs(i, 0, Pacific)
-            dfs(i, Col-1, Atlantic)
+        for row in range(Row):
+            dfs(row, 0, pacific_set) # Left border
+            dfs(row, Col-1, atlantic_set) # Right border
         
-        for i in range(Col):
-            dfs(0, i, Pacific)
-            dfs(Row-1, i, Atlantic)
-    
-        return list(Pacific.intersection(Atlantic))
+        for col in range(Col):
+            dfs(0, col, pacific_set) # Top border
+            dfs(Row-1, col, atlantic_set) # Bottom border
+        
+        return list(pacific_set.intersection(atlantic_set))
+        
