7ef728f3fa90eac7893d80989df2a716a1dd1424
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-06-22 11:53:26 -0400

    Time: 12 ms (88%), Space: 17.8 MB (79%) - LeetHub

--- 0945-snakes-and-ladders/0945-snakes-and-ladders.py
+++ 0945-snakes-and-ladders/0945-snakes-and-ladders.py
@@ -1,20 +1,23 @@
 class Solution:
     def snakesAndLadders(self, board: List[List[int]]) -> int:
         
-        end_idx = len(board)**2
+        # start_idx = 0
+        end_idx = len(board)**2 - 1
+
         flat_board = []
 
         i = 0
-        for row in range(len(board)-1, -1, -1):
-            if i % 2 == 0:
+        for row in range(len(board) - 1, -1, -1):
+            if i == 0:
                 for col in range(len(board)):
                     flat_board.append(board[row][col])
+                i = 1
             else:
                 for col in range(len(board)-1, -1, -1):
                     flat_board.append(board[row][col])
-            i += 1
+                i = 0
 
-        seen = {(0)}
+        seen = {0}
         queue = deque([(0, 0)])
         # steps = -1
 
@@ -23,11 +26,10 @@ class Solution:
 
             if flat_board[curr] != -1:
                 curr = flat_board[curr] - 1
-
-            if curr == end_idx - 1:
+            if curr == end_idx:
                 return steps
 
-            for next in range(curr+1, min(curr + 7, end_idx)):
+            for next in range(curr+1, min(curr + 7, end_idx+1)):
                 if next not in seen:
                     seen.add(next)
                     queue.append((next, steps+1))
