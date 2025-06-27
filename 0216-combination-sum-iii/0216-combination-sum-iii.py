0cc7cda5a76cf3d35773729726b86e20cb3133f8
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-06-27 09:57:04 -0400

    Time: 0 ms (100%), Space: 17.8 MB (37.46%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.py
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -5,12 +5,14 @@ class Solution:
             if currsum == n and len(curr) == k:
                 ans.append(curr[:])
                 return
+            
+            if currsum > n:
+                return 
 
             for num in range(i, 10):
-                if currsum + num <= n:
-                    curr.append(num)
-                    backtrack(num + 1, curr, currsum + num)
-                    curr.pop()
+                curr.append(num)
+                backtrack(num + 1, curr, currsum + num)
+                curr.pop()
         
         ans = []
         backtrack(1, [], 0)
