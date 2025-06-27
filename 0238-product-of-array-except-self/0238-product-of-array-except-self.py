9ae58ac307fd6e6e045c15c424ca22b364c0cf63
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-06-27 11:15:30 -0400

    Time: 17 ms (92.8%), Space: 23.4 MB (54.29%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,20 +1,42 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         
-        left_prod = [1]*len(nums)
-        right_prod = [1]*len(nums)
+        # left_prod = [1]*len(nums)
+        # right_prod = [1]*len(nums)
 
-        # Left product:
+        # # Left product:
 
-        for i in range(1, len(nums)):
-            left_prod[i] = left_prod[i-1]*nums[i-1]
+        # for i in range(1, len(nums)):
+        #     left_prod[i] = left_prod[i-1]*nums[i-1]
         
-        for i in range(len(nums)-2, -1, -1):
-            right_prod[i] = right_prod[i+1]*nums[i+1]
+        # for i in range(len(nums)-2, -1, -1):
+        #     right_prod[i] = right_prod[i+1]*nums[i+1]
+
+        # prod = []
+        # for i in range(len(nums)):
+        #     prod.append(left_prod[i]*right_prod[i])
+        
+        # return prod
+
+        # 1 array.
 
         prod = []
-        for i in range(len(nums)):
-            prod.append(left_prod[i]*right_prod[i])
+        prod.append(1)
+
+        for i in range(len(nums)-1):
+            prod.append(prod[i]*nums[i]) 
+
+        # print(prod)
+        
+        mul = 1
+        for i in range(len(nums)-1, -1, -1):
+            prod[i] = prod[i]*mul
+            mul = nums[i] * mul
         
         return prod
+
+
+
+
+
 
