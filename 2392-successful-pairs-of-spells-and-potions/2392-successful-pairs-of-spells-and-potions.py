68cc95894f50b06e73af1c28c7f0395690cbf913
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-06-28 16:54:17 -0400

    Time: 744 ms (21.56%), Space: 40.3 MB (98.52%) - LeetHub

--- 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.py
+++ 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.py
@@ -1,25 +1,28 @@
 class Solution:
     def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
-        
-        def binary_search(left, right, spell):
+        def binary_search(spell_strength):
+
+            left = 0
+            right = len(potions)
             while left < right:
-                mid = (left + right) // 2
-                if potions[mid]*spell >= success:
+                mid = left + (right - left) // 2
+                potion_strength = potions[mid]
+                prod = potion_strength * spell_strength
+
+                if prod >= success:
                     right = mid
                 else:
                     left = mid + 1
-            
             return left
 
-        left = 0
-        right = len(potions)
-
         potions.sort()
-        total_spells = len(spells)
-        pairs = [0] * total_spells
+        pairs = [0]*len(spells)
+        m = len(potions)
 
         for i in range(len(spells)):
-            index = binary_search(left, right, spells[i])
-            pairs[i] = right - index
+            index = binary_search(spells[i])
+            pairs[i] = m - index
         
         return pairs
+
+        
