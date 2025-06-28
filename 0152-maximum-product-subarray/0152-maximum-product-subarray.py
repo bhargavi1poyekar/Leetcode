bf3efa2e20769c826b30405a03b348612a75fbf7
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-06-28 17:34:23 -0400

    Time: 11 ms (50.65%), Space: 18.2 MB (80.01%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -1,37 +1,16 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
         
-        '''
-        Understand:
+        max_so_far = 1
+        min_so_far = 1
 
-        Given: nums -> find subarr with largest prod and return prod. 
-        
-        the integers will be negative ? yes
-        will there be 0 ? yes
+        max_prod = float('-inf')
 
-        Match: If not negative intergers -> we could have used greedy approach like Kadane's algorithm. Where if the subarray prod is lesser than curr element. then it wont exactly contribute, and hence we drop it. 
+        for num in nums:
+            max_temp = max(num, max_so_far*num, min_so_far*num)
+            min_so_far = min(num, max_so_far*num, min_so_far*num)
 
-        But here because of Negative integers and prod. -> 2 negative integers give a positive product. So here we can do is get max and min. because if we get any 2 non negative integers, then previous negative product can be saved. 
-
-        '''
-        if not nums:
-            return -1
-
-        max_so_far = nums[0]
-        min_so_far = nums[0]
-
-        max_prod = nums[0]
-
-        for i in range(1, len(nums)):
-            temp_max = max(nums[i], max_so_far * nums[i], min_so_far * nums[i])
-            min_so_far = min(nums[i], max_so_far * nums[i], min_so_far * nums[i])
-            max_so_far = temp_max
+            max_so_far = max_temp
             max_prod = max(max_prod, max_so_far)
-        
+
         return max_prod
-
-        '''
-        Time Complexity: O(n)
-        Space Complexity: O(1)
-        '''
-
