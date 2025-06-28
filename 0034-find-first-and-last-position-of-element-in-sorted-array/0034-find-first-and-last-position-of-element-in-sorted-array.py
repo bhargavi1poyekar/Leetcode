20f455663f4281f0006da2a7b634bbba5edb62fb
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-06-28 12:57:27 -0400

    Time: 0 ms (100%), Space: 19.1 MB (52.87%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,12 +1,9 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         
-        def first_position():
-            left = 0
-            right = len(nums)
-
+        def first_position(left, right):
             while left < right:
-                mid = (left + right) // 2
+                mid = left + (right - left) // 2
                 if nums[mid] >= target:
                     right = mid
                 else:
@@ -14,29 +11,25 @@ class Solution:
             
             return left
         
-        def last_position():
-            left = 0
-            right = len(nums)
-
+        def last_position(left, right):
             while left < right:
-                mid = (left + right) // 2
+                mid = left + (right - left) // 2
                 if nums[mid] > target:
                     right = mid
                 else:
                     left = mid + 1
             
-            return left
+            return left - 1
         
         ans = []
-        first_index = first_position()
+        first_index = first_position(0, len(nums))
         if not 0 <= first_index < len(nums):
-            return [-1, -1] 
+            return [-1, -1]
         elif nums[first_index] != target:
             return [-1, -1]
         else:
             ans.append(first_index)
         
-        ans.append(last_position()-1)
+        ans.append(last_position(0, len(nums)))
         return ans
-
-
+        
