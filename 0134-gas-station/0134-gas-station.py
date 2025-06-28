0e7b74db8b1a9f89c7a4fa1cb13f757d9cbab23e
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-06-28 18:36:34 -0400

    Time: 24 ms (54.82%), Space: 23.4 MB (44.17%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -3,14 +3,13 @@ class Solution:
         
         if sum(cost) > sum(gas):
             return -1
-
-        start_index = 0
-        curr_gas = 0
         
+        start = 0
+        curr_gas = 0
         for i in range(len(gas)):
             curr_gas += gas[i] - cost[i]
             if curr_gas < 0:
-                curr_gas = 0 
-                start_index = i + 1
+                curr_gas = 0
+                start = i + 1
         
-        return start_index
+        return start
