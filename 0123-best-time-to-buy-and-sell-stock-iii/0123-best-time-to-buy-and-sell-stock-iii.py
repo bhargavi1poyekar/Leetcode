d4961bf2969f73cf76458b10c2b392c831a8084c
Bhargavi Poyekar <54023812+bhargavi1poyekar@users.noreply.github.com>
2025-07-01 17:48:36 -0400

    Time: 1543 ms (14.97%), Space: 602.3 MB (5.1%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -1,60 +1,17 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        '''
-        Understand: 
-
-        Given arr prices -> price on ith day. 
-
-        find max profit -> complete atmost 2 transactions. 
-
-        transactions -> buy and sell. 
-
-        Match: Dynamic Programming. Current decision based on prev decisions. 
-
-        Plan:
-
-        dp(i, holding, remainTrans) -> return max profit that can be achieved on ith day. 
-
-        i -> curr day. 
-        holding -> whether we have buyed a stock and holding it. or not. -> boolean. 
-        remainTrans -> remaining Transactions. 
-
-        Recurrence Relations:
-
-        On each day -> we can either choose to buy/sell or not do anything and just move to next day. 
-
-        So dp = max(doNothing, doSomething)
-
-        Now for doing something -> it depends on whether we are currently holding a stock, or not. 
-        If holding, we can sell.
-        if not holding, we can buy. 
-
-        if sell -> go to next day with, dp(i+1, False, remainTrans - 1) + prices[i] (profit)
-        if buy -> go to next day with, dp(i+1, True, remainTrans) - prices[i]
-
-        Base Condition:
-        if we reached last day, or possible num of transactions completed -> then return 0. 
-        '''
-
-        @lru_cache()
-        def dp(i, holding, remainTrans):
-            if i == len(prices) or remainTrans == 0:
+        @lru_cache(None)
+        def dp(i, remainT, holding):
+            if i == len(prices) or remainT == 0:
                 return 0
             
-            doNothing = dp(i+1, holding, remainTrans)
+            doNothing = dp(i+1, remainT, holding)
 
             if holding:
-                # sell
-                doSomething = dp(i+1, False, remainTrans - 1) + prices[i]
+                doSomething = dp(i+1, remainT-1, False) + prices[i]
             else:
-                doSomething = dp(i+1, True, remainTrans) - prices[i]
+                doSomething = dp(i+1, remainT, True) - prices[i]
             
             return max(doNothing, doSomething)
-
-        return dp(0, False, 2)
-
-        '''
-        Time Complexity: O(n) -> number of states -> n x 2(True/False) x k(here value of k=2) * constant work. 
-        Space Complexity: O(n)
-        '''
-
+        
+        return dp(0, 2, False)
